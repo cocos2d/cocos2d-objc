d442b030557f5865e97d7f2c581b893f77a6ec65
Ricardo Quesada <ricardoquesada@gmail.com>
2008-06-06 18:51:35 +0000

    * Added missing actions:    . CallFunc, Spawn, Place, Show, Hide, ToggleVisibility * Splitted Action.h in IntervalAction and InstantAction * Added missing NSAsserts
    Former-commit-id: fd678dcbac97140170b3e0bfcd23dfa380f38a6c

---
+++ cocos2d/InstantAction.m
@@ -0,0 +1,130 @@
+//
+// cocos2d for iphone
+// InstantAction
+//
+
+
+#import "InstantAction.h"
+#import "CocosNode.h"
+
+//
+// InstantAction
+//
+@implementation InstantAction
+@synthesize duration;
+
+-(id) init
+{
+	if( ![super init] )
+		return nil;
+	
+	duration = 0;
+	return self;
+}
+
+- (BOOL) isDone
+{
+	return YES;
+}
+-(void) step
+{
+	[self update: 1];
+}
+-(void) update: (double) t
+{
+	// ignore
+}
+@end
+
+//
+// Show
+//
+@implementation Show
+-(void) start
+{
+	[super start];
+	target.visible = YES;
+}
+@end
+
+//
+// Hide
+//
+@implementation Hide
+-(void) start
+{
+	[super start];
+	target.visible = NO;
+}
+@end
+
+//
+// ToggleVisibility
+//
+@implementation ToggleVisibility
+-(void) start
+{
+	[super start];
+	target.visible = ! target.visible;
+}
+@end
+
+//
+// Place
+//
+@implementation Place
++(id) actionWithPosition: (CGPoint) pos
+{
+	return [[[self alloc]initWithPosition:pos]autorelease];
+}
+
+-(id) initWithPosition: (CGPoint) pos
+{
+	if( ! [super init] )
+		return nil;
+	position = pos;
+	return self;
+}
+
+-(void) start
+{
+	[super start];
+	target.position = position;
+}
+@end
+
+//
+// CallFunc
+//
+@implementation CallFunc
++(id) actionWithTarget: (id) receiver selector:(SEL) cb
+{
+	return [[[self alloc] initWithTarget: receiver selector: cb] autorelease];
+}
+
+-(id) initWithTarget: (id) rec selector:(SEL) cb
+{
+	NSMethodSignature * sig = nil;
+	sig = [[rec class] instanceMethodSignatureForSelector:cb];
+
+	invocation = nil;
+	invocation = [NSInvocation invocationWithMethodSignature:sig];
+	[invocation setTarget:rec];
+	[invocation setSelector:cb];
+	[invocation retain];
+	return self;
+}
+
+-(void) dealloc
+{
+	if( invocation )
+		[invocation release];
+	[super dealloc];
+}
+
+-(void) start
+{
+	[super start];
+	[invocation invoke];
+}
+@end
