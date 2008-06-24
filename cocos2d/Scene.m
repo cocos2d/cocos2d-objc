d70a76be4f7f733611cc0986ab6bd7d1067043ae
Ricardo Quesada <ricardoquesada@gmail.com>
2008-06-24 03:05:26 +0000

    added Copyright to cocos2d sources/headers added LICENSE /README / CHANGELOG files removed old Reame.txt
    Former-commit-id: 7db8e5b8d61263a3542179baf97aec4b41972613

--- cocos2d/Scene.m
+++ cocos2d/Scene.m
@@ -1,7 +1,23 @@
-//
-//  Scene.m
-//  cocos2d
-//
+/* cocos2d-iphone
+ *
+ * Copyright (C) 2008 Ricardo Quesada
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; version 3 or (it is your choice) any later
+ * version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
 
 #import "Scene.h"
 #import "Director.h"
