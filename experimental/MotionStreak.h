66d04d97dfad2036222fd330720e9085d52a08e0
Jason Booth <slipster216@gmail.com>
2009-02-03 03:40:07 +0000

    Ribbon and Streak code

---
+++ experimental/MotionStreak.h
@@ -0,0 +1,52 @@
+/* cocos2d for iPhone
+ *
+ * http://code.google.com/p/cocos2d-iphone
+ *
+ * Copyright (C) 2008 Ricardo Quesada
+ *
+ * Motion Streak Class by Jason Booth
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the 'cocos2d for iPhone' license.
+ *
+ * You will find a copy of this license within the cocos2d for iPhone
+ * distribution inside the "LICENSE" file.
+ *
+ *********************************************************
+ *
+ * Motion Streak manages a Ribbon based on it's motion in absolute space.
+ * You construct it with a fadeTime, minimum segment size, texture path, texture
+ * length and color. The fadeTime controls how long it takes each vertex in
+ * the streak to fade out, the minimum segment size it how many pixels the
+ * streak will move before adding a new ribbon segement, and the texture
+ * length is the how many pixels the texture is stretched across. The texture
+ * is vertically aligned along the streak segemnts. 
+ */
+
+#import <Foundation/Foundation.h>
+#import "CocosNode.h"
+#import "Ribbon.h"
+
+/** Motion Streak - creates a ribbon based on velocity of motion with fading tail */
+@interface MotionStreak : CocosNode 
+{
+  Ribbon* mRibbon;
+  float mFadeTime;
+  float mSegThreshold;
+  uint mColor;
+  NSString* mPath;
+  float mWidth;
+  float mTextureLength;
+  cpVect mLastLocation;
+}
+
+/** creates the streak */
++(id)streakWithFade:(float)fade minSeg:(float)seg image:(NSString*)path width:(float)width length:(float)length color:(uint)color;
+
+/** init the streak */
+-(id)initWithFade:(float)fade minSeg:(float)seg image:(NSString*)path width:(float)width length:(float)length color:(uint)color;
+
+/** polling function */
+-(void)update:(ccTime)delta;
+
+@end
---
+++ experimental/MotionStreak.m
@@ -0,0 +1,80 @@
+/* cocos2d for iPhone
+ *
+ * http://code.google.com/p/cocos2d-iphone
+ *
+ * Copyright (C) 2008 Ricardo Quesada
+ *
+ * Motion Streak Class by Jason Booth
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the 'cocos2d for iPhone' license.
+ *
+ * You will find a copy of this license within the cocos2d for iPhone
+ * distribution inside the "LICENSE" file.
+ *
+ *********************************************************
+ *
+ * Motion Streak manages a Ribbon based on it's motion in absolute space.
+ * You construct it with a fadeTime, minimum segment size, texture path, texture
+ * length and color. The fadeTime controls how long it takes each vertex in
+ * the streak to fade out, the minimum segment size it how many pixels the
+ * streak will move before adding a new ribbon segement, and the texture
+ * length is the how many pixels the texture is stretched across. The texture
+ * is vertically aligned along the streak segemnts. 
+ */
+
+#import "MotionStreak.h"
+#import "Scheduler.h"
+
+@implementation MotionStreak
+
++(id)streakWithFade:(float)fade minSeg:(float)seg image:(NSString*)path width:(float)width length:(float)length color:(uint)color
+{
+  self = [[[MotionStreak alloc] initWithFade:(float)fade minSeg:seg image:path width:width length:length color:color] autorelease];
+  return self;
+}
+
+-(id)initWithFade:(float)fade minSeg:(float)seg image:(NSString*)path width:(float)width length:(float)length color:(uint)color
+{
+  self = [super init];
+  if (self)
+  {
+    mFadeTime = fade;
+    mSegThreshold = seg;
+    mPath = path;
+    mWidth = width;
+    mTextureLength = length;
+    mColor = color;
+    mLastLocation = [self absolutePosition];
+    mRibbon = [Ribbon ribbonWithLocation:[self position] width:mWidth image:mPath length:mTextureLength color:color fade:fade];
+    [self add:mRibbon];
+    
+    // manually add timer to scheduler
+    Timer *timer = [Timer timerWithTarget:self selector:@selector(update:) interval:0];
+    [[Scheduler sharedScheduler] scheduleTimer:timer];
+  }
+  return self;
+}
+
+-(void)update:(ccTime)delta
+{
+  cpVect location = [self absolutePosition];
+  [mRibbon setPosition:cpv(-1*location.x, -1*location.y)];
+  float len = sqrt(pow(mLastLocation.x - location.x, 2) + pow(mLastLocation.y - location.y, 2));
+  if (len > mSegThreshold)
+  {
+    [mRibbon addPointAt:location width:mWidth];
+    mLastLocation = location;
+  }
+  [mRibbon update:delta];
+}
+
+
+-(void)dealloc
+{
+  [self remove:mRibbon];
+  [mRibbon release];
+  [super dealloc];
+}
+
+@end
