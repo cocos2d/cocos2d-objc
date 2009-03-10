f6e5b5d453b2b1953e246c15d8452127c18fa08c
Lhunath <lhunath@gmail.com>
2009-03-10 22:33:46 +0000

    Fixed issue #206

--- cocos2d/Menu.m
+++ cocos2d/Menu.m
@@ -183,6 +183,176 @@
 	}
 }
 
+-(void) alignItemsInColumns: (NSNumber *) columns, ...
+{
+	va_list args;
+	va_start(args, columns);
+	
+	[self alignItemsInColumns:columns vaList:args];
+	
+	va_end(args);
+}
+
+-(void) alignItemsInColumns: (NSNumber *) columns vaList: (va_list) args
+{
+    NSMutableArray *rows = [[NSMutableArray alloc] initWithObjects:columns, nil];
+	columns = va_arg(args, NSNumber*);
+	while(columns) {
+        [rows addObject:columns];
+		columns = va_arg(args, NSNumber*);
+	}
+    
+	int height = -5;
+    NSUInteger row = 0, rowHeight = 0, columnsOccupied = 0, rowColumns;
+	for(MenuItem *item in children) {
+        if(row >= [rows count])
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:@"Too many menu items for the amount of rows/columns."
+                                         userInfo:nil];
+        
+        rowColumns = [(NSNumber *) [rows objectAtIndex:row] unsignedIntegerValue];
+        if(rowColumns == 0)
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:[NSString stringWithFormat:@"Can't have zero columns on a row (row %d).", row]
+                                         userInfo:nil];
+        
+        rowHeight = fmaxf(rowHeight, [item contentSize].height);
+        ++columnsOccupied;
+        
+        if(columnsOccupied >= rowColumns) {
+            height += rowHeight + 5;
+            
+            columnsOccupied = 0;
+            rowHeight = 0;
+            ++row;
+        }
+    }
+    if(columnsOccupied != 0)
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                       reason:@"Too many rows/columns for available menu items."
+                                     userInfo:nil];
+
+    CGSize winSize = [[Director sharedDirector] winSize];
+    
+    row = 0; rowHeight = 0; rowColumns = 0;
+	float w, x, y = height / 2;
+	for(MenuItem *item in children) {
+        if(rowColumns == 0) {
+            rowColumns = [(NSNumber *) [rows objectAtIndex:row] unsignedIntegerValue];
+            w = winSize.width / (1 + rowColumns);
+            x = w;
+        }
+
+        rowHeight = fmaxf(rowHeight, [item contentSize].height);
+        [item setPosition:cpv(x - winSize.width / 2,
+                              y - [item contentSize].height / 2)];
+        NSLog(@"item r:%d, c:%d placed at: %s", row, columnsOccupied, cpvstr([item position]));
+            
+        x += w + 10;
+        ++columnsOccupied;
+        
+        if(columnsOccupied >= rowColumns) {
+            y -= rowHeight + 5;
+            
+            columnsOccupied = 0;
+            rowColumns = 0;
+            rowHeight = 0;
+            ++row;
+        }
+	}
+    
+    [rows release];
+}
+
+-(void) alignItemsInRows: (NSNumber *) rows, ...
+{
+	va_list args;
+	va_start(args, rows);
+	
+	[self alignItemsInRows:rows vaList:args];
+	
+	va_end(args);
+}
+
+-(void) alignItemsInRows: (NSNumber *) rows vaList: (va_list) args
+{
+    NSMutableArray *columns = [[NSMutableArray alloc] initWithObjects:rows, nil];
+	rows = va_arg(args, NSNumber*);
+	while(rows) {
+        [columns addObject:rows];
+		rows = va_arg(args, NSNumber*);
+	}
+
+    NSMutableArray *columnWidths = [[NSMutableArray alloc] init];
+    NSMutableArray *columnHeights = [[NSMutableArray alloc] init];
+    
+	int width = -10, columnHeight = -5;
+    NSUInteger column = 0, columnWidth = 0, rowsOccupied = 0, columnRows;
+	for(MenuItem *item in children) {
+        if(column >= [columns count])
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:@"Too many menu items for the amount of rows/columns."
+                                         userInfo:nil];
+        
+        columnRows = [(NSNumber *) [columns objectAtIndex:column] unsignedIntegerValue];
+        if(columnRows == 0)
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:[NSString stringWithFormat:@"Can't have zero rows on a column (column %d).", column]
+                                         userInfo:nil];
+        
+        columnWidth = fmaxf(columnWidth, [item contentSize].width);
+        columnHeight += [item contentSize].height + 5;
+        ++rowsOccupied;
+        
+        if(rowsOccupied >= columnRows) {
+            [columnWidths addObject:[NSNumber numberWithUnsignedInteger:columnWidth]];
+            [columnHeights addObject:[NSNumber numberWithUnsignedInteger:columnHeight]];
+            width += columnWidth + 10;
+            
+            rowsOccupied = 0;
+            columnWidth = 0;
+            columnHeight = -5;
+            ++column;
+        }
+    }
+    if(rowsOccupied != 0)
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                       reason:@"Too many rows/columns for available menu items."
+                                     userInfo:nil];
+    
+    CGSize winSize = [[Director sharedDirector] winSize];
+    
+    column = 0; columnWidth = 0; columnRows = 0;
+	float x = -width / 2, y;
+	for(MenuItem *item in children) {
+        if(columnRows == 0) {
+            columnRows = [(NSNumber *) [columns objectAtIndex:column] unsignedIntegerValue];
+            y = ([(NSNumber *) [columnHeights objectAtIndex:column] intValue] + winSize.height) / 2;
+        }
+        
+        columnWidth = fmaxf(columnWidth, [item contentSize].width);
+        [item setPosition:cpv(x + [(NSNumber *) [columnWidths objectAtIndex:column] unsignedIntegerValue] / 2,
+                              y - winSize.height / 2)];
+        NSLog(@"item r:%d, c:%d placed at: %s", rowsOccupied, column, cpvstr([item position]));
+        
+        y -= [item contentSize].height + 10;
+        ++rowsOccupied;
+        
+        if(rowsOccupied >= columnRows) {
+            x += columnWidth + 5;
+            
+            rowsOccupied = 0;
+            columnRows = 0;
+            columnWidth = 0;
+            ++column;
+        }
+	}
+    
+    [columns release];
+    [columnWidths release];
+    [columnHeights release];
+}
+
 #pragma mark Menu - Opacity Protocol
 
 /** Override synthesized setOpacity to recurse items */
