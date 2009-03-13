8cefb440f950b0436fccb8d819b842b083e80bfb
Ricardo Quesada <ricardoquesada@gmail.com>
2009-03-13 02:01:24 +0000

    using udpated TouchJSON from svn

--- external/TouchJSON/JSON/CJSONDeserializer.m
+++ external/TouchJSON/JSON/CJSONDeserializer.m
@@ -3,7 +3,28 @@
 //  TouchJSON
 //
 //  Created by Jonathan Wight on 12/15/2005.
-//  Copyright 2005 Toxic Software. All rights reserved.
+//  Copyright (c) 2005 Jonathan Wight
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
 //
 
 #import "CJSONDeserializer.h"
@@ -11,7 +32,7 @@
 #import "CJSONScanner.h"
 #import "CDataScanner.h"
 
-NSString *const kJSONDeserializerErrorDomain /* = @"CJSONDeserializerErrorDomain" */;
+NSString *const kJSONDeserializerErrorDomain  = @"CJSONDeserializerErrorDomain";
 
 @implementation CJSONDeserializer
 
@@ -24,7 +45,7 @@ return([[[self alloc] init] autorelease]);
 {
 if (inData == NULL || [inData length] == 0)
 	{
-	if (outError && *outError)
+	if (outError)
 		*outError = [NSError errorWithDomain:kJSONDeserializerErrorDomain code:-1 userInfo:NULL];
 
 	return(NULL);
@@ -47,7 +68,7 @@ else
 {
 if (inData == NULL || [inData length] == 0)
 	{
-	if (outError && *outError)
+	if (outError)
 		*outError = [NSError errorWithDomain:kJSONDeserializerErrorDomain code:-1 userInfo:NULL];
 
 	return(NULL);
