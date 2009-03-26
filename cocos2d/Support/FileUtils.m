c5c06091d27230a9acc35146aebe62d0b086bd42
Ricardo Quesada <ricardoquesada@gmail.com>
2009-03-26 20:41:34 +0000

    fixed issue #220
    Former-commit-id: 5aa610fefd75fc0de03ee3824e6b33a72142e3b4

---
+++ cocos2d/Support/FileUtils.m
@@ -0,0 +1,34 @@
+/* cocos2d for iPhone
+ *
+ * http://code.google.com/p/cocos2d-iphone
+ *
+ * Copyright (C) 2008,2009 Ricardo Quesada
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the 'cocos2d for iPhone' license.
+ *
+ * You will find a copy of this license within the cocos2d for iPhone
+ * distribution inside the "LICENSE" file.
+ *
+ */
+
+#import "FileUtils.h"
+
+@implementation FileUtils
++(NSString*) fullPathFromRelativePath:(NSString*) relPath
+{
+	NSMutableArray *imagePathComponents = [NSMutableArray arrayWithArray:[relPath pathComponents]];
+	NSString *file = [imagePathComponents lastObject];
+	
+	[imagePathComponents removeLastObject];
+	NSString *imageDirectory = [NSString pathWithComponents:imagePathComponents];
+	
+	NSString *fullpath = [[NSBundle mainBundle] pathForResource:file
+														 ofType:nil
+													inDirectory:imageDirectory];
+	if (fullpath == nil)
+		fullpath = relPath;
+
+	return fullpath;	
+}
+@end
