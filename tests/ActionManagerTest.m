a4fea4b59fec263598f372702bd3742afec6cd14
Ricardo Quesada <ricardoquesada@gmail.com>
2009-08-15 16:50:47 +0000

    fixed issue #507

---
+++ tests/ActionManagerTest.m
@@ -0,0 +1,232 @@
+//
+// Parallax Demo
+// a cocos2d example
+// http://www.cocos2d-iphone.org
+//
+
+// cocos import
+#import "cocos2d.h"
+
+// local import
+#import "ActionManagerTest.h"
+
+enum {
+	kTagNode,
+	kTagGrossini,
+};
+
+static int sceneIdx=-1;
+static NSString *transitions[] = {
+			@"Test1",
+			@"Test2",
+};
+
+Class nextAction()
+{
+	
+	sceneIdx++;
+	sceneIdx = sceneIdx % ( sizeof(transitions) / sizeof(transitions[0]) );
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+Class backAction()
+{
+	sceneIdx--;
+	int total = ( sizeof(transitions) / sizeof(transitions[0]) );
+	if( sceneIdx < 0 )
+		sceneIdx += total;	
+	
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+Class restartAction()
+{
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+
+@implementation ActionManagerTest
+-(id) init
+{
+	[super init];
+
+
+	CGSize s = [[Director sharedDirector] winSize];
+		
+	Label* label = [Label labelWithString:[self title] fontName:@"Arial" fontSize:32];
+	[self addChild: label z:1];
+	[label setPosition: ccp(s.width/2, s.height-50)];
+	
+	MenuItemImage *item1 = [MenuItemImage itemFromNormalImage:@"b1.png" selectedImage:@"b2.png" target:self selector:@selector(backCallback:)];
+	MenuItemImage *item2 = [MenuItemImage itemFromNormalImage:@"r1.png" selectedImage:@"r2.png" target:self selector:@selector(restartCallback:)];
+	MenuItemImage *item3 = [MenuItemImage itemFromNormalImage:@"f1.png" selectedImage:@"f2.png" target:self selector:@selector(nextCallback:)];
+	
+	Menu *menu = [Menu menuWithItems:item1, item2, item3, nil];
+	
+	menu.position = CGPointZero;
+	item1.position = ccp( s.width/2 - 100,30);
+	item2.position = ccp( s.width/2, 30);
+	item3.position = ccp( s.width/2 + 100,30);
+	[self addChild: menu z:1];	
+
+	return self;
+}
+
+-(void) dealloc
+{
+	[super dealloc];
+}
+
+-(void) restartCallback: (id) sender
+{
+	Scene *s = [Scene node];
+	[s addChild: [restartAction() node]];
+	[[Director sharedDirector] replaceScene: s];
+}
+
+-(void) nextCallback: (id) sender
+{
+	Scene *s = [Scene node];
+	[s addChild: [nextAction() node]];
+	[[Director sharedDirector] replaceScene: s];
+}
+
+-(void) backCallback: (id) sender
+{
+	Scene *s = [Scene node];
+	[s addChild: [backAction() node]];
+	[[Director sharedDirector] replaceScene: s];
+}
+
+-(NSString*) title
+{
+	return @"No title";
+}
+@end
+
+#pragma mark Test1
+
+@implementation Test1
+-(id) init
+{
+	if( (self=[super init] )) {
+		
+
+		Sprite *child = [Sprite spriteWithFile:@"grossini.png"];
+		[child setPosition:ccp(200,200)];
+		[self addChild:child z:1];
+
+		//Sum of all action's duration is 0.5 second.
+		[child runAction:[RotateBy actionWithDuration:0.5f angle:90]];
+		[child runAction:[Sequence actions:[DelayTime actionWithDuration:0.4f], [FadeOut 
+																				 actionWithDuration:0.1f], nil]];
+		
+		//After 0.5 second, self will be removed.
+		[self runAction:[Sequence actions:[DelayTime actionWithDuration:0.4f], [CallFunc 
+																				actionWithTarget:self selector:@selector(removeThis)], nil]];
+	}
+	
+	return self;
+	
+}
+
+-(void) removeThis
+{
+	[parent removeChild:self cleanup:YES];
+}
+
+-(NSString *) title
+{
+	return @"ActionManagerTest 1";
+}
+@end
+
+#pragma mark Test2
+
+@implementation Test2
+-(id) init
+{
+	if( (self=[super init] )) {
+
+	}
+	
+	return self;
+}
+
+-(NSString *) title
+{
+	return @"ActionManagerTest 2";
+}
+@end
+
+
+// CLASS IMPLEMENTATIONS
+@implementation AppController
+
+- (void) applicationDidFinishLaunching:(UIApplication*)application
+{
+	// Init the window
+	window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	
+	// cocos2d will inherit these values
+	[window setUserInteractionEnabled:YES];	
+	[window setMultipleTouchEnabled:NO];
+	
+	// must be called before any othe call to the director
+//	[Director useFastDirector];
+	
+	// before creating any layer, set the landscape mode
+	[[Director sharedDirector] setDeviceOrientation:CCDeviceOrientationLandscapeLeft];
+	[[Director sharedDirector] setAnimationInterval:1.0/60];
+	[[Director sharedDirector] setDisplayFPS:YES];
+	
+	// Default texture format for PNG/BMP/TIFF/JPEG/GIF images
+	// It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
+	// You can change anytime.
+	[Texture2D setDefaultAlphaPixelFormat:kTexture2DPixelFormat_RGBA8888];	
+
+	// create an openGL view inside a window
+	[[Director sharedDirector] attachInView:window];	
+	[window makeKeyAndVisible];	
+	
+	Scene *scene = [Scene node];
+	[scene addChild: [nextAction() node]];
+			 
+	[[Director sharedDirector] runWithScene: scene];
+}
+
+// getting a call, pause the game
+-(void) applicationWillResignActive:(UIApplication *)application
+{
+	[[Director sharedDirector] pause];
+}
+
+// call got rejected
+-(void) applicationDidBecomeActive:(UIApplication *)application
+{
+	[[Director sharedDirector] resume];
+}
+
+// purge memroy
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+	[[TextureMgr sharedTextureMgr] removeAllTextures];
+}
+
+// next delta time will be zero
+-(void) applicationSignificantTimeChange:(UIApplication *)application
+{
+	[[Director sharedDirector] setNextDeltaTimeZero:YES];
+}
+
+- (void) dealloc
+{
+	[window release];
+	[super dealloc];
+}
+@end
