fdc464812334aa98c70b783c24c09a88aaa711ba
Ricardo Quesada <ricardoquesada@gmail.com>
2009-08-25 00:01:02 +0000

    using box2d r10 (v2.1.0) . issue #494
    Former-commit-id: 1245c48576eac1172bce1768000540ba14c6c0de

--- external/Box2d/Testbed/Tests/TestEntries.cpp
+++ external/Box2d/Testbed/Tests/TestEntries.cpp
@@ -39,6 +39,7 @@
 #include "Prismatic.h"
 #include "Pulleys.h"
 #include "Pyramid.h"
+#include "RayCast.h"
 #include "Revolute.h"
 #include "SensorTest.h"
 #include "ShapeEditing.h"
@@ -53,8 +54,9 @@
 
 TestEntry g_testEntries[] =
 {
-	{"PolyCollision", PolyCollision::Create},
+	{"Ray-Cast", RayCast::Create},
 	{"Pyramid", Pyramid::Create},
+	{"PolyCollision", PolyCollision::Create},
 	{"One-Sided Platform", OneSidedPlatform::Create},
 	{"Apply Force", ApplyForce::Create},
 	{"Bridge", Bridge::Create},
