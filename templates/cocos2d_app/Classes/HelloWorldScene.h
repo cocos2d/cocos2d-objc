8d32dbe9086003e00e2bc1e5c76f50f0277c393f
Ricardo Quesada <ricardoquesada@gmail.com>
2009-09-22 16:45:57 +0000

    fixing mess. part of issue #561

---
+++ templates/cocos2d_app/Classes/HelloWorldScene.h
@@ -0,0 +1,13 @@
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorld Layer
+@interface HelloWorld : Layer
+{
+}
+
+// returns a Scene that contains the HelloWorld as the only child
++(id) scene;
+
+@end
