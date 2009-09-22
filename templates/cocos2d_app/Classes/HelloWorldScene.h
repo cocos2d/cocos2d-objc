490097cce29bf5d5565f39f8f050d49ec2527c68
Ricardo Quesada <ricardoquesada@gmail.com>
2009-09-22 16:11:46 +0000

    added resources
    Former-commit-id: 6badf9a5047eb0eac11fbc24b7e488a0fcc67e08

--- templates/cocos2d_app/Classes/HelloWorldScene.h
+++ templates/cocos2d_app/Classes/HelloWorldScene.h
@@ -1,10 +1,14 @@
 
 // When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
+#import "Box2D.h"
+#import "GLES-Render.h"
 
 // HelloWorld Layer
 @interface HelloWorld : Layer
 {
+	b2World* world;
+	GLESDebugDraw *m_debugDraw;
 }
 
 // returns a Scene that contains the HelloWorld as the only child
