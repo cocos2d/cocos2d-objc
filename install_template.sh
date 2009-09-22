75f2aec423ed27b9853457611b5cc6265679a746
Ricardo Quesada <ricardoquesada@gmail.com>
2009-09-22 16:43:26 +0000

    fixed issue #561 added box2d + cocos2d template

--- install_template.sh
+++ install_template.sh
@@ -5,6 +5,10 @@ echo 'cocos2d-iphone template installer'
 DST_DIR='/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Project Templates/Application/cocos2d-0.8.2-beta Application/'
 LIBS_DIR="$DST_DIR"libs
 
+echo 'Installing cocos2d template'
+echo '---------------------------'
+echo ''
+
 #rm -rf $DST_DIR
 
 if [[ -d $DST_DIR ]];  then
@@ -16,7 +20,7 @@ echo ...creating destination directory: $DST_DIR
 mkdir -p "$DST_DIR"
 
 echo ...copying template files
-cp -R cocos2d-iphone-template/ "$DST_DIR"
+cp -R templates/cocos2d_app/ "$DST_DIR"
 
 echo ...copying cocos2d files
 cp -R cocos2d "$LIBS_DIR"
@@ -32,5 +36,46 @@ cp -R cocoslive "$LIBS_DIR"
 
 echo ...copying cocoslive dependency files
 cp -R external/TouchJSON "$LIBS_DIR"
+
+echo done!
+
+echo ''
+echo ''
+echo ''
+echo 'Installing cocos2d + box2d template'
+echo '-----------------------------------'
+echo ''
+
+DST_DIR='/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Project Templates/Application/cocos2d-0.8.2-beta Box2d Application/'
+LIBS_DIR="$DST_DIR"libs
+
+if [[ -d $DST_DIR ]];  then
+    echo "template alredy installed"
+    exit 1
+fi
+
+echo ...creating destination directory: $DST_DIR
+mkdir -p "$DST_DIR"
+
+echo ...copying template files
+cp -R templates/cocos2d_box2d_app/ "$DST_DIR"
+
+echo ...copying cocos2d files
+cp -R cocos2d "$LIBS_DIR"
+
+echo ...copying cocos2d dependency files
+cp -R external/FontLabel "$LIBS_DIR"
+
+echo ...copying CocosDenshion files
+cp -R CocosDenshion "$LIBS_DIR"
+
+echo ...copying cocoslive files
+cp -R cocoslive "$LIBS_DIR"
+
+echo ...copying cocoslive dependency files
+cp -R external/TouchJSON "$LIBS_DIR"
+
+echo ...copying Box2D files
+cp -R external/Box2d/Box2D "$LIBS_DIR"
 
 echo done!
