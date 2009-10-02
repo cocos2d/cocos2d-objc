38401bf17ed14fd5bb2b5d8f71abbe0776c07324
Ricardo Quesada <ricardoquesada@gmail.com>
2009-10-02 18:00:28 +0000

    added cocos2d + chipmunk template
    Former-commit-id: 73cf21fe47bae94486d3c51934b2b9616e4596b1

--- install_template.sh
+++ install_template.sh
@@ -2,7 +2,7 @@
 
 echo 'cocos2d-iphone template installer'
 
-DST_DIR='/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Project Templates/Application/cocos2d-0.8.2-beta Application/'
+DST_DIR='/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Project Templates/Application/cocos2d-0.8.2-rc0 Application/'
 LIBS_DIR="$DST_DIR"libs
 
 echo 'Installing cocos2d template'
@@ -46,7 +46,7 @@ echo 'Installing cocos2d + box2d template'
 echo '-----------------------------------'
 echo ''
 
-DST_DIR='/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Project Templates/Application/cocos2d-0.8.2-beta Box2d Application/'
+DST_DIR='/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Project Templates/Application/cocos2d-0.8.2-rc0 Box2d Application/'
 LIBS_DIR="$DST_DIR"libs
 
 if [[ -d $DST_DIR ]];  then
@@ -77,5 +77,47 @@ cp -R external/TouchJSON "$LIBS_DIR"
 
 echo ...copying Box2D files
 cp -R external/Box2d/Box2D "$LIBS_DIR"
+
+echo done!
+
+
+echo ''
+echo ''
+echo ''
+echo 'Installing cocos2d + chipmunk template'
+echo '--------------------------------------'
+echo ''
+
+DST_DIR='/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Project Templates/Application/cocos2d-0.8.2-rc0 Chipmunk Application/'
+LIBS_DIR="$DST_DIR"libs
+
+if [[ -d $DST_DIR ]];  then
+    echo "template alredy installed"
+    exit 1
+fi
+
+echo ...creating destination directory: $DST_DIR
+mkdir -p "$DST_DIR"
+
+echo ...copying template files
+cp -R templates/cocos2d_chipmunk_app/ "$DST_DIR"
+
+echo ...copying cocos2d files
+cp -R cocos2d "$LIBS_DIR"
+
+echo ...copying cocos2d dependency files
+cp -R external/FontLabel "$LIBS_DIR"
+
+echo ...copying CocosDenshion files
+cp -R CocosDenshion "$LIBS_DIR"
+
+echo ...copying cocoslive files
+cp -R cocoslive "$LIBS_DIR"
+
+echo ...copying cocoslive dependency files
+cp -R external/TouchJSON "$LIBS_DIR"
+
+echo ...copying Chipmunk files
+cp -R external/Chipmunk "$LIBS_DIR"
 
 echo done!
