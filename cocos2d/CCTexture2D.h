f1797230d28c951d8bd4e90b0f0b7f0865ada084
Ricardo Quesada <ricardoquesada@gmail.com>
2010-02-28 19:51:10 +0000

    part of issue #697. removed unused code
    Former-commit-id: b4a827427a9fa4d0297602197390f0ddafccc4b3

--- cocos2d/CCTexture2D.h
+++ cocos2d/CCTexture2D.h
@@ -77,10 +77,7 @@ typedef enum {
 	//! 16-bit textures: RGBA4444
 	kTexture2DPixelFormat_RGBA4444,
 	//! 16-bit textures: RGB5A1
-	kTexture2DPixelFormat_RGB5A1,
-	
-	// You should never use this value. Used internally
-	kTexture2DPixelFormat_BGRA8888, 
+	kTexture2DPixelFormat_RGB5A1,	
 } Texture2DPixelFormat;
 
 /// Default pixel format: RGBA8888
