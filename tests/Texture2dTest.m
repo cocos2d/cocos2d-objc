3854b56cf037c54f75b97f84a2a6e348f68084fe
Ricardo Quesada <ricardoquesada@gmail.com>
2010-03-16 13:53:29 +0000

    texture2d sample uses libpng 1.4.1 API

--- tests/Texture2dTest.m
+++ tests/Texture2dTest.m
@@ -808,7 +808,7 @@ Class restartAction()
 		png_set_palette_to_rgb( png_ptr );
 	
 	if( color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8 )
-		png_set_gray_1_2_4_to_8( png_ptr );
+		png_set_expand_gray_1_2_4_to_8( png_ptr );
 	
 	if( png_get_valid( png_ptr, info_ptr, PNG_INFO_tRNS ) )
 		png_set_tRNS_to_alpha (png_ptr);
