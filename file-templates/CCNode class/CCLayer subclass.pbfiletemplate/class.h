8aa0b0fd274a13961a12fbed9a4c53af2fc3ff84
Stuart Carnie <stuart.carnie@gmail.com>
2010-03-31 23:39:59 -0700

    New install-templates.sh template installer adds templates to /Libraries shared area.  * Adds file-based templates, to ease creating classes that derive from CCNode

---
+++ file-templates/CCNode class/CCLayer subclass.pbfiletemplate/class.h
@@ -0,0 +1,16 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface «FILEBASENAMEASIDENTIFIER» : CCLayer {
+
+}
+
+@end
