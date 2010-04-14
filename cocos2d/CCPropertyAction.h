ff6aa135067fe28f8242c47ccc000eaf3bfd5423
Ricardo Quesada <ricardoquesada@gmail.com>
2010-04-14 15:56:31 -0400

    Added CCPropertyAction doxgen documentation
    Former-commit-id: 7aa6f5a907f4e428505c80163d667da4294fff1f

--- cocos2d/CCPropertyAction.h
+++ cocos2d/CCPropertyAction.h
@@ -15,6 +15,24 @@
 #import <Foundation/Foundation.h>
 #import "CCIntervalAction.h"
 
+/** CCPropertyAction
+ 
+ CCPropertyAction is an action that lets you update any property of an object.
+ For example, if you want to modify the "width" property of a target from 200 to 300 in 2 senconds, then:
+ 
+	id modifyWidth = [CCPropertyAction actionWithDuration:2 key:@"width" from:200 to:300];
+	[target runAction:modifyWidth];
+ 
+
+ Another example: CCScaleTo action could be rewriten using CCPropertyAction:
+ 
+	// scaleA and scaleB are equivalents
+	id scaleA = [CCScaleTo actionWithDuration:2 scale:3];
+	id scaleB = [CCPropertyAction actionWithDuration:2 key:@"scale" from:1 to:3];
+
+ 
+ @since v0.99.2
+ */
 @interface CCPropertyAction : CCIntervalAction {
 
 	NSString		*key_;
@@ -23,8 +41,10 @@
 	float			delta_;
 }
 
-+ (id)actionWithDuration:(ccTime)aDuration key:(NSString *)aKey from:(float)aFrom to:(float)aTo;
+/** creates an initializes the action with the property name (key), and the from and to parameters. */
++ (id)actionWithDuration:(ccTime)aDuration key:(NSString *)key from:(float)from to:(float)to;
 
-- (id)initWithDuration:(ccTime)aDuration key:(NSString *)aKey from:(float)aFrom to:(float)aTo;
+/** initializes the action with the property name (key), and the from and to parameters. */
+- (id)initWithDuration:(ccTime)aDuration key:(NSString *)key from:(float)from to:(float)to;
     
 @end
