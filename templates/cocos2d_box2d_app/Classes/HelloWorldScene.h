761c4c21ae3037821b90ebb2c6e0170c55e0e8d9
Ricardo Quesada <ricardoquesada@gmail.com>
2010-05-11 11:20:40 +0200

    Fixed issue #860
    Tempaltes uses uthash instead of ccHashSet.
    Added copyright Template to .m/.h files of the templates
    
    
    Former-commit-id: 73fbc3f226542bc2f8a670cb5d0925d67a00bddc

--- templates/cocos2d_box2d_app/Classes/HelloWorldScene.h
+++ templates/cocos2d_box2d_app/Classes/HelloWorldScene.h
@@ -1,3 +1,11 @@
+//
+//  HelloWorldScene.h
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
 
 // When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
