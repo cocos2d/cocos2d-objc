cdd88b6104bfd14672b549a1923f967aab97796f
Ricardo Quesada <ricardoquesada@gmail.com>
2010-05-18 14:09:48 +0200

    Improved doxygen documentation
    Former-commit-id: b012c2afefa889a5fb3600a3b395a8c599f7a0d9

--- cocos2d/CCDirector.h
+++ cocos2d/CCDirector.h
@@ -31,7 +31,7 @@
 // OpenGL related
 #import "Support/EAGLView.h"
 
-/** Possible Pixel Formats for the EAGLView */
+/** @typedef tPixelFormat Possible Pixel Formats for the EAGLView */
 typedef enum {
 	/** RGB565 pixel format. No alpha. 16-bit. (Default) */
 	kCCPixelFormatRGB565,
@@ -47,7 +47,7 @@ typedef enum {
 	kRGBA8 = kCCPixelFormatRGBA8888,
 } tPixelFormat;
 
-/** Possible DepthBuffer Formats for the EAGLView.
+/** @typedef tDepthBufferFormat Possible DepthBuffer Formats for the EAGLView.
  Use 16 or 24 bit depth buffers if you are going to use real 3D objects.
  */
 typedef enum {
@@ -63,7 +63,7 @@ typedef enum {
 	kDepthBuffer24 = kCCDepthBuffer24,
 } tDepthBufferFormat;
 
-/** Possible OpenGL projections used by director */
+/** @typedef ccDirectorProjection Possible OpenGL projections used by director */
 typedef enum {
 	/// sets a 2D projection (orthogonal projection)
 	kCCDirectorProjection2D,
@@ -84,7 +84,7 @@ typedef enum {
 
 } ccDirectorProjection;
 
-/** Possible Director Types.
+/** @typedef ccDirectorType Possible Director Types.
  @since v0.8.2
  */
 typedef enum {
@@ -139,7 +139,7 @@ typedef enum {
 
 } ccDirectorType;
 
-/** Possible device orientations */
+/** @typedef ccDeviceOrientation Possible device orientations */
 typedef enum {
 	/// Device oriented vertically, home button on the bottom
 	kCCDeviceOrientationPortrait = UIDeviceOrientationPortrait,	
