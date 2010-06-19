59db177f164398c3263436f99587f74c28d40dcb
Ricardo Quesada <ricardoquesada@gmail.com>
2010-06-19 10:14:51 +0200

    Updated bug886 test case
    Former-commit-id: cd03d9748bcc06fedd4108305043857f7d8040a8

--- tests/bugs/Bug-886.m
+++ tests/bugs/Bug-886.m
@@ -6,19 +6,28 @@
 #import "Bug-886.h"
 
 #pragma mark -
-#pragma mark MemBug
 
 @implementation Layer1
 -(id) init
 {
-	if((self=[super init])) {
-		CCSprite *background = [CCSprite spriteWithFile:@"bugs/bug886.jpg"];
-		background.anchorPoint = CGPointZero;
-		background.position = CGPointZero;
-		[self addChild:background];
+	if( (self=[super init] )) {
+		
+		// ask director the the window size
+		//		CGSize size = [[CCDirector sharedDirector] winSize];
+		
+		CCSprite* sprite = [CCSprite spriteWithFile:@"bugs/bug886.jpg"];
+		sprite.anchorPoint = CGPointZero;
+		sprite.position =  CGPointZero;
+		sprite.scaleX = 0.6f;
+		[self addChild: sprite];
+		
+		CCSprite* sprite2 = [CCSprite spriteWithFile:@"bugs/bug886.png"];
+		sprite2.anchorPoint = CGPointZero;
+		sprite2.scaleX = 0.6f;
+		sprite2.position =  ccp( [sprite contentSize].width * 0.6f + 10, 0 );
+		[self addChild: sprite2];
 		
 	}
-    
 	return self;
 }
 @end
@@ -52,6 +61,8 @@
 	// Turn on display FPS
 	[director setDisplayFPS:YES];
 	
+//	[CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA4444];
+
 	CCScene *scene = [CCScene node];	
 	[scene addChild:[Layer1 node] z:0];
 			
