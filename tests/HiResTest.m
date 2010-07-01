afaebe1370f551eda93f6cbd401d31079616c282
Ricardo Quesada <ricardoquesada@gmail.com>
2010-07-01 15:30:31 -0300

    Fixed issue #902. Director#convertToUI works as expected

---
+++ tests/HiResTest.m
@@ -0,0 +1,244 @@
+//
+// HiRes Test
+// a cocos2d example
+// http://www.cocos2d-iphone.org
+//
+
+// cocos import
+#import "cocos2d.h"
+
+// local import
+#import "HiResTest.h"
+
+static int sceneIdx=-1;
+static NSString *transitions[] = {	
+			@"Test1",
+};
+
+enum {
+	kTagSprite1,
+	kTagSprite2,
+	kTagSprite3,
+	kTagSprite4,
+	kTagSprite5,
+	kTagSprite6,
+	kTagSprite7,
+	kTagSprite8,
+};
+
+Class nextAction()
+{
+	
+	sceneIdx++;
+	sceneIdx = sceneIdx % ( sizeof(transitions) / sizeof(transitions[0]) );
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+Class backAction()
+{
+	sceneIdx--;
+	int total = ( sizeof(transitions) / sizeof(transitions[0]) );
+	if( sceneIdx < 0 )
+		sceneIdx += total;	
+	
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+Class restartAction()
+{
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+#pragma mark -
+#pragma mark SpriteDemo
+
+@implementation HiResDemo
+-(id) init
+{
+	if( (self = [super init]) ) {
+
+
+		CGSize s = [[CCDirector sharedDirector] winSize];
+			
+		CCLabel* label = [CCLabel labelWithString:[self title] fontName:@"Arial" fontSize:32];
+		[self addChild: label z:1];
+		[label setPosition: ccp(s.width/2, s.height-50)];
+
+		NSString *subtitle = [self subtitle];
+		if( subtitle ) {
+			CCLabel* l = [CCLabel labelWithString:subtitle fontName:@"Thonburi" fontSize:16];
+			[self addChild:l z:1];
+			[l setPosition:ccp(s.width/2, s.height-80)];
+		}
+		
+		
+		CCMenuItemImage *item1 = [CCMenuItemImage itemFromNormalImage:@"b1.png" selectedImage:@"b2.png" target:self selector:@selector(backCallback:)];
+		CCMenuItemImage *item2 = [CCMenuItemImage itemFromNormalImage:@"r1.png" selectedImage:@"r2.png" target:self selector:@selector(restartCallback:)];
+		CCMenuItemImage *item3 = [CCMenuItemImage itemFromNormalImage:@"f1.png" selectedImage:@"f2.png" target:self selector:@selector(nextCallback:)];
+		
+		CCMenu *menu = [CCMenu menuWithItems:item1, item2, item3, nil];
+		
+		menu.position = CGPointZero;
+		item1.position = ccp( s.width/2 - 100,30);
+		item2.position = ccp( s.width/2, 30);
+		item3.position = ccp( s.width/2 + 100,30);
+		[self addChild: menu z:1];	
+	}
+	return self;
+}
+
+-(void) dealloc
+{
+	[super dealloc];
+}
+
+-(void) restartCallback: (id) sender
+{
+	CCScene *s = [CCScene node];
+	[s addChild: [restartAction() node]];
+	[[CCDirector sharedDirector] replaceScene: s];
+}
+
+-(void) nextCallback: (id) sender
+{
+	CCScene *s = [CCScene node];
+	[s addChild: [nextAction() node]];
+	[[CCDirector sharedDirector] replaceScene: s];
+}
+
+-(void) backCallback: (id) sender
+{
+	CCScene *s = [CCScene node];
+	[s addChild: [backAction() node]];
+	[[CCDirector sharedDirector] replaceScene: s];
+}
+
+-(NSString*) title
+{
+	return @"No title";
+}
+
+-(NSString*) subtitle
+{
+	return nil;
+}
+@end
+
+#pragma mark -
+#pragma mark Example Test1
+
+
+@implementation Test1
+
+-(id) init
+{
+	if( (self=[super init]) ) {
+
+		CGSize size = [[CCDirector sharedDirector] winSize];
+
+		CCSprite *sprite = [CCSprite spriteWithFile:@"grossini.png"];
+		[self addChild:sprite];
+		
+		[sprite setPosition:ccp(size.width/2, size.width/2)];
+	}	
+	return self;
+}
+
+@end
+
+#pragma mark -
+#pragma mark AppDelegate
+
+// CLASS IMPLEMENTATIONS
+@implementation AppController
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+	// Init the window
+	window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+	// must be called before any othe call to the director
+	[CCDirector setDirectorType:kCCDirectorTypeDisplayLink];
+	
+	// before creating any layer, set the landscape mode
+	CCDirector *director = [CCDirector sharedDirector];
+	
+	// landscape orientation
+	[director setDeviceOrientation:kCCDeviceOrientationLandscapeLeft];
+	
+	// set FPS at 60
+	[director setAnimationInterval:1.0/60];
+	
+	// Display FPS: yes
+	[director setDisplayFPS:YES];
+
+	// Create an EAGLView with a RGB8 color buffer, and a depth buffer of 24-bits
+	EAGLView *glView = [EAGLView viewWithFrame:[window bounds]
+								   pixelFormat:kEAGLColorFormatRGBA8
+								   depthFormat:GL_DEPTH_COMPONENT24_OES
+							preserveBackbuffer:NO];
+	
+	// attach the openglView to the director
+	[director setOpenGLView:glView];
+	
+	// Hi-Res mode
+	[director setContentScaleFactor:2];	
+	
+	// make the OpenGLView a child of the main window
+	[window addSubview:glView];
+	
+	// make main window visible
+	[window makeKeyAndVisible];	
+	
+	// Default texture format for PNG/BMP/TIFF/JPEG/GIF images
+	// It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
+	// You can change anytime.
+	[CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];	
+	
+	// create the main scene
+	CCScene *scene = [CCScene node];
+	[scene addChild: [nextAction() node]];
+	
+	
+	// and run it!
+	[director runWithScene: scene];
+	
+	return YES;
+}
+
+// getting a call, pause the game
+-(void) applicationWillResignActive:(UIApplication *)application
+{
+	[[CCDirector sharedDirector] pause];
+}
+
+// call got rejected
+-(void) applicationDidBecomeActive:(UIApplication *)application
+{
+	[[CCDirector sharedDirector] resume];
+}
+
+// purge memory
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application
+{
+	[[CCDirector sharedDirector] purgeCachedData];
+}
+
+// next delta time will be zero
+-(void) applicationSignificantTimeChange:(UIApplication *)application
+{
+	[[CCDirector sharedDirector] setNextDeltaTimeZero:YES];
+}
+
+- (void) dealloc
+{
+	[window release];
+	[super dealloc];
+}
+@end
