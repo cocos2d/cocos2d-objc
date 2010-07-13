5d016fbbca71864be292cbbd3a16cdb2f5c89528
Ricardo Quesada <ricardoquesada@gmail.com>
2010-07-13 11:30:47 -0300

    Fixed issue #926
    ccCArrayAppendValueWithResize fixed signature
    
    
    Former-commit-id: 1ba580123226ba26662fe1726478901b158b28bb

--- tests/PerformanceTests/PerformanceParticleTest/AppController.m
+++ tests/PerformanceTests/PerformanceParticleTest/AppController.m
@@ -22,7 +22,7 @@
 	CCDirector *director = [CCDirector sharedDirector];
 	
 	// before creating any layer, set the landscape mode
-	[director setDeviceOrientation:kCCDeviceOrientationLandscapeLeft];
+//	[director setDeviceOrientation:kCCDeviceOrientationLandscapeLeft];
 	
 	// display FPS (useful when debugging)
 	[director setDisplayFPS:YES];
