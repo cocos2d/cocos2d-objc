003e49eb58bce4ecb17289121e3f4f2771a88092
Ricardo Quesada <ricardoquesada@gmail.com>
2010-07-16 12:11:55 -0300

    libpng udpated to v1.2.44
    Former-commit-id: c388fffec00c83873ebcfa6de6fc13d752315dd6

--- external/libpng/pngtest.c
+++ external/libpng/pngtest.c
@@ -1702,4 +1702,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_43 your_png_h_is_not_version_1_2_43;
+typedef version_1_2_44 your_png_h_is_not_version_1_2_44;
