ec35b1cd8d6d7f173943bb1dcd616c249908bcbc
Ricardo Quesada <ricardoquesada@gmail.com>
2010-08-10 12:38:53 -0300

    chipmunk: correct fix to make it compile on iPhone

--- external/Chipmunk/src/cpPolyShape.c
+++ external/Chipmunk/src/cpPolyShape.c
@@ -211,8 +211,8 @@ cpPolyShapeNew(cpBody *body, int numVerts, cpVect *verts, cpVect offset)
 cpPolyShape *
 cpBoxShapeInit(cpPolyShape *poly, cpBody *body, cpFloat width, cpFloat height)
 {
-	cpFloat hw = width/2.0f;
-	cpFloat hh = height/2.0f;
+	cpFloat hw = width/(cpFloat)2.0;
+	cpFloat hh = height/(cpFloat)2.0;
 	
 	cpVect verts[] = {
 		cpv(-hw,-hh),
