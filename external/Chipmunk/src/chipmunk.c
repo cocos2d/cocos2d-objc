bab4df00271cdb84ca695e28c7b641204cbaac8f
Ricardo Quesada <ricardoquesada@gmail.com>
2010-08-10 12:38:53 -0300

    chipmunk: correct fix to make it compile on iPhone
    Former-commit-id: ec35b1cd8d6d7f173943bb1dcd616c249908bcbc

--- external/Chipmunk/src/chipmunk.c
+++ external/Chipmunk/src/chipmunk.c
@@ -98,7 +98,7 @@ cpMomentForPoly(cpFloat m, const int numVerts, cpVect *verts, cpVect offset)
 cpFloat
 cpMomentForBox(cpFloat m, cpFloat width, cpFloat height)
 {
-	return m*(width*width + height*height)/12.0f;
+	return m*(width*width + height*height)/(cpFloat)12.0;
 }
 
 
