1d420e541e2334e239e77c32813fcad26d39c992
Ricardo Quesada <ricardoquesada@gmail.com>
2010-08-25 23:38:57 -0300

    MacPort: texture2d test ported.
    TextureCache Async loading works on Mac
    libpng ported to mac

--- tests/Texture2dTest.h
+++ tests/Texture2dTest.h
@@ -1,11 +1,26 @@
 #import "cocos2d.h"
 
 //CLASS INTERFACE
-@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate, UIApplicationDelegate>
+//CLASS INTERFACE
+#if __IPHONE_OS_VERSION_MIN_REQUIRED
+@interface AppController : NSObject <UIApplicationDelegate>
 {
-	UIWindow	*window;
+	UIWindow *window;
 }
 @end
+
+#elif __MAC_OS_X_VERSION_MIN_REQUIRED
+@interface cocos2dmacAppDelegate : NSObject <NSApplicationDelegate>
+{
+	NSWindow	*window_;
+	MacGLView	*glView_;
+}
+
+@property (assign) IBOutlet NSWindow	*window;
+@property (assign) IBOutlet MacGLView	*glView;
+
+@end
+#endif // Mac
 
 @interface TextureDemo : CCLayer
 {}
