2b9d5268ee792ea168657315fc030fa5d804bdac
Ricardo Quesada <ricardoquesada@gmail.com>
2010-11-04 11:47:35 -0300

    PVRTexture: doesn't use NSData. Improved performance

--- cocos2d/CCTexturePVR.h
+++ cocos2d/CCTexturePVR.h
@@ -53,19 +53,21 @@ Copyright (C) 2008 Apple Inc. All Rights Reserved.
 #import "ccCArray.h"
 
 #pragma mark -
-#pragma mark CCTextureCache PVR extension
-
-#pragma mark -
-#pragma mark CCTexture2D PVR extension
-
-
-#pragma mark -
 #pragma mark CCTexturePVR
+
+struct CCPVRMipmap {
+	unsigned char *address;
+	unsigned int len;
+};
+
+enum {
+	CC_PVRMIPMAP_MAX = 16,
+};
 
 @interface CCTexturePVR : NSObject
 {
-	ccArray *imageData_;
-	
+	struct CCPVRMipmap	mipmaps_[CC_PVRMIPMAP_MAX];	// pointer to mipmap images
+	int		numberOfMipmaps_;					// number of mipmap used
 	
 	unsigned int	tableFormatIndex_;
 	uint32_t width_, height_;
