e91a9fe5003e96a8614e36c2cab84d196866b2c5
Ricardo Quesada <ricardoquesada@gmail.com>
2010-11-09 09:42:04 -0300

    TARGET_SIMULATOR fixes
    TARGET_SIMULATOR is defined on SDK 4.2, so we should check if
    it is 1, and not if it is defined.
    Perhaps this was the cause of the small slowdown of v0.99.5
    
    
    Former-commit-id: 150b17189c54e18bd55405c6cf2d87b26ede787f

--- cocos2d/CCSprite.m
+++ cocos2d/CCSprite.m
@@ -584,7 +584,7 @@ struct transformValues_ {
 #pragma mark CCSprite - draw
 
 -(void) draw
-{	
+{
 	NSAssert(!usesBatchNode_, @"If CCSprite is being rendered by CCSpriteBatchNode, CCSprite#draw SHOULD NOT be called");
 
 	// Default GL states: GL_TEXTURE_2D, GL_VERTEX_ARRAY, GL_COLOR_ARRAY, GL_TEXTURE_COORD_ARRAY
