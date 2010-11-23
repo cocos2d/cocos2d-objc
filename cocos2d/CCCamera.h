c96a337475e9186b4dbd09e0071a3502bb47b132
Ricardo Quesada <ricardoquesada@gmail.com>
2010-11-23 15:54:26 -0300

    Camera: Updated doxygen strings

--- cocos2d/CCCamera.h
+++ cocos2d/CCCamera.h
@@ -27,7 +27,7 @@
 #import "CCNode.h"
 
 /** 
-    A Camera is used in every CocosNode.
+    A CCCamera is used in every CCNode.
     Useful to look at the object from different views.
     The OpenGL gluLookAt() function is used to locate the
     camera.
@@ -42,6 +42,11 @@
  
      - Some nodes, like CCParallaxNode, CCParticle uses world node coordinates, and they won't work properly if you move them (or any of their ancestors)
        using the camera.
+     
+     - It doesn't work on batched nodes like CCSprite objects when they are parented to a CCSpriteBatchNode object.
+ 
+	 - It is recommended to use it ONLY if you are going to create 3D effects. For 2D effecs, use the action CCFollow or position/scale/rotate.
+ 
 */
 
 @interface CCCamera : NSObject {
@@ -70,16 +75,16 @@
 -(void) restore;
 /** Sets the camera using gluLookAt using its eye, center and up_vector */
 -(void) locate;
-/** sets the eye values */
+/** sets the eye values in points */
 -(void) setEyeX: (float)x eyeY:(float)y eyeZ:(float)z;
-/** sets the center values */
+/** sets the center values in points */
 -(void) setCenterX: (float)x centerY:(float)y centerZ:(float)z;
 /** sets the up values */
 -(void) setUpX: (float)x upY:(float)y upZ:(float)z;
 
-/** get the eye vector values */
+/** get the eye vector values in points */
 -(void) eyeX:(float*)x eyeY:(float*)y eyeZ:(float*)z;
-/** get the center vector values */
+/** get the center vector values in points */
 -(void) centerX:(float*)x centerY:(float*)y centerZ:(float*)z;
 /** get the up vector values */
 -(void) upX:(float*)x upY:(float*)y upZ:(float*)z;
