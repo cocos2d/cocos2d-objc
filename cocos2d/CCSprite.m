b0f54b80b46eeb8cee30718f0875e7d2e89052e3
Ricardo Quesada <ricardoquesada@gmail.com>
2010-12-06 14:45:34 -0300

    Fixed issue #1055
    CCSprite: correctly overrides setIsRelativeAnchorPoint

--- cocos2d/CCSprite.m
+++ cocos2d/CCSprite.m
@@ -778,7 +778,7 @@ struct transformValues_ {
 	SET_DIRTY_RECURSIVELY();
 }
 
--(void)setRelativeAnchorPoint:(BOOL)relative
+-(void)setIsRelativeAnchorPoint:(BOOL)relative
 {
 	NSAssert( ! usesBatchNode_, @"relativeTransformAnchor is invalid in CCSprite");
 	[super setIsRelativeAnchorPoint:relative];
