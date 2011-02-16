537a5c7e9f6b8da4bc87acbf45a9fc7267ca536c
Ricardo Quesada <ricardoquesada@gmail.com>
2011-02-16 12:17:46 -0300

    Particles: consume less memory & are faster
    Use C4B instead of C4F

--- cocos2d/CCParticleSystemPoint.m
+++ cocos2d/CCParticleSystemPoint.m
@@ -81,7 +81,8 @@
 	// place vertices and colos in array
 	vertices[particleIdx].pos = (ccVertex2F) {newPos.x, newPos.y};
 	vertices[particleIdx].size = p->size;
-	vertices[particleIdx].colors = p->color;
+	ccColor4B color =  { p->color.r*255, p->color.g*255, p->color.b*255, p->color.a*255 };
+	vertices[particleIdx].color = color;
 }
 
 -(void) postStep
@@ -115,7 +116,7 @@
 
 	glVertexPointer(2,GL_FLOAT, kPointSize, 0);
 
-	glColorPointer(4, GL_FLOAT, kPointSize, (GLvoid*) offsetof(ccPointSprite, colors) );
+	glColorPointer(4, GL_UNSIGNED_BYTE, kPointSize, (GLvoid*) offsetof(ccPointSprite, color) );
 
 	glEnableClientState(GL_POINT_SIZE_ARRAY_OES);
 	glPointSizePointerOES(GL_FLOAT, kPointSize, (GLvoid*) offsetof(ccPointSprite, size) );
@@ -123,8 +124,8 @@
 	int offset = (int)vertices;
 	glVertexPointer(2,GL_FLOAT, kPointSize, (GLvoid*) offset);
 	
-	int diff = offsetof(ccPointSprite, colors);
-	glColorPointer(4, GL_FLOAT, kPointSize, (GLvoid*) (offset+diff));
+	int diff = offsetof(ccPointSprite, color);
+	glColorPointer(4, GL_UNSIGNED_BYTE, kPointSize, (GLvoid*) (offset+diff));
 	
 	glEnableClientState(GL_POINT_SIZE_ARRAY_OES);
 	diff = offsetof(ccPointSprite, size);
