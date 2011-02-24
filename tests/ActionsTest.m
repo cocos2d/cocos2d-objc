1aa538760adb8bf765a5996ff2b5a4e97c207d2d
Ricardo Quesada <ricardoquesada@gmail.com>
2011-02-24 10:11:43 -0300

    Improved ActionsTest: CCBlink has more tests
    Former-commit-id: 156d1bd7f45dc255073daf1c9ecd1c953e0ce8c0

--- tests/ActionsTest.m
+++ tests/ActionsTest.m
@@ -14,6 +14,7 @@ enum {
 
 static int sceneIdx=-1;
 static NSString *transitions[] = {
+	@"ActionBlink",
 	
 	@"ActionManual",
 	@"ActionMove",
@@ -386,13 +387,15 @@ Class restartAction()
 {
 	[super onEnter];
 	
-	[self centerSprites:2];
+	[self centerSprites:3];
 	
-	id action1 = [CCBlink actionWithDuration:2 blinks:10];
-	id action2 = [CCBlink actionWithDuration:2 blinks:5];
+	id action1 = [CCBlink actionWithDuration:3 blinks:10];
+	id action2 = [CCBlink actionWithDuration:3 blinks:5];
+	id action3 = [CCBlink actionWithDuration:0.5f blinks:5];
 	
 	[tamara runAction: action1];
 	[kathia runAction:action2];
+	[grossini runAction:action3];
 }
 -(NSString *) title
 {
