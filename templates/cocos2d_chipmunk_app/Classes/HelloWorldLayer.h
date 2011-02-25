cdce7e7be1a60089868b2d4f1cdc5b7da5f9cff1
Ricardo Quesada <ricardoquesada@gmail.com>
2011-02-25 01:01:02 -0300

    Issue #873
    Templates are more user friendly. Thanks Paul Hampson

--- templates/cocos2d_chipmunk_app/Classes/HelloWorldLayer.h
+++ templates/cocos2d_chipmunk_app/Classes/HelloWorldLayer.h
@@ -1,10 +1,11 @@
 //
-//  HelloWorldScene.m
+//  HelloWorldLayer.h
 //  ___PROJECTNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
 //
+
 
 // When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
@@ -12,14 +13,14 @@
 // Importing Chipmunk headers
 #import "chipmunk.h"
 
-// HelloWorld Layer
-@interface HelloWorld : CCLayer
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
 {
 	cpSpace *space;
 }
 
-// returns a Scene that contains the HelloWorld as the only child
-+(id) scene;
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
 -(void) step: (ccTime) dt;
 -(void) addNewSpriteX:(float)x y:(float)y;
 
