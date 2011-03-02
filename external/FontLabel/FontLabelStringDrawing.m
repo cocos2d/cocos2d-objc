90d18b08567cd227af851380acd759e08e9d01a4
Ricardo Quesada <ricardoquesada@gmail.com>
2011-03-02 18:17:39 -0300

    Fixed issue #1124

--- external/FontLabel/FontLabelStringDrawing.m
+++ external/FontLabel/FontLabelStringDrawing.m
@@ -456,7 +456,7 @@ static CGSize drawOrSizeTextConstrainedToSize(BOOL performDraw, NSString *string
 	READ_GLYPHS();
 	
 	NSMutableCharacterSet *alphaCharset = [NSMutableCharacterSet alphanumericCharacterSet];
-    [alphaCharset addCharactersInString:@"'\u2019\u02BC"]; // apostrophes
+	[alphaCharset addCharactersInString:@"([{'\"\u2019\u02BC"];
 	
 	// scan left-to-right looking for newlines or until we hit the width constraint
 	// When we hit a wrapping point, calculate truncation as follows:
