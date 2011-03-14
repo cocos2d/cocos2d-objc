45b934d1c396b285f423413b15b3873ca511dead
Ricardo Quesada <ricardoquesada@gmail.com>
2011-03-14 14:24:11 -0300

    Improved generator: creates groups dinamically

--- tools/template_generator.py
+++ tools/template_generator.py
@@ -37,11 +37,13 @@ import getopt
 import glob
 
 class Xcode4Template(object):
-    def __init__( self, directory, group='cocos2d' ):
+    def __init__( self, directory, group=None):
         self.directory = directory
         self.files_to_include = []
         self.wildcard = '*'
-        self.group = group
+        self.ignore_extensions = ['h','txt']
+        self.group = group                  # fixed group name
+        self.group_index = 1                # automatic group name taken from path
         self.output = []
 
     def scandirs(self, path):
@@ -51,22 +53,60 @@ class Xcode4Template(object):
             else:
                 self.files_to_include.append( currentFile )
 
+    #
+    # Generate the "Definitions" section
+    #
     def generate_definitions( self ):
         output_header = "<key>Definitions</key>"
         output_dict_open = "<dict>"
         output_dict_close = "</dict>"
 
         output_body = []
-        for file in self.files_to_include:
-#            filename = os.path.basename(file)
-            filename = file
-            output_body.append("  <key>%s</key>" % filename )
-            output_body.append("""  <dict>
+        for path in self.files_to_include:
+
+            # group name
+            group = None
+            if self.group is not None:
+                group = self.group
+            else:
+                # obtain group name from directory
+                dirs = os.path.dirname(path)
+                subdirs = dirs.split('/')
+                if self.group_index < len(subdirs):
+                    group = subdirs[self.group_index]
+                else:
+                    # error
+                    group = 'Unknown Group'
+
+            # get the extension
+            filename = os.path.basename(path)
+            name_extension= filename.split('.')
+            extension = None
+            if len(name_extension) == 2:
+                extension = name_extension[1]
+
+            output_body.append("  <key>%s</key>" % path )
+
+            # if extension is in array of ignored extesions,
+            # then add it to the "TargetIndices" == empty group
+            if extension in self.ignore_extensions:
+                output_body.append("""  <dict>
     <key>Group</key>
     <string>%s</string>
     <key>Path</key>
     <string>%s</string>
-  </dict>""" % (self.group, file) )
+    <key>TargetIndices</key>
+    <array/>
+  </dict>""" % (group, path) )
+
+            # Add it to the correct TargetIndices
+            else:
+                output_body.append("""  <dict>
+    <key>Group</key>
+    <string>%s</string>
+    <key>Path</key>
+    <string>%s</string>
+  </dict>""" % (group, path) )
 
 
         self.output.append( output_header )
@@ -74,22 +114,26 @@ class Xcode4Template(object):
         self.output.append( "\n".join( output_body ) )
         self.output.append( output_dict_close )
 
+    # 
+    # Generates the "Nodes" section
+    #
     def generate_nodes( self ):
         output_header = "<key>Nodes</key>"
         output_open = "<array>"
         output_close = "</array>"
 
         output_body = []
-        for file in self.files_to_include:
-#            filename = os.path.basename(file)
-            filename = file
-            output_body.append("  <string>%s</string>" % filename )
+        for path in self.files_to_include:
+            output_body.append("  <string>%s</string>" % path )
 
         self.output.append( output_header )
         self.output.append( output_open )
         self.output.append( "\n".join( output_body ) )
         self.output.append( output_close )
-       
+      
+    #
+    # Generates the plist. Send it to to stdout
+    #
     def generate_xml( self ):
         self.output.append( _template_open_body )
         self.generate_definitions()
@@ -116,7 +160,7 @@ if __name__ == "__main__":
         help()
 
     directory = None
-    group = 'cocos2d'
+    group = None
     argv = sys.argv[1:]
     try:                                
         opts, args = getopt.getopt(argv, "d:g:", ["directory=","group="])
