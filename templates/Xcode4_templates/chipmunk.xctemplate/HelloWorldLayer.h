972f697a85da0cc0bbdc629e4b08f6614c1fd7f4
Ricardo Quesada <ricardoquesada@gmail.com>
2011-03-16 20:04:22 -0300

    Xcode 4 template: uses template names

--- templates/Xcode4_templates/chipmunk.xctemplate/HelloWorldLayer.h
+++ templates/Xcode4_templates/chipmunk.xctemplate/HelloWorldLayer.h
@@ -1,27 +1,21 @@
 //
 //  HelloWorldLayer.h
-//  c1cc
+//  ___PROJECTNAME___
 //
-//  Created by Ricardo Quesada on 3/16/11.
-//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
 //
 
 
 // When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
 
-// Importing Chipmunk headers
-#import "chipmunk.h"
-
 // HelloWorldLayer
 @interface HelloWorldLayer : CCLayer
 {
-	cpSpace *space;
 }
 
 // returns a CCScene that contains the HelloWorldLayer as the only child
 +(CCScene *) scene;
--(void) step: (ccTime) dt;
--(void) addNewSpriteX:(float)x y:(float)y;
 
 @end
