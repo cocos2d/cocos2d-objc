9fb81ac10102b44ca03e4541142b31e9290410a4
Ricardo Quesada <ricardoquesada@gmail.com>
2011-03-17 14:28:02 -0300

    Fixed issue #1127
    Fixed documentation in drawing primitives
    
    Former-commit-id: 49e420ca136dc0c5d8c5350e15c60752d91c9f0b

--- cocos2d/CCDrawingPrimitives.h
+++ cocos2d/CCDrawingPrimitives.h
@@ -49,7 +49,7 @@ extern "C" {
   - ccDrawCubicBezier
 
  You can change the color, width and other property by calling the
-   glColor4ub(), glLineWitdh(), glPointSize().
+   glColor4ub(), glLineWidth(), glPointSize().
  
  @warning These functions draws the Line, Point, Polygon, immediately. They aren't batched. If you are going to make a game that depends on these primitives, I suggest creating a batch.
  */
