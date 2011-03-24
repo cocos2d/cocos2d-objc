8910e074fd34f735e51a150cf74e02f7fdc9da74
Ricardo Quesada <ricardoquesada@gmail.com>
2011-03-24 08:56:55 -0300

    Templates Xcode4: Added Box2d & Chipmunk Mac OS X templates

---
+++ templates/Xcode4_templates/cocos2d_macosx_chipmunk.xctemplate/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import "cocos2d.h"
+
+@interface ___PROJECTNAMEASIDENTIFIER___AppDelegate : NSObject <NSApplicationDelegate>
+{
+	NSWindow	*window_;
+	MacGLView	*glView_;
+}
+
+@property (assign) IBOutlet NSWindow	*window;
+@property (assign) IBOutlet MacGLView	*glView;
+
+- (IBAction)toggleFullScreen:(id)sender;
+
+@end
