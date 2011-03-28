95060a7f73d6830a7b62e88a3bb6f03e93643ae7
Ricardo Quesada <ricardoquesada@gmail.com>
2011-03-27 21:00:11 -0300

    Mac OS x tests fixed

--- tests/Texture2dTest.m
+++ tests/Texture2dTest.m
@@ -1759,6 +1759,9 @@ Class restartAction()
 	CCDirectorMac *director = (CCDirectorMac*) [CCDirector sharedDirector];
 	[director setResizeMode:kCCDirectorResize_AutoScale];	
 	
+	// Turn on display FPS
+	[director setDisplayFPS:YES];
+
 	CCScene *scene = [CCScene node];
 	[scene addChild: [nextAction() node]];
 	
