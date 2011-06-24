6fb13f8aaa7dc5d1559710db18e9f40c8abc6780
Tommaso <tommaso.piazza@gmail.com>
2011-06-24 17:05:42 +0800

    Added trigger mode selection functionality to CCMenu. Default trigger mode is onRelease, alternate trigger mode is onPress. In onPress trigger mode an item's active method is called on ccTouchBegan.

--- cocos2d/CCMenu.m
+++ cocos2d/CCMenu.m
@@ -46,7 +46,7 @@ enum {
 
 @implementation CCMenu
 
-@synthesize opacity = opacity_, color = color_;
+@synthesize opacity = opacity_, color = color_, triggerMode = triggerMode_;
 
 - (id) init
 {
@@ -110,6 +110,7 @@ enum {
 		
 		selectedItem_ = nil;
 		state_ = kCCMenuStateWaiting;
+        triggerMode_ = kCCMenuTriggerModeOnRelease;
 	}
 	
 	return self;
@@ -179,6 +180,10 @@ enum {
 	
 	if( selectedItem_ ) {
 		state_ = kCCMenuStateTrackingTouch;
+        
+        if(triggerMode_ == kCCMenuTriggerModeOnPress)
+            [selectedItem_ activate];
+
 		return YES;
 	}
 	return NO;
@@ -189,7 +194,9 @@ enum {
 	NSAssert(state_ == kCCMenuStateTrackingTouch, @"[Menu ccTouchEnded] -- invalid state");
 	
 	[selectedItem_ unselected];
-	[selectedItem_ activate];
+    
+    if(triggerMode_ == kCCMenuTriggerModeOnRelease)
+        [selectedItem_ activate];
 	
 	state_ = kCCMenuStateWaiting;
 }
