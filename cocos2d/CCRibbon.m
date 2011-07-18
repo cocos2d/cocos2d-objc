d9e1c6b291d1fe5913ef2602bf34bda76c7d9781
Wenqi Chen <wenqi.chen@gmx.com>
2011-07-18 01:07:01 +0100

    - make uinteger cross-platform

--- cocos2d/CCRibbon.m
+++ cocos2d/CCRibbon.m
@@ -347,7 +347,7 @@
 		{
 			// generate alpha/color for each point
 			glEnableClientState(GL_COLOR_ARRAY);
-			uint i = begin;
+			NSUInteger i = begin;
 			for (; i < end; ++i)
 			{
 				int idx = i*8;
