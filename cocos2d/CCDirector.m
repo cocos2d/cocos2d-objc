ac08149df879ecb16eb02bb0681601f9ac627567
Maarten Billemont <lhunath@lyndir.com>
2011-08-20 11:57:54 +0200

    Fix: don't use depth testing when no depth buffer will be used.

--- cocos2d/CCDirector.m
+++ cocos2d/CCDirector.m
@@ -202,7 +202,7 @@ static CCDirector *_sharedDirector = nil;
 	NSAssert( view_, @"view_ must be initialized");
 
 	[self setAlphaBlending: YES];
-	[self setDepthTest: YES];
+	[self setDepthTest: openGLView_.depthFormat];
 	[self setProjection: projection_];
 
 	// set other opengl default values
