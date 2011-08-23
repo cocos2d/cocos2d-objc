56887beffffea98ad79097167cb86e89ba41cc5c
Ricardo Quesada <ricardoquesada@gmail.com>
2011-08-22 21:22:42 -0300

    Templates: Shaders are placed in group, not folder Shader
    Former-commit-id: 1ba7122396388497ce06c556b54db6d9d184c067

--- tests/Box2DTestBed-Cocos2dIntegration/GLES-Render.mm
+++ tests/Box2DTestBed-Cocos2dIntegration/GLES-Render.mm
@@ -46,8 +46,8 @@ GLESDebugDraw::GLESDebugDraw( float32 ratio )
 
 void GLESDebugDraw::initShader( void )
 {
-	mShaderProgram = [[GLProgram alloc] initWithVertexShaderFilename:@"Shaders/Position1Color.vsh"
-											  fragmentShaderFilename:@"Shaders/Position1Color.fsh"];
+	mShaderProgram = [[GLProgram alloc] initWithVertexShaderFilename:@"Position1Color.vsh"
+											  fragmentShaderFilename:@"Position1Color.fsh"];
 	
 	[mShaderProgram addAttribute:@"aVertex" index:kCCAttribPosition];
 	
