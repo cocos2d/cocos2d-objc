c6e3f10f4892ddea83805d8c5a0e755728b70b5d
Ricardo Quesada <ricardoquesada@gmail.com>
2011-10-20 07:41:35 -0700

    Restore old code. For the moment Batch Particle are not supported
    Former-commit-id: caad384d1b052a7ad6ab751d5e66571cd2d60a02

--- cocos2d/CCParticleSystemQuad.h
+++ cocos2d/CCParticleSystemQuad.h
@@ -44,29 +44,15 @@
   - On 3rd gen iPhone and iPads: It is MUCH faster than CCParticleSystemPoint
   - It consumes more RAM and more GPU memory than CCParticleSystemPoint
   - It supports subrects
-  - It supports batched rendering since 1.1
  @since v0.8
  */
 @interface CCParticleSystemQuad : CCParticleSystem
 {
-	ccV3F_C4B_T2F_Quad	*quads_;		// quads to be rendered
+	ccV2F_C4B_T2F_Quad	*quads_;		// quads to be rendered
 	GLushort			*indices_;		// indices
 	GLuint				VAOname_;
 	GLuint				buffersVBO_[2]; //0: vertex  1: indices
-	CGRect				textureRect_;
 }
-
-@property (nonatomic, readwrite) ccV3F_C4B_T2F_Quad* quads;
-
-/** create system with properties from plist, batchnode and rect on the sprite sheet 
-   use nil for batchNode to not use batch rendering 
-   if rect is (0.0f,0.0f,0.0f,0.0f) the whole texture width and height will be used
-*/ 
-+(id) particleWithFile:(NSString*) plistFile batchNode:(CCParticleBatchNode*) batchNode rect:(CGRect) rect;
-
--(id) initWithFile:(NSString *)plistFile batchNode:(CCParticleBatchNode*) batchNode rect:(CGRect) rect;
-
--(id) initWithTotalParticles:(NSUInteger)numberOfParticles batchNode:(CCParticleBatchNode*) batchNode rect:(CGRect) rect;
 
 /** initialices the indices for the vertices */
 -(void) initIndices;
@@ -76,10 +62,9 @@
 
 /** Sets a new CCSpriteFrame as particle.
  WARNING: this method is experimental. Use setTexture:withRect instead.
- uses the texture and the rect of the spriteframe to call setTexture:Rect:
  @since v0.99.4
  */
--(void) setDisplayFrame:(CCSpriteFrame*)spriteFrame;
+-(void)setDisplayFrame:(CCSpriteFrame*)spriteFrame;
 
 /** Sets a new texture with a rect. The rect is in Points.
  @since v0.99.4
@@ -87,3 +72,4 @@
 -(void) setTexture:(CCTexture2D *)texture withRect:(CGRect)rect;
 
 @end
+
--- cocos2d/CCParticleSystemQuad.m
+++ cocos2d/CCParticleSystemQuad.m
@@ -36,11 +36,10 @@
 #import "CCTextureCache.h"
 #import "ccMacros.h"
 #import "CCSpriteFrame.h"
+#import "CCDirector.h"
 #import "CCShaderCache.h"
 #import "ccGLState.h"
 #import "GLProgram.h"
-#import "CCParticleBatchNode.h"
-#import "CCTextureAtlas.h"
 
 // support
 #import "Support/OpenGL_Internal.h"
@@ -56,73 +55,35 @@
 
 @implementation CCParticleSystemQuad
 
-@synthesize quads=quads_;
-
-+(id) particleWithFile:(NSString*) plistFile batchNode:(CCParticleBatchNode*) batchNode rect:(CGRect) rect
-{
-	return [[[self alloc] initWithFile:plistFile batchNode:batchNode rect:rect] autorelease];
-}
-
--(id) initWithFile:(NSString *)plistFile  batchNode:(CCParticleBatchNode*) batchNode rect:(CGRect) rect
-{
-	batchNode_ = batchNode;
-	textureRect_ = rect; 
-	return [super initWithFile:plistFile];
-}
-
+// overriding the init method
 -(id) initWithTotalParticles:(NSUInteger) numberOfParticles
 {
-	return [self initWithTotalParticles:numberOfParticles batchNode:nil rect:CGRectZero];
-}
-
-// overriding the init method, this is the base initializer
--(id) initWithTotalParticles:(NSUInteger)numberOfParticles batchNode:(CCParticleBatchNode*) batchNode rect:(CGRect) rect
-{
-	batchNode_ = batchNode; 
-	textureRect_ = rect;
+	// base initialization
+	if( (self=[super initWithTotalParticles:numberOfParticles]) ) {
 	
-	//first super then self 
-	if( (self=[super initWithTotalParticles:numberOfParticles]) )
-	{
-		if( ! batchNode ) {
-			quads_ = calloc( sizeof(quads_[0]) * totalParticles, 1 );
-			indices_ = calloc( sizeof(indices_[0]) * totalParticles * 6, 1 );
+		// allocating data space
+		quads_ = calloc( sizeof(quads_[0]) * totalParticles, 1 );
+		indices_ = calloc( sizeof(indices_[0]) * totalParticles * 6, 1 );
+		
+		if( !quads_ || !indices_) {
+			NSLog(@"cocos2d: Particle system: not enough memory");
+			if( quads_ )
+				free( quads_ );
+			if(indices_)
+				free(indices_);
 			
-			if( !quads_ || !indices_) {
-				NSLog(@"cocos2d: Particle system: not enough memory");
-				if( quads_ )
-					free( quads_ );
-				if(indices_)
-					free(indices_);
-				
-				[self release];
-				return nil;
-			}
-
-			// initialize only once the texCoords and the indices
-			[self initTexCoordsWithRect:CGRectMake(0, 0, [texture_ pixelsWide], [texture_ pixelsHigh])];
-			[self initIndices];
-			[self initVAO];
-			
-			self.shaderProgram = [[CCShaderCache sharedShaderCache] programForKey:kCCShader_PositionTextureColor];
-
+			[self release];
+			return nil;
 		}
-		else
-		{
-			quads_ = NULL;
-			indices_ = NULL;
-
-			texture_ = [batchNode.textureAtlas.texture retain];
-			textureRect_ = rect; 
-
-			// batchNode
-			if (rect.size.width == 0.f && rect.size.height == 0.f )
-				rect.size = CGSizeMake([batchNode.textureAtlas.texture pixelsWide], [batchNode.textureAtlas.texture pixelsHigh]);
-	
-			[self setBatchNode: batchNode];
-		}
+		
+		// initialize only once the texCoords and the indices
+		[self initTexCoordsWithRect:CGRectMake(0, 0, [texture_ pixelsWide], [texture_ pixelsHigh])];
+		[self initIndices];
+		[self initVAO];
+		
+		self.shaderProgram = [[CCShaderCache sharedShaderCache] programForKey:kCCShader_PositionTextureColor];
 	}
-
+		
 	return self;
 }
 
@@ -162,15 +123,11 @@
 
 -(void) dealloc
 {
-	if( quads_)
-		free(quads_);
-	if( indices_)
-		free(indices_);
-
-	if( ! batchNode_ ) {
-		glDeleteBuffers(2, &buffersVBO_[0]);
-		glDeleteVertexArrays(1, &VAOname_);
-	}
+	free(quads_);
+	free(indices_);
+	
+	glDeleteBuffers(2, &buffersVBO_[0]);
+	glDeleteVertexArrays(1, &VAOname_);
 	
 	[super dealloc];
 }
@@ -180,71 +137,44 @@
 {
     // convert to Tex coords
 	
-	textureRect_ = pointRect;
-	
-	if (texture_)
-	{
-		// convert to pixels coords
-		CGRect rect = CGRectMake(
-								 pointRect.origin.x * CC_CONTENT_SCALE_FACTOR(),
-								 pointRect.origin.y * CC_CONTENT_SCALE_FACTOR(),
-								 pointRect.size.width * CC_CONTENT_SCALE_FACTOR(),
-								 pointRect.size.height * CC_CONTENT_SCALE_FACTOR() );
+	CGRect rect = CGRectMake(
+							 pointRect.origin.x * CC_CONTENT_SCALE_FACTOR(),
+							 pointRect.origin.y * CC_CONTENT_SCALE_FACTOR(),
+							 pointRect.size.width * CC_CONTENT_SCALE_FACTOR(),
+							 pointRect.size.height * CC_CONTENT_SCALE_FACTOR() );
 
-		GLfloat wide = [texture_ pixelsWide];
-		GLfloat high = [texture_ pixelsHigh];
-		
+	GLfloat wide = [texture_ pixelsWide];
+	GLfloat high = [texture_ pixelsHigh];
+
 #if CC_FIX_ARTIFACTS_BY_STRECHING_TEXEL
-		GLfloat left = (rect.origin.x*2+1) / (wide*2);
-		GLfloat bottom = (rect.origin.y*2+1) / (high*2);
-		GLfloat right = left + (rect.size.width*2-2) / (wide*2);
-		GLfloat top = bottom + (rect.size.height*2-2) / (high*2);
+	GLfloat left = (rect.origin.x*2+1) / (wide*2);
+	GLfloat bottom = (rect.origin.y*2+1) / (high*2);
+	GLfloat right = left + (rect.size.width*2-2) / (wide*2);
+	GLfloat top = bottom + (rect.size.height*2-2) / (high*2);
 #else
-		GLfloat left = rect.origin.x / wide;
-		GLfloat bottom = rect.origin.y / high;
-		GLfloat right = left + rect.size.width / wide;
-		GLfloat top = bottom + rect.size.height / high;
+	GLfloat left = rect.origin.x / wide;
+	GLfloat bottom = rect.origin.y / high;
+	GLfloat right = left + rect.size.width / wide;
+	GLfloat top = bottom + rect.size.height / high;
 #endif // ! CC_FIX_ARTIFACTS_BY_STRECHING_TEXEL
 	
-		// Important. Texture in cocos2d are inverted, so the Y component should be inverted
-		CC_SWAP( top, bottom);
-		
-		ccV3F_C4B_T2F_Quad *quadCollection; 
-		NSUInteger start, end; 
-		if (batchNode_)
-		{
-			quadCollection = [[batchNode_ textureAtlas] quads]; 
-			start = atlasIndex_; 
-			end = atlasIndex_ + totalParticles; 
-		}
-		else 
-		{
-			quadCollection = quads_; 
-			start = 0; 
-			end = totalParticles; 
-		}
-
-		for(NSUInteger i=start; i<end; i++) {
-			// bottom-left vertex:
-			quadCollection[i].bl.texCoords.u = left;
-			quadCollection[i].bl.texCoords.v = bottom;
-			// bottom-right vertex:
-			quadCollection[i].br.texCoords.u = right;
-			quadCollection[i].br.texCoords.v = bottom;
-			// top-left vertex:
-			quadCollection[i].tl.texCoords.u = left;
-			quadCollection[i].tl.texCoords.v = top;
-			// top-right vertex:
-			quadCollection[i].tr.texCoords.u = right;
-			quadCollection[i].tr.texCoords.v = top;
-		}
+	// Important. Texture in cocos2d are inverted, so the Y component should be inverted
+	CC_SWAP( top, bottom);
+	
+	for(NSUInteger i = 0; i < totalParticles; i++) {
+		// bottom-left vertex:
+		quads_[i].bl.texCoords.u = left;
+		quads_[i].bl.texCoords.v = bottom;
+		// bottom-right vertex:
+		quads_[i].br.texCoords.u = right;
+		quads_[i].br.texCoords.v = bottom;
+		// top-left vertex:
+		quads_[i].tl.texCoords.u = left;
+		quads_[i].tl.texCoords.v = top;
+		// top-right vertex:
+		quads_[i].tr.texCoords.u = right;
+		quads_[i].tr.texCoords.v = top;
 	}
-}
-
--(void) setTexture:(CCTexture2D *)texture
-{
-	CGSize s = [texture contentSize];
-	[self setTexture:texture withRect:CGRectMake(0,0, s.width, s.height)];
 }
 
 -(void) setTexture:(CCTexture2D *)texture withRect:(CGRect)rect
@@ -256,13 +186,20 @@
 	[self initTexCoordsWithRect:rect];
 }
 
+-(void) setTexture:(CCTexture2D *)texture
+{
+	CGSize s = [texture contentSize];
+	[self setTexture:texture withRect:CGRectMake(0,0, s.width, s.height)];
+}
+
 -(void) setDisplayFrame:(CCSpriteFrame *)spriteFrame
 {
+
 	NSAssert( CGPointEqualToPoint( spriteFrame.offsetInPixels , CGPointZero ), @"QuadParticle only supports SpriteFrames with no offsets");
 
 	// update texture before updating texture rect
 	if ( spriteFrame.texture.name != texture_.name )
-		[self setTexture: spriteFrame.texture withRect:spriteFrame.rect];	
+		[self setTexture: spriteFrame.texture];	
 }
 
 -(void) initIndices
@@ -283,15 +220,7 @@
 -(void) updateQuadWithParticle:(tCCParticle*)p newPosition:(CGPoint)newPos
 {
 	// colors
-	ccV3F_C4B_T2F_Quad *quad; 
-
-	if (batchNode_) 
-	{	
-		ccV3F_C4B_T2F_Quad *batchQuads = [[batchNode_ textureAtlas] quads]; 
-		quad = &(batchQuads[atlasIndex_+p->atlasIndex]); 
-	}
-	else
-		quad = &(quads_[particleIdx]);
+	ccV2F_C4B_T2F_Quad *quad = &(quads_[particleIdx]);
 	
 	ccColor4B color = { p->color.r*255, p->color.g*255, p->color.b*255, p->color.a*255};
 	quad->bl.colors = color;
@@ -301,51 +230,7 @@
 	
 	// vertices
 	GLfloat size_2 = p->size/2;
-	//don't transform particles if type is free
-	if (batchNode_ && transformSystemDirty_ && positionType_!=kCCPositionTypeFree)
-	{
-		GLfloat x1 = -size_2*scaleX_;
-		GLfloat y1 = -size_2*scaleY_;
-		
-		GLfloat x2 = size_2*scaleX_;
-		GLfloat y2 = size_2*scaleY_;
-		GLfloat x = newPos.x;
-		GLfloat y = newPos.y;
-		
-		GLfloat r = (GLfloat)-CC_DEGREES_TO_RADIANS(p->rotation+rotation_);
-		GLfloat cr = cosf(r) * scaleX_;
-		GLfloat sr = sinf(r) * scaleX_;
-		GLfloat cr2 = cosf(r) * scaleY_;
-		GLfloat sr2 = sinf(r) * scaleY_;
-		GLfloat ax = x1 * cr - y1 * sr2 + x;
-		GLfloat ay = x1 * sr + y1 * cr2 + y;
-		GLfloat bx = x2 * cr - y1 * sr2 + x;
-		GLfloat by = x2 * sr + y1 * cr2 + y;
-		GLfloat cx = x2 * cr - y2 * sr2 + x;
-		GLfloat cy = x2 * sr + y2 * cr2 + y;
-		GLfloat dx = x1 * cr - y2 * sr2 + x;
-		GLfloat dy = x1 * sr + y2 * cr2 + y;
-		
-		// bottom-left
-		quad->bl.vertices.x = ax;
-		quad->bl.vertices.y = ay;
-		quad->bl.vertices.z = p->z;
-		
-		// bottom-right vertex:
-		quad->br.vertices.x = bx;
-		quad->br.vertices.y = by;
-		quad->br.vertices.z = p->z;
-		
-		// top-left vertex:
-		quad->tl.vertices.x = dx;
-		quad->tl.vertices.y = dy;
-		quad->tl.vertices.z = p->z;
-		// top-right vertex:
-		quad->tr.vertices.x = cx;
-		quad->tr.vertices.y = cy;
-		quad->tr.vertices.z = p->z;	
-	}
-	else if( p->rotation ) {
+	if( p->rotation ) {
 		GLfloat x1 = -size_2;
 		GLfloat y1 = -size_2;
 		
@@ -412,8 +297,6 @@
 // overriding draw method
 -(void) draw
 {	
-	NSAssert(!batchNode_,@"draw should not be called when added to a particleBatchNode"); 
-
 	[super draw];
 	
 	ccGLBindTexture2D( [texture_ name] );
@@ -430,36 +313,6 @@
 	glBindVertexArray( 0 );
 	
 	CHECK_GL_ERROR_DEBUG();
-}
-
--(void) batchNodeInitialization
-{
-	[self initTexCoordsWithRect:textureRect_]; 	
-}
-
--(void) setBatchNode:(CCParticleBatchNode *)batchNode
-{
-	if( batchNode != batchNode_ ) {
-		[super setBatchNode:batchNode];
-		
-		if( ! batchNode )
-		{
-			// do something ??
-		}
-		else
-		{
-			if (quads_)
-				free(quads_);
-			quads_ = NULL;
-			
-			if (indices_)
-				free(indices_);
-			indices_ = NULL;
-			
-			glDeleteBuffers(2, &buffersVBO_[0]);
-			glDeleteVertexArrays(1, &VAOname_);
-		}
-	}
 }
 
 @end
