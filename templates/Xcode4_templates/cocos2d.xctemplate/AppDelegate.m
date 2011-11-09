975a2d7781ad7a18647efedbee3702480d6f0164
Ricardo Quesada <ricardoquesada@gmail.com>
2011-11-08 18:38:37 -0800

    Templates use the correct API
    Former-commit-id: 4e53868455aaed3379237a100982d36c4d0e17d1

--- templates/Xcode4_templates/cocos2d.xctemplate/AppDelegate.m
+++ templates/Xcode4_templates/cocos2d.xctemplate/AppDelegate.m
@@ -67,7 +67,7 @@
 		CCLOG(@"Retina Display Not supported");
 		
 	[director setAnimationInterval:1.0/60];
-	[director setDisplayFPS:YES];
+	[director setDisplayStats:kCCDirectorStatsFPS];
 	
 	
 	// make the OpenGLView a child of the view controller
