60b59c00901de269bb125b9963f5bf8e8c79264f
Ricardo Quesada <ricardoquesada@gmail.com>
2011-12-17 12:59:50 -0800

    BaseAppController used as base AppController for all tests
    Former-commit-id: 2648583a99eba09f52a53c6e2b88cb2a2211b220

---
+++ tests/BaseAppController.m
@@ -0,0 +1,13 @@
+//
+//  AppController.m
+//  cocos2d-ios
+//
+//  Created by Ricardo Quesada on 12/17/11.
+//  Copyright (c) 2011 Sapus Media. All rights reserved.
+//
+
+#import "AppController.h"
+
+@implementation AppController
+
+@end
