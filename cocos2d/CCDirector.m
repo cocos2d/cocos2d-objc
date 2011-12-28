28d71b691de3bc3e0e2aafee0a84ebc6c7200324
Ricardo Quesada <ricardoquesada@gmail.com>
2011-12-27 21:32:06 -0300

    IMproved CCDirector logging
    Former-commit-id: 0fea9379c6c0b89c530c1431cec0382d50751d73

--- cocos2d/CCDirector.m
+++ cocos2d/CCDirector.m
@@ -166,6 +166,11 @@ static CCDirector *_sharedDirector = nil;
 	return self;
 }
 
+- (NSString*) description
+{
+	return [NSString stringWithFormat:@"<%@ = %08X | Size: %0.f x %0.f, view = %@>", [self class], self, winSizeInPoints_.width, winSizeInPoints_.height, self.view];
+}
+
 - (void) dealloc
 {
 	CCLOGINFO(@"cocos2d: deallocing %@", self);
