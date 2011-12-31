e0c373dbfa496f919070dde4642bac7ef07cd636
Ricardo Quesada <ricardoquesada@gmail.com>
2011-12-31 10:11:36 -0300

    CC_DIRECTOR_INIT enables Retina Display

--- tests/SpriteTest.m
+++ tests/SpriteTest.m
@@ -4536,7 +4536,6 @@ Class restartAction()
 	[director_ setProjection:kCCDirectorProjection2D];
 //	[director setProjection:kCCDirectorProjection3D];
 
-
 	// Enables High Res mode (Retina Display) on iPhone 4 and maintains low res on all other devices
 	if( ! [director_ enableRetinaDisplay:YES] )
 		CCLOG(@"Retina Display Not supported");
