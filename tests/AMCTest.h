100ba995bf1a3591390d5dac11065fc63dd6029a
Stepan Generalov <psi.pk.ru@gmail.com>
2012-01-06 19:56:26 +0400

    AMCTest: add ParallaxAMC layer.

--- tests/AMCTest.h
+++ tests/AMCTest.h
@@ -67,6 +67,10 @@
 }
 @end
 
+@interface ParallaxAMC : CCLayer {
+}
+@end
+
 
 
 
--- tests/AMCTest.m
+++ tests/AMCTest.m
@@ -17,6 +17,7 @@ static NSString *transitions[] = {
     @"NodeAMC",
 	@"SpriteAMC1",
     @"LayersAMC",
+    @"ParallaxAMC",
 };
 
 Class nextAction(void);
@@ -511,6 +512,115 @@ enum nodeTags {
 - (NSString *) subtitle
 {
     return @"They should load the same as were saved.";
+}
+
+@end
+
+@implementation ParallaxAMC
+-(id) init
+{
+	if( (self=[super init] )) {
+        
+#ifdef __IPHONE_OS_VERSION_MAX_ALLOWED
+		self.isTouchEnabled = YES;
+#elif defined(__MAC_OS_X_VERSION_MAX_ALLOWED)
+		self.isMouseEnabled = YES;
+#endif
+		
+		// Top Layer, a simple image
+		CCSprite *cocosImage = [CCSprite spriteWithFile:@"powered.png"];
+		// scale the image (optional)
+		cocosImage.scale = 2.5f;
+		// change the transform anchor point to 0,0 (optional)
+		cocosImage.anchorPoint = ccp(0,0);
+		
+		
+		// Middle layer: a Tile map atlas
+		CCSprite *tilesImage =  [CCSPrite spriteWithFile: @"TileMaps/tiles.png" ];
+		
+		// change the transform anchor to 0,0 (optional)
+		tilemap.anchorPoint = ccp(0, 0);        
+		
+		
+		// background layer: another image
+		CCSprite *background = [CCSprite spriteWithFile:@"background.png"];
+		// scale the image (optional)
+		background.scale = 1.5f;
+		// change the transform anchor point (optional)
+		background.anchorPoint = ccp(0,0);
+		
+		
+		// create a void node, a parent node
+		CCParallaxNode *voidNode = [CCParallaxNode node];
+		
+		// NOW add the 3 layers to the 'void' node
+		
+		// background image is moved at a ratio of 0.4x, 0.5y
+		[voidNode addChild:background z:-1 parallaxRatio:ccp(0.4f,0.5f) positionOffset:CGPointZero];
+		
+		// tiles are moved at a ratio of 1.0, 1.0y
+		[voidNode addChild:tilesImage z:1 parallaxRatio:ccp(1.0f,1.0f) positionOffset:ccp(0,-200)];
+		
+		// top image is moved at a ratio of 3.0x, 2.5y
+		[voidNode addChild:cocosImage z:2 parallaxRatio:ccp(3.0f,2.5f) positionOffset:ccp(200,1000)];
+		[self addChild:voidNode z:0 tag:kTagNode];
+        
+	}
+	
+	return self;
+}
+
+#ifdef __IPHONE_OS_VERSION_MAX_ALLOWED
+-(void) registerWithTouchDispatcher
+{
+	[[CCTouchDispatcher sharedDispatcher] addTargetedDelegate:self priority:0 swallowsTouches:YES];
+}
+
+-(BOOL) ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	return YES;
+}
+
+-(void) ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{
+}
+
+-(void) ccTouchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
+{
+}
+
+-(void) ccTouchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	CGPoint touchLocation = [touch locationInView: [touch view]];	
+	CGPoint prevLocation = [touch previousLocationInView: [touch view]];	
+    
+	touchLocation = [[CCDirector sharedDirector] convertToGL: touchLocation];
+	prevLocation = [[CCDirector sharedDirector] convertToGL: prevLocation];
+    
+	CGPoint diff = ccpSub(touchLocation,prevLocation);
+	
+	CCNode *node = [self getChildByTag:kTagNode];
+	CGPoint currentPos = [node position];
+	[node setPosition: ccpAdd(currentPos, diff)];
+}
+
+#elif defined(__MAC_OS_X_VERSION_MAX_ALLOWED)
+
+-(BOOL) ccMouseDragged:(NSEvent *)event
+{
+	CCNode *node = [self getChildByTag:kTagNode];
+	CGPoint currentPos = [node position];
+	[node setPosition: ccpAdd(currentPos, CGPointMake( event.deltaX, -event.deltaY) )];
+	
+	return YES;
+}
+
+#endif
+
+
+-(NSString *) title
+{
+	return @"Parallax: drag screen";
 }
 
 @end
