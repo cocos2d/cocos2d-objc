6ea198bb87836547c972585b56290a24c66547ae
Ricardo Quesada <ricardoquesada@gmail.com>
2012-01-07 06:59:34 -0800

    Oops... missing templates

---
+++ templates/Xcode4_templates/cocos2d iOS.xctemplate/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
