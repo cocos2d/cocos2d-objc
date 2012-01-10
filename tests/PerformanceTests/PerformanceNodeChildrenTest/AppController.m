25b7a3a6726bbd1646e26408315cfff89dc4f331
Ricardo Quesada <ricardoquesada@gmail.com>
2012-01-09 20:54:51 -0800

    PerformanceTests compiles and run with v2.0b2

--- tests/PerformanceTests/PerformanceNodeChildrenTest/AppController.m
+++ tests/PerformanceTests/PerformanceNodeChildrenTest/AppController.m
@@ -9,47 +9,83 @@
 
 @implementation AppController
 
-- (void)applicationDidFinishLaunching:(UIApplication *)application
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+	// Don't call super
 	// Init the window
-	window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-
-	// get instance of the shared director
-	CCDirector *director = [CCDirector sharedDirector];
-
-	// display FPS (useful when debugging)
-	[director setDisplayStats:YES];
-
-	// frames per second
-	[director setAnimationInterval:1.0/60];
-
-	// create an OpenGL view
-	EAGLView *glView = [EAGLView viewWithFrame:[window bounds]];
-
-	// connect it to the director
-	[director setOpenGLView:glView];
-
+	window_ = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	
+	
+	// Create an EAGLView with a RGB8 color buffer, and a depth buffer of 24-bits
+	EAGLView *glView = [EAGLView viewWithFrame:[window_ bounds]
+								   pixelFormat:kEAGLColorFormatRGBA8
+								   depthFormat:GL_DEPTH_COMPONENT24_OES
+							preserveBackbuffer:NO
+									sharegroup:nil
+								 multiSampling:NO
+							   numberOfSamples:0];
+	
+	director_ = (CCDirectorIOS*) [CCDirector sharedDirector];
+	
+	director_.wantsFullScreenLayout = YES;
+	// Display Milliseconds Per Frame
+	[director_ setDisplayStats:YES];
+	
+	// set FPS at 60
+	[director_ setAnimationInterval:1.0/60];
+	
+	// attach the openglView to the director
+	[director_ setView:glView];
+	
+	// for rotation and other messages
+	[director_ setDelegate:self];
+	
+	// 2D projection
+	[director_ setProjection:kCCDirectorProjection2D];
+	//	[director setProjection:kCCDirectorProjection3D];
+	
 	// Enables High Res mode (Retina Display) on iPhone 4 and maintains low res on all other devices
-	if( ! [director enableRetinaDisplay:YES] )
+	if( ! [director_ enableRetinaDisplay:YES] )
 		CCLOG(@"Retina Display Not supported");
-
-	navigationController_ = [[UINavigationController alloc] initWithRootViewController:viewController_];
-	navigationController_.navigationBarHidden = YES;
-
+	
+	navController_ = [[UINavigationController alloc] initWithRootViewController:director_];
+	navController_.navigationBarHidden = YES;
+	
 	// set the Navigation Controller as the root view controller
-	[window_ setRootViewController:navigationController_];
-
-	[viewController_ release];
-	[navigationController_ release];
-
-	// Make the window visible
-	[window makeKeyAndVisible];
-
+	//	[window_ setRootViewController:rootViewController_];
+	[window_ addSubview:navController_.view];
+	
+	// make main window visible
+	[window_ makeKeyAndVisible];
+	
+	// Default texture format for PNG/BMP/TIFF/JPEG/GIF images
+	// It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
+	// You can change anytime.
+	[CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];
+	
+	// When in iPad / RetinaDisplay mode, CCFileUtils will append the "-ipad" / "-hd" to all loaded files
+	// If the -ipad  / -hdfile is not found, it will load the non-suffixed version
+	[CCFileUtils setiPadSuffix:@"-ipad"];			// Default on iPad is "" (empty string)
+	[CCFileUtils setRetinaDisplaySuffix:@"-hd"];	// Default on RetinaDisplay is "-hd"
+	
+	// Assume that PVR images have premultiplied alpha
+	[CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
+	
+	// create the main scene
 	CCScene *scene = [CCScene node];
 	[scene addChild: [nextAction() testWithQuantityOfNodes:kNodesIncrease]];
+	
+	[[director_ scheduler] scheduleSelector:@selector(dumpProfilerInfo:) forTarget:self interval:2 paused:NO];
+	
+	// and run it!
+	[director_ pushScene: scene];
+	
+	return YES;
+}
 
-	[[CCScheduler sharedScheduler] scheduleSelector:@selector(dumpProfilerInfo:) forTarget:self interval:2 paused:NO];
-	[director pushScene:scene];
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return UIInterfaceOrientationIsLandscape(interfaceOrientation);
 }
 
 -(void) dumpProfilerInfo:(ccTime)dt
@@ -57,47 +93,55 @@
 	CC_PROFILER_DISPLAY_TIMERS();
 }
 
-- (void)dealloc {
-	[window release];
-	[super dealloc];
-}
-
 // getting a call, pause the game
 -(void) applicationWillResignActive:(UIApplication *)application
 {
-	[[CCDirector sharedDirector] pause];
+	if( [navController_ visibleViewController] == director_ )
+		[director_ pause];
 }
 
 // call got rejected
 -(void) applicationDidBecomeActive:(UIApplication *)application
 {
-	[[CCDirector sharedDirector] resume];
+	if( [navController_ visibleViewController] == director_ )
+		[director_ resume];
 }
 
 -(void) applicationDidEnterBackground:(UIApplication*)application
 {
-	[[CCDirector sharedDirector] stopAnimation];
+	if( [navController_ visibleViewController] == director_ )
+		[director_ stopAnimation];
 }
 
 -(void) applicationWillEnterForeground:(UIApplication*)application
 {
-	[[CCDirector sharedDirector] startAnimation];
+	if( [navController_ visibleViewController] == director_ )
+		[director_ startAnimation];
 }
 
+// application will be killed
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-	[[CCDirector sharedDirector] end];
+	CC_DIRECTOR_END();
 }
 
 // purge memory
-- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
-	[[CCDirector sharedDirector] purgeCachedData];
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application
+{
+	[director_ purgeCachedData];
 }
 
 // next delta time will be zero
 -(void) applicationSignificantTimeChange:(UIApplication *)application
 {
-	[[CCDirector sharedDirector] setNextDeltaTimeZero:YES];
+	[director_ setNextDeltaTimeZero:YES];
 }
 
+- (void) dealloc
+{
+	[window_ release];
+	[navController_ release];
+	
+	[super dealloc];
+}
 @end
