835a1377ce13c389616ed800c91867288d7764e3
Panajev <Panajev@gmail.com>
2012-02-08 23:33:33 +0100

    (x): fixed new line issues. (x): upgraded project's settings. (+): added new TargetedAction action (developed by Karl Stenerud)

--- cocos2d/CCActionInterval.m
+++ cocos2d/CCActionInterval.m
@@ -1366,3 +1366,65 @@ static inline float bezierat( float a, float b, float c, float d, ccTime t )
 }
 
 @end
+
+@interface TargetedAction (Private)
+
+// Ugly hack to get around a compiler bug.
+- (id) initWithTarget:(id) targetIn actionByAnotherName:(CCFiniteTimeAction*) actionIn;
+
+@end
+
+@implementation TargetedAction
+
+@synthesize forcedTarget;
+
++ (id) actionWithTarget:(id) target action:(CCFiniteTimeAction*) action
+{
+	return [[[self alloc] initWithTarget:target actionByAnotherName:action] autorelease];
+}
+
+- (id) initWithTarget:(id) targetIn action:(CCFiniteTimeAction*) actionIn
+{
+	return [self initWithTarget:targetIn actionByAnotherName:actionIn];
+}
+
+- (id) initWithTarget:(id) targetIn actionByAnotherName:(CCFiniteTimeAction*) actionIn
+{
+	if(nil != (self = [super initWithDuration:actionIn.duration]))
+	{
+		forcedTarget = [targetIn retain];
+		action = [actionIn retain];
+	}
+	return self;
+}
+
+- (void) dealloc
+{
+	[forcedTarget release];
+	[action release];
+	[super dealloc];
+}
+
+//- (void) updateDuration:(id)aTarget
+//{
+//	[action updateDuration:forcedTarget];
+//	duration_ = action.duration;
+//}
+
+- (void) startWithTarget:(id)aTarget
+{
+	[super startWithTarget:forcedTarget];
+	[action startWithTarget:forcedTarget];
+}
+
+- (void) stop
+{
+	[action stop];
+}
+
+- (void) update:(ccTime) time
+{
+	[action update:time];
+}
+
+@end
