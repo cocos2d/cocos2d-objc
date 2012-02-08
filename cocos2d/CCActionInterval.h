62d89dd2a31c567a527401046079670ee5afcd48
Panajev <Panajev@gmail.com>
2012-02-08 23:33:33 +0100

    (x): fixed new line issues. (x): upgraded project's settings. (+): added new TargetedAction action (developed by Karl Stenerud)
    Former-commit-id: 835a1377ce13c389616ed800c91867288d7764e3

--- cocos2d/CCActionInterval.h
+++ cocos2d/CCActionInterval.h
@@ -423,3 +423,22 @@ typedef struct _ccBezierConfig {
  */
 -(id) initWithDuration:(ccTime)duration animation:(CCAnimation*)animation restoreOriginalFrame:(BOOL)restoreOriginalFrame;
 @end
+
+/** Overrides the target of an action so that it always runs on the target
+ * specified at action creation rather than the one specified by runAction.
+ */
+@interface TargetedAction : CCActionInterval <NSCopying>
+{
+	id forcedTarget;
+	CCFiniteTimeAction* action;
+}
+/** This is the target that the action will be forced to run with */
+@property(readwrite,nonatomic,retain) id forcedTarget;
+
+/** Create an action with the specified action and forced target */
++ (id) actionWithTarget:(id) target action:(CCFiniteTimeAction*) action;
+
+/** Init an action with the specified action and forced target */
+- (id) initWithTarget:(id) target action:(CCFiniteTimeAction*) action;
+
+@end
