f9184eb71c92cb50398f53d4390f5cb69fc84d40
Ricardo Quesada <ricardoquesada@gmail.com>
2012-03-12 12:20:19 -0700

    tests: documentaions said that default was "-ipad-hd" when if fact it is "-ipadhd"

--- tests/HiResTest.m
+++ tests/HiResTest.m
@@ -269,7 +269,7 @@ Class restartAction()
 	// If the -hd, -ipad, -ipadhd files are not found, it will load the non-suffixed version
 	[CCFileUtils setiPhoneRetinaDisplaySuffix:@"-hd"];		// Default on iPhone RetinaDisplay is "-hd"
 	[CCFileUtils setiPadSuffix:@"-ipad"];					// Default on iPad is "" (empty string)
-	[CCFileUtils setiPadRetinaDisplaySuffix:@"-ipadhd"];	// Default on iPad RetinaDisplay is "-ipad-hd"
+	[CCFileUtils setiPadRetinaDisplaySuffix:@"-ipadhd"];	// Default on iPad RetinaDisplay is "-ipadhd"
 	
 	// create the main scene
 	CCScene *scene = [CCScene node];
