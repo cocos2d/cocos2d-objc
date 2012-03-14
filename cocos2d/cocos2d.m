5e66d92f2d9c570442e9f77dcd9cf845446f2b77
Ricardo Quesada <ricardoquesada@gmail.com>
2012-03-14 16:45:48 -0700

    updated cocos2d version
    Former-commit-id: fc297e07ba21a9155bba96a593de45d84f63aa80

--- cocos2d/cocos2d.m
+++ cocos2d/cocos2d.m
@@ -26,7 +26,7 @@
 
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
-static NSString *version = @"cocos2d v2.0.0-rc0";
+static NSString *version = @"cocos2d v2.0.0-rc0-post";
 
 NSString *cocos2dVersion()
 {
