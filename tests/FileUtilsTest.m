5fd1a0cadff9a245192a0e125d832db65fceed13
Ricardo Quesada <ricardoquesada@gmail.com>
2012-03-23 18:25:00 -0700

    fixes issue #1344
    FileUtils returns a valid path even if the no-suffix file is not present.
    
    Conflicts:
    
    	cocos2d-ios.xcodeproj/project.pbxproj

---
+++ tests/FileUtilsTest.m
@@ -0,0 +1,317 @@
+//
+// FileUtils Demo
+// a cocos2d example
+// http://www.cocos2d-iphone.org
+//
+
+// cocos import
+#import "cocos2d.h"
+
+// local import
+#import "FileUtilsTest.h"
+
+static int sceneIdx=-1;
+static NSString *transitions[] = {
+	@"Issue1344",
+};
+
+Class nextAction(void);
+Class backAction(void);
+Class restartAction(void);
+
+Class nextAction()
+{
+	
+	sceneIdx++;
+	sceneIdx = sceneIdx % ( sizeof(transitions) / sizeof(transitions[0]) );
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+Class backAction()
+{
+	sceneIdx--;
+	int total = ( sizeof(transitions) / sizeof(transitions[0]) );
+	if( sceneIdx < 0 )
+		sceneIdx += total;
+	
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+Class restartAction()
+{
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+#pragma mark -
+#pragma mark SpriteDemo
+
+@implementation SpriteDemo
+-(id) init
+{
+	if( (self = [super init]) ) {
+		
+		
+		CGSize s = [[CCDirector sharedDirector] winSize];
+		
+		CCLabelTTF *label = [CCLabelTTF labelWithString:[self title] fontName:@"Arial" fontSize:26];
+		[self addChild: label z:1];
+		[label setPosition: ccp(s.width/2, s.height-50)];
+		
+		NSString *subtitle = [self subtitle];
+		if( subtitle ) {
+			CCLabelTTF *l = [CCLabelTTF labelWithString:subtitle fontName:@"Thonburi" fontSize:16];
+			[self addChild:l z:1];
+			[l setPosition:ccp(s.width/2, s.height-80)];
+		}
+		
+		CCMenuItemImage *item1 = [CCMenuItemImage itemWithNormalImage:@"b1.png" selectedImage:@"b2.png" target:self selector:@selector(backCallback:)];
+		CCMenuItemImage *item2 = [CCMenuItemImage itemWithNormalImage:@"r1.png" selectedImage:@"r2.png" target:self selector:@selector(restartCallback:)];
+		CCMenuItemImage *item3 = [CCMenuItemImage itemWithNormalImage:@"f1.png" selectedImage:@"f2.png" target:self selector:@selector(nextCallback:)];
+		
+		CCMenu *menu = [CCMenu menuWithItems:item1, item2, item3, nil];
+		
+		menu.position = CGPointZero;
+		item1.position = ccp( s.width/2 - item2.contentSize.width*2, item2.contentSize.height/2);
+		item2.position = ccp( s.width/2, item2.contentSize.height/2);
+		item3.position = ccp( s.width/2 + item2.contentSize.width*2, item2.contentSize.height/2);
+		[self addChild: menu z:1];
+	}
+	return self;
+}
+
+-(void) dealloc
+{
+	[super dealloc];
+}
+
+-(void) restartCallback: (id) sender
+{
+	CCScene *s = [CCScene node];
+	[s addChild: [restartAction() node]];
+	[[CCDirector sharedDirector] replaceScene: s];
+}
+
+-(void) nextCallback: (id) sender
+{
+	CCScene *s = [CCScene node];
+	[s addChild: [nextAction() node]];
+	[[CCDirector sharedDirector] replaceScene: s];
+}
+
+-(void) backCallback: (id) sender
+{
+	CCScene *s = [CCScene node];
+	[s addChild: [backAction() node]];
+	[[CCDirector sharedDirector] replaceScene: s];
+}
+
+-(NSString*) title
+{
+	return @"No title";
+}
+
+-(NSString*) subtitle
+{
+	return nil;
+}
+@end
+
+#pragma mark -
+#pragma mark Example Sprite 1
+
+
+@implementation Sprite1
+
+-(id) init
+{
+	if( (self=[super init]) ) {
+		
+#ifdef __CC_PLATFORM_IOS
+		self.isTouchEnabled = YES;
+#elif defined(__CC_PLATFORM_MAC)
+		self.isMouseEnabled = YES;
+#endif
+		
+		CGSize s = [[CCDirector sharedDirector] winSize];
+		[self addNewSpriteWithCoords:ccp(s.width/2, s.height/2)];
+	}
+	return self;
+}
+
+-(void) addNewSpriteWithCoords:(CGPoint)p
+{
+	int idx = CCRANDOM_0_1() * 1400 / 100;
+	int x = (idx%5) * 85;
+	int y = (idx/5) * 121;
+	
+	
+	CCSprite *sprite = [CCSprite spriteWithFile:@"grossini_dance_atlas.png" rect:CGRectMake(x,y,85,121)];
+	[self addChild:sprite];
+	
+	sprite.position = ccp( p.x, p.y);
+	
+	id action;
+	float rand = CCRANDOM_0_1();
+	
+	if( rand < 0.20 )
+		action = [CCScaleBy actionWithDuration:3 scale:2];
+	else if(rand < 0.40)
+		action = [CCRotateBy actionWithDuration:3 angle:360];
+	else if( rand < 0.60)
+		action = [CCBlink actionWithDuration:1 blinks:3];
+	else if( rand < 0.8 )
+		action = [CCTintBy actionWithDuration:2 red:0 green:-255 blue:-255];
+	else
+		action = [CCFadeOut actionWithDuration:2];
+	id action_back = [action reverse];
+	id seq = [CCSequence actions:action, action_back, nil];
+	
+	[sprite runAction: [CCRepeatForever actionWithAction:seq]];
+}
+
+#ifdef __CC_PLATFORM_IOS
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	for( UITouch *touch in touches ) {
+		CGPoint location = [touch locationInView: [touch view]];
+		
+		location = [[CCDirector sharedDirector] convertToGL: location];
+		
+		[self addNewSpriteWithCoords: location];
+	}
+}
+#elif defined(__CC_PLATFORM_MAC)
+-(BOOL) ccMouseUp:(NSEvent *)event
+{
+	CGPoint location = [[CCDirector sharedDirector] convertEventToGL:event];
+	[self addNewSpriteWithCoords: location];
+	
+	return YES;
+	
+}
+#endif
+
+-(NSString *) title
+{
+	return @"Sprite (tap screen)";
+}
+@end
+
+
+#pragma mark - AppDelegate - iOS
+
+// CLASS IMPLEMENTATIONS
+
+#ifdef __CC_PLATFORM_IOS
+@implementation AppController
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+	// Don't call super
+	// Init the window
+	window_ = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	
+	
+	// Create an CCGLView with a RGB8 color buffer, and a depth buffer of 24-bits
+	CCGLView *glView = [CCGLView viewWithFrame:[window_ bounds]
+								   pixelFormat:kEAGLColorFormatRGBA8
+								   depthFormat:GL_DEPTH_COMPONENT24_OES
+							preserveBackbuffer:NO
+									sharegroup:nil
+								 multiSampling:NO
+							   numberOfSamples:4];
+	
+	director_ = (CCDirectorIOS*) [CCDirector sharedDirector];
+	
+	director_.wantsFullScreenLayout = YES;
+	// Display Milliseconds Per Frame
+	[director_ setDisplayStats:YES];
+	
+	// set FPS at 60
+	[director_ setAnimationInterval:1.0/60];
+	
+	// attach the openglView to the director
+	[director_ setView:glView];
+	
+	// for rotation and other messages
+	[director_ setDelegate:self];
+	
+	// 2D projection
+	[director_ setProjection:kCCDirectorProjection2D];
+	//	[director setProjection:kCCDirectorProjection3D];
+	
+	// Enables High Res mode (Retina Display) on iPhone 4 and maintains low res on all other devices
+	if( ! [director_ enableRetinaDisplay:YES] )
+		CCLOG(@"Retina Display Not supported");
+	
+	navController_ = [[UINavigationController alloc] initWithRootViewController:director_];
+	navController_.navigationBarHidden = YES;
+	
+	// set the Navigation Controller as the root view controller
+	//	[window_ setRootViewController:rootViewController_];
+	[window_ addSubview:navController_.view];
+	
+	// make main window visible
+	[window_ makeKeyAndVisible];
+	
+	// Default texture format for PNG/BMP/TIFF/JPEG/GIF images
+	// It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
+	// You can change anytime.
+	[CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];
+	
+	// If the 1st suffix is not found, then the fallback suffixes are going to used. If none is found, it will try with the name without suffix.
+	// On iPad HD  : "-ipadhd", "-ipad",  "-hd"
+	// On iPad     : "-ipad", "-hd"
+	// On iPhone HD: "-hd"
+	CCFileUtils *sharedFileUtils = [CCFileUtils sharedFileUtils];
+	[sharedFileUtils setEnableFallbackSuffixes:YES];			// Default: NO. No fallback suffixes are going to be used
+	[sharedFileUtils setiPhoneRetinaDisplaySuffix:@"-hd"];		// Default on iPhone RetinaDisplay is "-hd"
+	[sharedFileUtils setiPadSuffix:@"-ipad"];					// Default on iPad is "ipad"
+	[sharedFileUtils setiPadRetinaDisplaySuffix:@"-ipadhd"];	// Default on iPad RetinaDisplay is "-ipadhd"
+	
+	// Assume that PVR images have premultiplied alpha
+	[CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
+	
+	// create the main scene
+	CCScene *scene = [CCScene node];
+	[scene addChild: [nextAction() node]];
+	
+	// and run it!
+	[director_ pushScene: scene];
+	
+	return YES;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+	//	return UIInterfaceOrientationIsLandscape(interfaceOrientation);
+}
+
+@end
+
+#pragma mark -
+#pragma mark AppController - Mac
+
+#elif defined(__CC_PLATFORM_MAC)
+
+@implementation AppController
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	[super applicationDidFinishLaunching:aNotification];
+	
+	CCScene *scene = [CCScene node];
+	[scene addChild: [nextAction() node]];
+	
+	[director_ pushScene:scene];	
+	[director_ startAnimation];
+}
+@end
+#endif
