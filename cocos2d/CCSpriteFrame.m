9b2bac6ea4ae8b7d2a9c7780ce18aab16fdda483
Marco Tillemans <marcotillemans@gmail.com>
2012-04-16 02:36:19 +0200

    Made CCSpriteFrame description more verbose.
    Former-commit-id: c67bab2ea9b8344b1498919f9c088b638d801d96

--- cocos2d/CCSpriteFrame.m
+++ cocos2d/CCSpriteFrame.m
@@ -64,13 +64,17 @@
 
 - (NSString*) description
 {
-	return [NSString stringWithFormat:@"<%@ = %08X | TextureName=%d, Rect = (%.2f,%.2f,%.2f,%.2f)> rotated:%d", [self class], self,
+	return [NSString stringWithFormat:@"<%@ = %08X | TextureName=%d, Rect = (%.2f,%.2f,%.2f,%.2f)> rotated:%d, offset = (%.2f,%.2f), originalSize w %.2f h %.2f)", [self class], self,
 			texture_.name,
 			rect_.origin.x,
 			rect_.origin.y,
 			rect_.size.width,
 			rect_.size.height,
-			rotated_
+			rotated_,
+            offsetInPixels_.x,
+            offsetInPixels_.y,
+            originalSizeInPixels_.width,
+            originalSizeInPixels_.height
 			];
 }
 
