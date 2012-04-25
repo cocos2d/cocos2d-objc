f909f6ea92e46c4da7345a0d9f229c439654c763
Ricardo Quesada <ricardoquesada@gmail.com>
2012-04-24 18:35:57 -0700

    JS 'converted' to Objective-c
    Former-commit-id: 95832eb002b553785b39997eb5b41d46742af929

--- tests/js/test-sprite.js
+++ tests/js/test-sprite.js
@@ -2,18 +2,19 @@
 var director = [CCDirector sharedDirector];
 var mainScene = director.runningScene;
 var layer = mainScene.children[0];
-var sprite = CCSprite.alloc.initWithFile('grossini.png');
-layer.addChild( sprite );
+var sprite = [CCSprite spriteWithFile:'grossini.png'];
+[layer addChild: sprite ];
 var s = [director winSize];
 sprite.setPosition( CGPointMake( s.width/2, s.height/2 ) );
-sprite.release;
 
 
 // action
 var rotate = [CCRotateBy actionWithDuration:2 angle:360];
 var move = [CCMoveBy actionWithDuration:2 position:CGPointMake(200,0)];
-var seq = [CCSequence actionsWithArray: [rotate, move] ];
-sprite.runAction( seq );
+var jump = [CCJumpBy actionWithDuration:2 position:CGPointMake(-300,0) height:100 jumps:2];
+
+var seq = [CCSequence actionsWithArray: [rotate, move, jump] ];
+[sprite runAction:seq ];
 
 
 
