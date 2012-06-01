53d0843bf248e1f2c51c5beea2bc5ecee8f43b68
Ricardo Quesada <ricardoquesada@gmail.com>
2012-05-31 21:56:41 -0700

    JS script: retval and args are parsed in different functions
    Former-commit-id: a82e1d432506ea4d27b8d7f30faa6ab4efab46c1

--- tests/SpiderMonkeyTest/js_bindings_old_manual.h
+++ tests/SpiderMonkeyTest/js_bindings_old_manual.h
@@ -1,5 +1,9 @@
 #import "js_bindings_NSObject.h"
 
+#import "cocos2d.h"
+
+
+#pragma mark - With Proxy
 
 @interface ProxyJS_CCNode : JSPROXY_NSObject
 {
@@ -15,5 +19,21 @@
 
 -(void) onEnter_JS;
 -(void) onExit_JS;
+
+@end
+
+
+
+#pragma mark - Subclass
+
+@interface JSExt_CCNode : CCNode
+{
+	JSObject	*jsObject_;
+}
+
+@property (nonatomic, readwrite) JSObject *jsObject;
+
++(void) js_createClassWithContext:(JSContext*)cx object:(JSObject*)globalObj name:(NSString*)name;
+
 
 @end
