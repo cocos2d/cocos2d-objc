6f7f2056a035723260b9f65eafd7e7bfc92bb30d
Nicolas Gramlich <ngramlich@zynga.com>
2012-06-26 12:06:16 -0700

    Added JSConsole over TCP (with Bonjour pairing) for the JSExample. So far only tested running the client and server on the same Mac.
    Former-commit-id: e6f3a9e2439fbb4e2c6901b6f220d9c420630d39

--- tests/BaseAppController.h
+++ tests/BaseAppController.h
@@ -5,7 +5,6 @@
 //  Created by Ricardo Quesada on 12/17/11.
 //  Copyright (c) 2011 Sapus Media. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
 
 #import "cocos2d.h"
@@ -32,11 +31,18 @@
 
 #elif defined(__CC_PLATFORM_MAC)
 
-@interface BaseAppController : NSObject <NSApplicationDelegate>
+#import <Cocoa/Cocoa.h>
+#import "ThoMoServerStub.h"
+
+@class SMLTextView;
+@class MGSFragaria;
+
+@interface BaseAppController : NSObject <NSApplicationDelegate, ThoMoServerDelegateProtocol>
 {
 	NSWindow		*window_;
 	CCGLView		*glView_;
 	CCDirectorMac	*director_;							// weak ref
+    ThoMoServerStub *thoMoServer;
 }
 
 @property (nonatomic, assign) IBOutlet NSWindow	*window;
@@ -44,6 +50,8 @@
 @property (nonatomic, readonly) CCDirectorMac	*director;
 
 - (IBAction)toggleFullScreen:(id)sender;
+
+- (void)initThoMoServer;
 
 @end
 #endif // __CC_PLATFORM_MAC
