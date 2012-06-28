b5ebd5f167b2b50ab5bbd892761ef3d0ec6d9fef
Ricardo Quesada <ricardoquesada@gmail.com>
2012-06-27 18:21:07 -0700

    JS: BridgeSupport: added exception file for cocos2d-ios
    Former-commit-id: d20ba6cff631289af3d23a6e0ba748d4b21949ca

--- tools/js/cocos2d_gen_bridgesupport.sh
+++ tools/js/cocos2d_gen_bridgesupport.sh
@@ -1,3 +1,3 @@
 gen_bridge_metadata -F complete --64-bit -c '-D__CC_PLATFORM_MAC -ISupport -IPlatforms -IPlatforms/Mac -I.' *.h Support/*.h Platforms/*.h Platforms/Mac/*.h -o ../tools/js/cocos2d-mac.bridgesupport 
 
-gen_bridge_metadata -F complete --no-64-bit -c '-D__CC_PLATFORM_IOS -ISupport -IPlatforms -IPlatforms/iOS -I.' *.h Support/*.h Platforms/*.h Platforms/iOS/*.h -o ../tools/js/cocos2d-ios.bridgesupport 
+gen_bridge_metadata -F complete --no-64-bit -c '-D__CC_PLATFORM_IOS -ISupport -IPlatforms -IPlatforms/iOS -I.' *.h Support/*.h Platforms/*.h Platforms/iOS/*.h -o ../tools/js/cocos2d-ios.bridgesupport -e ../tools/js/cocos2d-ios-exceptions.bridgesupport
