976250ab51a2824a1df6646a7883a32423814b54
Ricardo Quesada <ricardoquesada@gmail.com>
2012-06-29 13:17:56 -0700

    JS: remote console works on iOS

--- tests/SpiderMonkeyTest/JavascriptSpidermonkey.h
+++ tests/SpiderMonkeyTest/JavascriptSpidermonkey.h
@@ -2,22 +2,11 @@
 #import "BaseAppController.h"
 #import "cocos2d.h"
 
-#ifdef __CC_PLATFORM_IOS
-
-@interface AppController : BaseAppController
-{
-	ThoMoServerStub *thoMoServer;
-}
-@end
-
-#elif defined( __CC_PLATFORM_MAC )
 #import "ThoMoServerStub.h"
 
 @interface AppController : BaseAppController <ThoMoServerDelegateProtocol>
 {
 	ThoMoServerStub *thoMoServer;
 }
-- (void)initThoMoServer;
 @end
 
-#endif // __CC_PLATFORM_IOS
