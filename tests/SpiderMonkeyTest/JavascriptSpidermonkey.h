6169f6a567b96e5ede4c1bef8d9b461f08fccae6
Ricardo Quesada <ricardoquesada@gmail.com>
2012-06-29 12:33:33 -0700

    JS: ThoMo moved to JS only

--- tests/SpiderMonkeyTest/JavascriptSpidermonkey.h
+++ tests/SpiderMonkeyTest/JavascriptSpidermonkey.h
@@ -2,9 +2,22 @@
 #import "BaseAppController.h"
 #import "cocos2d.h"
 
+#ifdef __CC_PLATFORM_IOS
 
-//CLASS INTERFACE
 @interface AppController : BaseAppController
 {
+	ThoMoServerStub *thoMoServer;
 }
 @end
+
+#elif defined( __CC_PLATFORM_MAC )
+#import "ThoMoServerStub.h"
+
+@interface AppController : BaseAppController <ThoMoServerDelegateProtocol>
+{
+	ThoMoServerStub *thoMoServer;
+}
+- (void)initThoMoServer;
+@end
+
+#endif // __CC_PLATFORM_IOS
