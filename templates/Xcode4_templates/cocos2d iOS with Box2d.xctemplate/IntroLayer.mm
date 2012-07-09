11c99d3f7fd8ca0581ced4f67233e63b9f311fea
Ricardo Quesada <ricardoquesada@gmail.com>
2012-07-08 19:32:08 -0700

    Templates generates non-flicker code

---
+++ templates/Xcode4_templates/cocos2d iOS with Box2d.xctemplate/IntroLayer.mm
@@ -0,0 +1,65 @@
+//
+//  IntroLayer.m
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+
+// Import the interfaces
+#import "IntroLayer.h"
+#import "HelloWorldLayer.h"
+
+
+#pragma mark - IntroLayer
+
+// HelloWorldLayer implementation
+@implementation IntroLayer
+
+// Helper class method that creates a Scene with the HelloWorldLayer as the only child.
++(CCScene *) scene
+{
+	// 'scene' is an autorelease object.
+	CCScene *scene = [CCScene node];
+	
+	// 'layer' is an autorelease object.
+	IntroLayer *layer = [IntroLayer node];
+	
+	// add layer as a child to scene
+	[scene addChild: layer];
+	
+	// return the scene
+	return scene;
+}
+
+// 
+-(void) onEnter
+{
+	[super onEnter];
+
+	// ask director for the window size
+	CGSize size = [[CCDirector sharedDirector] winSize];
+
+	CCSprite *background;
+	
+	if( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ) {
+		background = [CCSprite spriteWithFile:@"Default.png"];
+		background.rotation = 90;
+	} else {
+		background = [CCSprite spriteWithFile:@"Default-Landscape~ipad.png"];
+	}
+	background.position = ccp(size.width/2, size.height/2);
+
+	// add the label as a child to this Layer
+	[self addChild: background];
+	
+	// In one second transition to the new scene
+	[self scheduleOnce:@selector(makeTransition:) delay:1];
+}
+
+-(void) makeTransition:(ccTime)dt
+{
+	[[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0 scene:[HelloWorldLayer scene] withColor:ccWHITE]];
+}
+@end
