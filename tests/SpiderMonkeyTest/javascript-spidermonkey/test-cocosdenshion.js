0f61830f78ca64d0cc038fa47554be11bb6ba62b
Ricardo Quesada <ricardoquesada@gmail.com>
2012-07-12 09:39:03 -0700

    JS: Simple subclassing is in the helpers.js only
    Former-commit-id: 32ef76c4780630f2a6eaa5410fa8213fe7898d66

--- tests/SpiderMonkeyTest/javascript-spidermonkey/test-cocosdenshion.js
+++ tests/SpiderMonkeyTest/javascript-spidermonkey/test-cocosdenshion.js
@@ -14,7 +14,6 @@ var _winSize = director.winSize();
 var winSize = {width:_winSize[0], height:_winSize[1]};
 var centerPos = cc.p( winSize.width/2, winSize.height/2 );
 
-//var MUSIC_FILE = "mula_tito_on_timbales.mp3";
 var MUSIC_FILE = "Cyber Advance!.mp3";
 var EFFECT_FILE = "cowbell.wav";
 
@@ -143,59 +142,6 @@ var DenshionTests = [
     }
 
 ];
-
-cc.LayerGradient.extend = function (prop) {
-    var _super = this.prototype;
-
-    // Instantiate a base class (but only create the instance,
-    // don't run the init constructor)
-    initializing = true;
-    var prototype = new this();
-    initializing = false;
-    fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-
-    // Copy the properties over onto the new prototype
-    for (var name in prop) {
-        // Check if we're overwriting an existing function
-        prototype[name] = typeof prop[name] == "function" &&
-            typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-            (function (name, fn) {
-                return function () {
-                    var tmp = this._super;
-
-                    // Add a new ._super() method that is the same method
-                    // but on the super-class
-                    this._super = _super[name];
-
-                    // The method only need to be bound temporarily, so we
-                    // remove it when we're done executing
-                    var ret = fn.apply(this, arguments);
-                    this._super = tmp;
-
-                    return ret;
-                };
-            })(name, prop[name]) :
-            prop[name];
-    }
-
-    // The dummy class constructor
-    function Class() {
-        // All construction is actually done in the init method
-        if (!initializing && this.ctor)
-            this.ctor.apply(this, arguments);
-    }
-
-    // Populate our constructed prototype object
-    Class.prototype = prototype;
-
-    // Enforce the constructor to be what we expect
-    Class.prototype.constructor = Class;
-
-    // And make this class extendable
-    Class.extend = arguments.callee;
-
-    return Class;
-};
 
 CocosDenshionTest = cc.LayerGradient.extend({
     _menu:null,
