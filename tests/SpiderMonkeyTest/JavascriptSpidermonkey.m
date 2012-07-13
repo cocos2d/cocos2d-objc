37c8b0968bd177c4b9e6564ccf16130c81ade568
Ricardo Quesada <ricardoquesada@gmail.com>
2012-07-13 14:41:54 -0700

    JS: RenderToTexture working
    Former-commit-id: 7cffc3f313504ca3078198f7e22ef977db862277

--- tests/SpiderMonkeyTest/JavascriptSpidermonkey.m
+++ tests/SpiderMonkeyTest/JavascriptSpidermonkey.m
@@ -202,8 +202,10 @@
 	[self initThoMoServer];
 #endif
 	
-	[[ScriptingCore sharedInstance] runScript:@"javascript-spidermonkey/main.js"];
+//	[[ScriptingCore sharedInstance] runScript:@"javascript-spidermonkey/main.js"];
 //	[[ScriptingCore sharedInstance] runScript:@"javascript-spidermonkey/playground.js"];
+	[[ScriptingCore sharedInstance] runScript:@"javascript-spidermonkey/test-rendertexture.js"];
+
 }
 @end
 
