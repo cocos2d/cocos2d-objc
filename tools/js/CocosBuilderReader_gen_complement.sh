44d7b0fc057d7c2371698bffbe0e7b9896bd038a
Ricardo Quesada <ricardoquesada@gmail.com>
2012-08-14 13:49:33 -0700

    removes the js scripts and configs files.
    They are in externals/jsbindings/config now
    
    
    Former-commit-id: 6ae06513579e357af8f23f9c6f970920336a8d4f

--- tools/js/CocosBuilderReader_gen_complement.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# run this script from the cocosBuilderReader directory. eg:
-# cd ~/src/cocos2d-iphone/externals/CocosBuilderReader/
-#
-../../tools/js/generate_complement.py -o ../../tools/js/CocosBuilderReader-complement.txt *.h
-
