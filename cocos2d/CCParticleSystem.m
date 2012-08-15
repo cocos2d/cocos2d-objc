09c5eca2db2e4a2cbc73b37a27091c9ac6cc318a
yang3wei <88480585@qq.com>
2012-08-15 10:00:17 +0800

    Update cocos2d/CCParticleSystem.m
    I found there'are some visual differences between retina and low resolution mode when i use a same .plist generated by ParticleDesigner.

--- cocos2d/CCParticleSystem.m
+++ cocos2d/CCParticleSystem.m
@@ -172,6 +172,10 @@
 			// gravity
 			mode.A.gravity.x = [[dictionary valueForKey:@"gravityx"] floatValue];
 			mode.A.gravity.y = [[dictionary valueForKey:@"gravityy"] floatValue];
+			
+			// There're some differences between high and low resolutions
+			mode.A.gravity.x *= CC_CONTENT_SCALE_FACTOR;
+			mode.A.gravity.y *= CC_CONTENT_SCALE_FACTOR;
 
 			//
 			// speed
