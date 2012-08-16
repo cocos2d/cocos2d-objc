3e2812df436460e5e97103d6b283f7a6868854a2
Ricardo Quesada <ricardoquesada@gmail.com>
2012-08-15 17:35:37 -0700

    JS bindings is an external repo
    Former-commit-id: 5f60d87445069023a9b7ddde752b1c630e15f75c

--- external/jsbindings/configs/CocosDenshion/gen_CocosDenshion_complement.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# run this script from the CocosDenshion directory. eg:
-# cd ~/src/cocos2d-iphone/CocosDenshion/CocosDenshion
-#
-../../tools/js/generate_complement.py -o ../../tools/js/CocosDenshion-complement.txt *.h
-
--- external/jsbindings/src/js/jsb_constants_chipmunk.js
+++
@@ -1,8 +0,0 @@
-//
-// Chipmunk defines
-//
-
-cp.v = cc.p;
-cp._v = cc._p;
-cp.vzero  = cp.v(0,0);
-
