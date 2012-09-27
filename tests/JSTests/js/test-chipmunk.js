c20226e217db8b138aaed13289e29ba707f62494
Ricardo Quesada <ricardoquesada@gmail.com>
2012-09-26 21:28:04 -0700

    chipmunk demo: started buoyancy test
    Former-commit-id: 8ef9967088589695c3fcc7a9d59af3bba848658b

--- tests/JSTests/js/test-chipmunk.js
+++ tests/JSTests/js/test-chipmunk.js
@@ -1,3 +1,29 @@
+ /*
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * Chipmunk Demo code:
+ *    Original Demo code written in C by Scott Lembcke
+ *    Ported to JavaScript by Joseph Gentle
+ *    Additions to the demo code by Ricardo Quesada
+ */
+
 //
 // http://www.cocos2d-iphone.org
 //
@@ -130,17 +156,14 @@ BaseLayer.prototype.onEnter = function() {
 };
 
 BaseLayer.prototype.restartCallback = function (sender) {
-    cc.log("restart called");
     restartSpriteTestAction();
 };
 
 BaseLayer.prototype.nextCallback = function (sender) {
-    cc.log("next called");
     nextSpriteTestAction();
 };
 
 BaseLayer.prototype.backCallback = function (sender) {
-    cc.log("back called");
     backSpriteTestAction();
 };
 
@@ -1022,11 +1045,198 @@ var Balls = function() {
 goog.inherits( Balls, ChipmunkDemo );
 
 
+//------------------------------------------------------------------
+//
+// Buoyancy
+//
+//------------------------------------------------------------------
+
+var FLUID_DENSITY = 0.00014;
+var FLUID_DRAG = 2.0;
+
+var Buoyancy = function() {
+	goog.base(this);
+	this.title = 'Chipmunk Demo';
+	this.subtitle = 'Buoyancy';
+
+	var space = this.space;
+	space.iterations = 30;
+	space.gravity = cp.v(0,-500);
+//	cpSpaceSetDamping(space, 0.5);
+	space.sleepTimeThreshold = 0.5;
+	space.collisionSlop = 0.5;
+	
+	var staticBody = space.staticBody;
+	
+	// Create segments around the edge of the screen.
+	var shape = space.addShape( new cp.SegmentShape(staticBody, cp.v(-320,-240), cp.v(-320,240), 0.0));
+	shape.setElasticity(1.0);
+	shape.setFriction(1.0);
+	shape.setLayers(NOT_GRABABLE_MASK);
+
+	shape = space.addShape( new cp.SegmentShape(staticBody, cp.v(320,-240), cp.v(320,240), 0.0));
+	shape.setElasticity(1.0);
+	shape.setFriction(1.0);
+	shape.setLayers(NOT_GRABABLE_MASK);
+
+	shape = space.addShape( new cp.SegmentShape(staticBody, cp.v(-320,-240), cp.v(320,-240), 0.0));
+	shape.setElasticity(1.0);
+	shape.setFriction(1.0);
+	shape.setLayers(NOT_GRABABLE_MASK);
+	
+	shape = space.addShape( new cp.SegmentShape(staticBody, cp.v(-320,240), cp.v(320,240), 0.0));
+	shape.setElasticity(1.0);
+	shape.setFriction(1.0);
+	shape.setLayers(NOT_GRABABLE_MASK);
+	
+	// {
+		// Add the edges of the bucket
+		var bb = new cp.BB(-300, -200, 100, 0);
+		var radius = 5.0;
+		
+		shape = space.addShape( new cp.SegmentShape(staticBody, cp.v(bb.l, bb.b), cp.v(bb.l, bb.t), radius));
+		shape.setElasticity(1.0);
+		shape.setFriction(1.0);
+		shape.setLayers(NOT_GRABABLE_MASK);
+
+		shape = space.addShape( new cp.SegmentShape(staticBody, cp.v(bb.r, bb.b), cp.v(bb.r, bb.t), radius));
+		shape.setElasticity(1.0);
+		shape.setFriction(1.0);
+		shape.setLayers(NOT_GRABABLE_MASK);
+
+		shape = space.addShape( new cp.SegmentShape(staticBody, cp.v(bb.l, bb.b), cp.v(bb.r, bb.b), radius));
+		shape.setElasticity(1.0);
+		shape.setFriction(1.0);
+		shape.setLayers(NOT_GRABABLE_MASK);
+		
+		// Add the sensor for the water.
+		shape = space.addShape( new cp.BoxShape2(staticBody, bb) );
+		shape.setSensor(true);
+		shape.setCollisionType(1);
+	// }
+
+
+	// {
+		var width = 200.0;
+		var height = 50.0;
+		var mass = 0.3*FLUID_DENSITY*width*height;
+		var moment = cp.momentForBox(mass, width, height);
+		
+		body = space.addBody( new cp.Body(mass, moment));
+		body.setPos( cp.v(-50, -100));
+		body.setVel( cp.v(0, -100));
+		body.setAngVel( 1 );
+		
+		shape = space.addShape( new cp.BoxShape(body, width, height));
+		shape.setFriction(0.8);
+	// }
+	
+	// {
+		width = 40.0;
+		height = width*2;
+		mass = 0.3*FLUID_DENSITY*width*height;
+		moment = cp.momentForBox(mass, width, height);
+		
+		body = space.addBody( new cp.Body(mass, moment));
+		body.setPos(cp.v(-200, -50));
+		body.setVel(cp.v(0, -100));
+		body.setAngVel(1);
+		
+		shape = space.addShape(new cp.BoxShape(body, width, height));
+		shape.setFriction(0.8);
+	// }
+	
+	space.addCollisionHandler( 1, 0, null, this.waterPreSolve.bind(this), null, null);
+};
+goog.inherits( Buoyancy, ChipmunkDemo );
+
+Buoyancy.prototype.update = function(dt)
+{
+	var steps = 3;
+	dt /= steps;
+	for (var i = 0; i < 3; i++){
+		this.space.step(dt);
+	}
+};
+
+Buoyancy.prototype.waterPreSolve = function(arb, space, ptr) {
+	var shapes = arb.getShapes();
+	cc.log(shapes);
+	var water = shapes[0];
+	var poly = shapes[1];
+
+	cc.log( water.getCollisionType() );
+	cc.log( poly.getCollisionType() );
+	cc.log( poly.getFriction() );
+
+	var body = poly.getBody();
+
+	// Get the top of the water sensor bounding box to use as the water level.
+	var level = water.getBB().t;
+
+	// Clip the polygon against the water level
+	var count = poly.getNumVerts();
+
+	var clippedCount = 0;
+	var clipped = [];
+
+	var j=count-1;
+	for(var i=0; i<count; i++) {
+		var a = body.local2World( poly.getVert(j));
+		var b = body.local2World( poly.getVert(i));
+		
+		if(a.y < level){
+			clipped.push( a );
+			clippedCount++;
+		}
+		
+		var a_level = a.y - level;
+		var b_level = b.y - level;
+		
+		if(a_level*b_level < 0.0){
+			var t = Math.abs(a_level)/(Math.abs(a_level) + Math.abs(b_level));
+			
+			clipped.push( cp.v.lerp(a, b, t) );
+			clippedCount++;
+		}
+
+		j=i;
+	}
+	
+	// Calculate buoyancy from the clipped polygon area
+	var clippedArea = cp.areaForPoly(clippedCount, clipped);
+	var displacedMass = clippedArea*FLUID_DENSITY;
+	var centroid = cp.centroidForPoly(clippedCount, clipped);
+	var r = cp.v.sub(centroid, body.getPos());
+		
+	var dt = this.space.getCurrentTimeStep();
+	var g = this.space.gravity;
+	
+	// Apply the buoyancy force as an impulse.
+	body.applyImpulse( cp.v.mult(g, -displacedMass*dt), r);
+	
+	// Apply linear damping for the fluid drag.
+	var v_centroid = cp.v.add(body.v, cp.v.mult(cp.v.perp(r), body.w));
+	var k = k_scalar_body(body, r, cp.v.normalize_safe(v_centroid));
+	var damping = clippedArea*FLUID_DRAG*FLUID_DENSITY;
+	var v_coef = Math.exp(-damping*dt*k); // linear drag
+//	cpFloat v_coef = 1.0/(1.0 + damping*dt*cpvlength(v_centroid)*k); // quadratic drag
+	body.applyImpulse( cp.v.mult(cp.v.sub(cp.v.mult(v_centroid, v_coef), v_centroid), 1.0/k), r);
+	
+	// Apply angular damping for the fluid drag.
+	var w_damping = cp.momentForPoly(FLUID_DRAG*FLUID_DENSITY*clippedArea, clippedCount, clipped, cp.v.neg(body.p));
+	body.w *= Math.exp(-w_damping*dt*body.i_inv);
+	
+	return true;
+};
+
+
 //
 // Order of tests
 //
 
 // Chipmunk Demos
+scenes.push( Buoyancy );
 scenes.push( PyramidStack );
 scenes.push( PyramidTopple );
 scenes.push( Joints );
