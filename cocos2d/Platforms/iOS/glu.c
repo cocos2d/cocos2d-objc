77993b460f35800f8f4cc07e20c03f222897184f
Marco Tillemans <marcotillemans@gmail.com>
2012-11-10 05:36:21 +0100

    gluLookAt a bit more efficient, added convenience method to retrieve gluLookAt matrix.
    -removed glTranslate call, expressed it in matrix. 
    -used matrix layout from http://www.opengl.org/archives/resources/faq/technical/transformations.htm

--- cocos2d/Platforms/iOS/glu.c
+++ cocos2d/Platforms/iOS/glu.c
@@ -14,6 +14,7 @@
 #import <OpenGLES/ES1/gl.h>
 #import <math.h>
 #import "../../Support/OpenGL_Internal.h"
+#include <stdlib.h>
 #include "glu.h"
 
 void gluPerspective(GLfloat fovy, GLfloat aspect, GLfloat zNear, GLfloat zFar)
@@ -84,30 +85,99 @@ void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez,
         y[2] /= mag;
     }
 
-#define M(row,col)  m[col*4+row]
-    M(0, 0) = x[0];
-    M(0, 1) = x[1];
-    M(0, 2) = x[2];
-    M(0, 3) = 0.0f;
-    M(1, 0) = y[0];
-    M(1, 1) = y[1];
-    M(1, 2) = y[2];
-    M(1, 3) = 0.0f;
-    M(2, 0) = z[0];
-    M(2, 1) = z[1];
-    M(2, 2) = z[2];
-    M(2, 3) = 0.0f;
-    M(3, 0) = 0.0f;
-    M(3, 1) = 0.0f;
-    M(3, 2) = 0.0f;
-    M(3, 3) = 1.0f;
-#undef M
-
+    m[0] = x[0];
+    m[1] = x[1];
+    m[2] = x[2];
+    m[3] = 0.0f;
+    m[4] = y[0];
+    m[5] = y[1];
+    m[6] = y[2];
+    m[7] = 0.0f;
+    m[8] = z[0];
+    m[9] = z[1];
+    m[10] = z[2];
+    m[11] = 0.0f;
+    m[12] = -eyex;
+    m[13] = -eyey;
+    m[14] = -eyez;
+    m[15] = 1.0f;
+   
 	glMultMatrixf(m);
-
-
-    /* Translate Eye to Origin */
-    glTranslatef(-eyex, -eyey, -eyez);
 }
+
+GLfloat* gluLookAtMatrix(GLfloat eyex, GLfloat eyey, GLfloat eyez,
+               GLfloat centerx, GLfloat centery, GLfloat centerz,
+               GLfloat upx, GLfloat upy, GLfloat upz)
+{
+    GLfloat *m = malloc(sizeof(GLfloat)*16);
+    GLfloat x[3], y[3], z[3];
+    GLfloat mag;
+    
+    /* Make rotation matrix */
+    
+    /* Z vector */
+    z[0] = eyex - centerx;
+    z[1] = eyey - centery;
+    z[2] = eyez - centerz;
+    mag = (float)sqrtf(z[0] * z[0] + z[1] * z[1] + z[2] * z[2]);
+    if (mag) {
+        z[0] /= mag;
+        z[1] /= mag;
+        z[2] /= mag;
+    }
+    
+    /* Y vector */
+    y[0] = upx;
+    y[1] = upy;
+    y[2] = upz;
+    
+    /* X vector = Y cross Z */
+    x[0] = y[1] * z[2] - y[2] * z[1];
+    x[1] = -y[0] * z[2] + y[2] * z[0];
+    x[2] = y[0] * z[1] - y[1] * z[0];
+    
+    /* Recompute Y = Z cross X */
+    y[0] = z[1] * x[2] - z[2] * x[1];
+    y[1] = -z[0] * x[2] + z[2] * x[0];
+    y[2] = z[0] * x[1] - z[1] * x[0];
+    
+    /* cross product gives area of parallelogram, which is < 1.0 for
+     * non-perpendicular unit-length vectors; so normalize x, y here
+     */
+    
+    mag = (float)sqrtf(x[0] * x[0] + x[1] * x[1] + x[2] * x[2]);
+    if (mag) {
+        x[0] /= mag;
+        x[1] /= mag;
+        x[2] /= mag;
+    }
+    
+    mag = (float)sqrtf(y[0] * y[0] + y[1] * y[1] + y[2] * y[2]);
+    if (mag) {
+        y[0] /= mag;
+        y[1] /= mag;
+        y[2] /= mag;
+    }
+    
+    m[0] = x[0];
+    m[1] = x[1];
+    m[2] = x[2];
+    m[3] = 0.0f;
+    m[4] = y[0];
+    m[5] = y[1];
+    m[6] = y[2];
+    m[7] = 0.0f;
+    m[8] = z[0];
+    m[9] = z[1];
+    m[10] = z[2];
+    m[11] = 0.0f;
+    m[12] = -eyex;
+    m[13] = -eyey;
+    m[14] = -eyez;
+    m[15] = 1.0f;
+    
+	return m; 
+}
+
 
 #endif // __IPHONE_OS_VERSION_MAX_ALLOWED
--- cocos2d/Platforms/iOS/glu.h
+++ cocos2d/Platforms/iOS/glu.h
@@ -23,6 +23,11 @@ void gluLookAt(float eyeX, float eyeY, float eyeZ, float lookAtX, float lookAtY,
 /** OpenGL gluPerspective implementation */
 void gluPerspective(GLfloat fovy, GLfloat aspect, GLfloat zNear, GLfloat zFar);
 
+GLfloat* gluLookAtMatrix(GLfloat eyex, GLfloat eyey, GLfloat eyez,
+                         GLfloat centerx, GLfloat centery, GLfloat centerz,
+                         GLfloat upx, GLfloat upy, GLfloat upz);
+
+void mult_44(float matrix1[4][4], float matrix2[4][4], float result[4][4]);
 #endif // __IPHONE_OS_VERSION_MAX_ALLOWED
 
 #endif /* __COCOS2D_GLU_H */
