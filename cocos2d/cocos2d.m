4983918ea1d12ad42ab84ae8d75821646c9b744b
Ricardo Quesada <ricardoquesada@gmail.com>
2012-11-13 00:04:43 -0300

    Using cocos2d-js-tests
    ...instead of cocos2d-html5-tests repo.
    
    Also, the js files are in a "blue" folder, instead of a yellow one.
    Easier to add new files to the project.
    
    
    Former-commit-id: 9eaaa17e13ba834befbb1e740839f7e916d9779e

--- cocos2d/cocos2d.m
+++ cocos2d/cocos2d.m
@@ -26,7 +26,7 @@
 
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
-static NSString *version = @"cocos2d v2.1-beta3";
+static NSString *version = @"cocos2d v2.1-beta4-pre";
 
 NSString *cocos2dVersion()
 {
