5470fad1da020255d4763d788855ef654f610366
Kefu Chai <tchaikov@gmail.com>
2013-01-02 18:17:02 +0800

    Protect CCConfiguration accessor with sync lock

--- cocos2d/CCConfiguration.m
+++ cocos2d/CCConfiguration.m
@@ -55,9 +55,10 @@ static char * glExtensions;
 
 + (CCConfiguration *)sharedConfiguration
 {
-	if (!_sharedConfiguration)
-		_sharedConfiguration = [[self alloc] init];
-
+	@synchronized(self)     {
+		if (!_sharedConfiguration)
+			_sharedConfiguration = [[self alloc] init];
+	}
 	return _sharedConfiguration;
 }
 
