99fd1d226046fb9403135093e26f4ec9b3a9eff3
Ricardo Quesada <ricardoquesada@gmail.com>
2013-02-07 21:02:34 -0300

    Version is v2.1rc0a

--- cocos2d/cocos2d.m
+++ cocos2d/cocos2d.m
@@ -26,7 +26,7 @@
 
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
-const char *cocos2d_version = "cocos2d-iphone v2.1-rc0";
+const char *cocos2d_version = "cocos2d-iphone v2.1-rc0a";
 
 NSString *cocos2dVersion()
 {
