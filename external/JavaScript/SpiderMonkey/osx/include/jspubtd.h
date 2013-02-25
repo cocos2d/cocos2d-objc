945b3005dff2d7ef0ceffdf43eb25baf05ce50ff
Ricardo Quesada <ricardoquesada@gmail.com>
2013-02-25 13:41:02 -0800

    SpiderMonkey: works OK on DEBUG and RELEASE builds

--- external/JavaScript/SpiderMonkey/osx/include/jspubtd.h
+++ external/JavaScript/SpiderMonkey/osx/include/jspubtd.h
@@ -39,6 +39,8 @@ namespace JS { class Value; }
  */
 #ifdef __cplusplus
 
+#define JS_NO_JSVAL_JSID_STRUCT_TYPES
+
 # if defined(DEBUG) && !defined(JS_NO_JSVAL_JSID_STRUCT_TYPES)
 #  define JS_USE_JSID_STRUCT_TYPES
 # endif
