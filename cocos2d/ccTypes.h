d1464c3a6e9ab99643270571a3421dc827eb8f83
carlo morgantini <cmorgantini@zynga.com>
2013-06-05 17:20:33 -0700

    Shadow + Stroke for CCLabelTTF. Also new objectiveC test added.

--- cocos2d/ccTypes.h
+++ cocos2d/ccTypes.h
@@ -385,7 +385,57 @@ typedef CGFloat ccTime;
 //typedef double ccTime;
 
 typedef float ccMat4[16];
-	
+    
+    
+typedef struct _ccFontShadow
+{
+    // true if shadow enabled
+    bool   m_shadowEnabled;
+    // shadow x and y offset
+    CGSize m_shadowOffset;
+    // shadow blurrines
+    float  m_shadowBlur;
+    // shadow opacity
+    float  m_shadowOpacity;
+    
+} ccFontShadow;
+
+typedef struct _ccFontStroke
+{
+    // true if stroke enabled
+    bool        m_strokeEnabled;
+    // stroke color
+    ccColor3B   m_strokeColor;
+    // stroke size
+    float       m_strokeSize;
+    
+} ccFontStroke;
+    
+typedef struct _ccFontDefinition
+{
+    // font name
+    NSString                *m_fontName;
+    // font size
+    int                     m_fontSize;
+    // horizontal alignment
+    CCTextAlignment         m_alignment;
+    // vertical alignment
+    CCVerticalTextAlignment m_vertAlignment;
+    // line break mode
+    CCLineBreakMode         m_lineBreakMode;
+    // renering box
+    CGSize                  m_dimensions;
+    // font color
+    ccColor3B               m_fontFillColor;
+    // font shadow
+    ccFontShadow            m_shadow;
+    // font stroke
+    ccFontStroke            m_stroke;
+    
+} ccFontDefinition;
+
+
+    
 #ifdef __cplusplus
 }
 #endif
