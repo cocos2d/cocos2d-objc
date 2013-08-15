8b7abbd1cba68521c66f93f3155c70a77818a54d
Viktor <Lidholt@Viktors-MacBook-Pro.local>
2013-08-15 14:29:18 -0700

    Possibly fixed issue in scheduler with overreleased objects

--- cocos2d/CCScheduler.m
+++ cocos2d/CCScheduler.m
@@ -632,12 +632,14 @@ typedef struct _hashSelectorEntry
 		free( element->entry );
 		
 		// hash entry
-		id target = element->target;
+		//id target = element->target;
 		HASH_DEL( hashForUpdates, element);
 		free(element);
 		
 		// target#release should be the last one to prevent
 		// a possible double-free. eg: If the [target dealloc] might want to remove it itself from there
+#warning There may be a memory leak here (but cannot verify with instruments)!
+        //CFRelease(t);
 	}
 }
 
