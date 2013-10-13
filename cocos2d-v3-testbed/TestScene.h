04957d3e2676279cda68fdf28f32176400f902ab
Birkemose <lars.birkemose@gmail.com>
2013-10-13 18:45:57 +0200

    Testbed skeleton

---
+++ cocos2d-v3-testbed/TestScene.h
@@ -0,0 +1,53 @@
+/*
+ * cocos2d for iPhone: http://www.cocos2d-iphone.org
+ *
+ * Copyright (c) 2008-2010 Ricardo Quesada
+ * Copyright (c) 2011 Zynga Inc.
+ * Copyright (c) 2013 Lars Birkemose
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+// -----------------------------------------------------------------
+/**
+ *  TestScene is the baseclass for all test scenes created for the test bed
+ *
+ *  Creating a new test scene, takes two steps
+ *  1) create a class, based on TestScene
+ *  2) add the name of the class, to the list of test classes on TestBedMainScene.m
+ *
+ *
+ *
+ */
+@interface TestScene : CCScene
+
+// -----------------------------------------------------------------
+
+- (id)init;
+
++ (UIUserInterfaceLayoutDirection)layout;
++ (NSString *)group;
++ (NSString *)title;
+
+// -----------------------------------------------------------------
+
+@end
