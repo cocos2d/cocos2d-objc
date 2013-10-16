ea95ef5ec0902faf7e554dc85cc8caafdee0594e
Viktor <viktorlidholt@gmail.com>
2013-10-16 16:55:17 -0700

    Added tests for basic transitions

--- cocos2d-ui-tests/tests/CCTransitionTest.m
+++ cocos2d-ui-tests/tests/CCTransitionTest.m
@@ -10,4 +10,48 @@
 
 @implementation CCTransitionTest
 
+- (NSArray*) testConstructors
+{
+    return [NSArray arrayWithObjects:
+            @"setupCrossFadeTest",
+            @"setupFadeWithColorTest",
+            // TODO: Fix all tests
+            nil];
+}
+
+- (void) setupCrossFadeTest
+{
+    self.subTitle = @"Cross fade";
+    _nextTransition = [CCTransition crossFadeWithDuration:1];
+    
+    CCLayerColor* bgLayer = [CCLayerColor layerWithColor:ccc4(255, 0, 0, 255)];
+    bgLayer.contentSize = CGSizeMake(1, 1);
+    bgLayer.contentSizeType = kCCContentSizeTypeNormalized;
+    [self.contentNode addChild:bgLayer];
+}
+
+- (void) setupFadeWithColorTest
+{
+    self.subTitle = @"Fade with Color";
+    _nextTransition = [CCTransition fadeWithColor:ccc3(0, 0, 0) duration:1];
+    
+    CCLayerColor* bgLayer = [CCLayerColor layerWithColor:ccc4(0, 255, 0, 255)];
+    bgLayer.contentSize = CGSizeMake(1, 1);
+    bgLayer.contentSizeType = kCCContentSizeTypeNormalized;
+    [self.contentNode addChild:bgLayer];
+}
+
+// Overridden methods to support transitions
+
+- (void)  pressedNext:(id)sender
+{
+    NSInteger newTest = _currentTest + 1;
+    if (newTest >= self.testConstructors.count) newTest = 0;
+    
+    CCScene* testScene = [TestBase sceneWithTestName:self.testName];
+    [[[testScene children] objectAtIndex:0] setupTestWithIndex:newTest];
+    
+    [[CCDirector sharedDirector] replaceScene:testScene withTransition:_nextTransition];
+}
+
 @end
