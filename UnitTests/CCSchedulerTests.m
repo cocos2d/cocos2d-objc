b9777169b6af51f3d534c52b437cbdfca1386f5b
slembcke <slembcke@gmail.com>
2013-10-22 15:45:23 -0500

    Unit testing and bug fixes.
    Former-commit-id: 9cb1f2b32ce792661057df85e9e729dba48e23ba

--- UnitTests/CCSchedulerTests.m
+++ UnitTests/CCSchedulerTests.m
@@ -9,39 +9,104 @@
 #import <XCTest/XCTest.h>
 #import "cocos2d.h"
 
+
+@interface SchedulerTarget : NSObject<CCSchedulerTarget>
+@property(nonatomic, strong) NSMutableArray *sequence;
+@property(nonatomic, assign) NSInteger priority;
+@end
+
+
+@implementation SchedulerTarget
+
+-(void)update:(ccTime)delta
+{
+	[_sequence addObject:[NSString stringWithFormat:@"update:%f", delta]];
+}
+
+-(void)fixedUpdate:(ccTime)delta
+{
+	[_sequence addObject:[NSString stringWithFormat:@"fixedUpdate:%f", delta]];
+}
+
+@end
+
+
 @interface CCSchedulerTests : XCTestCase
 
 @end
 
 @implementation CCSchedulerTests {
-	NSArray *_sequence;
 }
 
-- (void)setUp
+- (void)testInvoke0
 {
-    [super setUp];
-}
-
-- (void)tearDown
-{
-    [super tearDown];
-}
-
--(void)tick:(ccTime)dt
-{
-	_sequence = [_sequence arrayByAddingObject:[NSString stringWithFormat:@"%.1f", dt]];
-}
-
-- (void)testSchedule
-{
+	NSMutableArray *seq = [NSMutableArray array];
 	CCScheduler *scheduler = [[CCScheduler alloc] init];
+	scheduler.maxTimeStep = INFINITY;
+	// Easier to debug if it doesn't call fixed updates.
+	scheduler.fixedTimeStep = INFINITY;
 	
-	_sequence = @[];
-	[scheduler scheduleSelector:@selector(tick:) forTarget:self interval:1.0 paused:NO];
+	[scheduler scheduleBlock:^(CCTimer *timer){
+			[seq addObject:@(timer.invokeTime)];
+	} forTarget:nil withDelay:0.0];
 	
 	[scheduler update:0.0];
-	[scheduler update:3.0];
-	XCTAssertEqualObjects(_sequence,  (@[@"1.0", @"1.0", @"1.0"]), @"");
+	
+	XCTAssertEqualObjects(seq, (@[@0.0]), @"");
+}
+
+- (void)testInvokeDelay
+{
+	NSMutableArray *seq = [NSMutableArray array];
+	CCScheduler *scheduler = [[CCScheduler alloc] init];
+	scheduler.maxTimeStep = INFINITY;
+	// Easier to debug if it doesn't call fixed updates.
+	scheduler.fixedTimeStep = INFINITY;
+	
+	[scheduler scheduleBlock:^(CCTimer *timer){
+			[seq addObject:@(timer.invokeTime)];
+	} forTarget:nil withDelay:1.0];
+	
+	[scheduler update:1.0];
+	
+	XCTAssertEqualObjects(seq, (@[@1.0]), @"");
+}
+
+- (void)testInvokeDelay2
+{
+	NSMutableArray *seq = [NSMutableArray array];
+	CCScheduler *scheduler = [[CCScheduler alloc] init];
+	scheduler.maxTimeStep = INFINITY;
+	// Easier to debug if it doesn't call fixed updates.
+	scheduler.fixedTimeStep = INFINITY;
+	
+	[scheduler scheduleBlock:^(CCTimer *timer){
+			[seq addObject:@(timer.invokeTime)];
+	} forTarget:nil withDelay:1.0];
+	
+	// Updating beyond the timestep should still work.
+	[scheduler update:10.0];
+	
+	XCTAssertEqualObjects(seq, (@[@1.0]), @"");
+}
+
+- (void)testInvokeReschedule
+{
+	NSMutableArray *seq = [NSMutableArray array];
+	CCScheduler *scheduler = [[CCScheduler alloc] init];
+	scheduler.maxTimeStep = INFINITY;
+	// Easier to debug if it doesn't call fixed updates.
+	scheduler.fixedTimeStep = INFINITY;
+	
+	[scheduler scheduleBlock:^(CCTimer *timer){
+			[seq addObject:@(timer.invokeTime)];
+			[timer repeatOnceWithInterval:0.5];
+	} forTarget:nil withDelay:1.0];
+	
+	// Updating beyond the timestep should still work.
+	[scheduler update:2.0];
+	
+	XCTAssertEqualObjects(seq, (@[@1.0, @1.5, @2.0]), @"");
 }
 
 @end
