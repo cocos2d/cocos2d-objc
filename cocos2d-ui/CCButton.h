a4a589acd232dbde05a0e332014fb81ea3730c11
Viktor <viktorlidholt@gmail.com>
2013-10-24 16:59:08 -0700

    Adds documentation to CCButton
    Former-commit-id: da61b720bf3af31fed53ad6733c9d327307ae131

--- cocos2d-ui/CCButton.h
+++ cocos2d-ui/CCButton.h
@@ -87,26 +87,144 @@
  *  @return A new button.
  */
 + (id) buttonWithTitle:(NSString*) title spriteFrame:(CCSpriteFrame*) spriteFrame;
+
+/**
+ *  Creates a new button with the speicified title for the label, sprite frames for its background in different states.
+ *
+ *  @param title       The title text of the button.
+ *  @param spriteFrame Stretchable background image for the normal state.
+ *  @param highlighted Stretchable background image for the highlighted state.
+ *  @param disabled    Stretchable background image for the disabled state.
+ *
+ *  @return A new button.
+ */
 + (id) buttonWithTitle:(NSString*) title spriteFrame:(CCSpriteFrame*) spriteFrame highlightedSpriteFrame:(CCSpriteFrame*) highlighted disabledSpriteFrame:(CCSpriteFrame*) disabled;
 
+/**
+ *  Initializes a new button with a title and no background. Uses default font and font size.
+ *
+ *  @param title The title text of the button.
+ *
+ *  @return A new button.
+ */
 - (id) initWithTitle:(NSString*) title;
+
+/**
+ *  Initializes a new button with a title and no background.
+ *
+ *  @param title    The title text of the button.
+ *  @param fontName Name of the TTF font to use for the title label.
+ *  @param size     Font size for the title label.
+ *
+ *  @return A new button.
+ */
 - (id) initWithTitle:(NSString *)title fontName:(NSString*)fontName fontSize:(float)size;
+
+/**
+ *  Initializes a new button with the specified title for the label and sprite frame for its background.
+ *
+ *  @param title       The title text of the button.
+ *  @param spriteFrame Stretchable background image.
+ *
+ *  @return A new button.
+ */
 - (id) initWithTitle:(NSString*) title spriteFrame:(CCSpriteFrame*) spriteFrame;
+
+/**
+ *  Initializes a new button with the speicified title for the label, sprite frames for its background in different states.
+ *
+ *  @param title       The title text of the button.
+ *  @param spriteFrame Stretchable background image for the normal state.
+ *  @param highlighted Stretchable background image for the highlighted state.
+ *  @param disabled    Stretchable background image for the disabled state.
+ *
+ *  @return A new button.
+ */
 - (id) initWithTitle:(NSString*) title spriteFrame:(CCSpriteFrame*) spriteFrame highlightedSpriteFrame:(CCSpriteFrame*) highlighted disabledSpriteFrame:(CCSpriteFrame*) disabled;
 
+/**
+ *  Sets the background color for the specified state. The color is multiplied into the background sprite frame.
+ *
+ *  @param color Color applied to background image.
+ *  @param state State to apply the color to.
+ */
 - (void) setBackgroundColor:(ccColor3B) color forState:(CCControlState) state;
+
+/**
+ *  Gets the background color for the specified state.
+ *
+ *  @param state State to get the color for.
+ *
+ *  @return Background color.
+ */
+- (ccColor3B) backgroundColorForState:(CCControlState)state;
+
+/**
+ *  Sets the background's opacity for the specified state.
+ *
+ *  @param opacity Opacity to apply to the background image
+ *  @param state   State to apply the opacity to.
+ */
 - (void) setBackgroundOpacity:(GLubyte) opacity forState:(CCControlState) state;
 
-- (void) setLabelColor:(ccColor3B) color forState:(CCControlState) state;
-- (void) setLabelOpacity:(GLubyte) opacity forState:(CCControlState) state;
-
-- (ccColor3B) backgroundColorForState:(CCControlState)state;
+/**
+ *  Gets the background opacity for the specified state.
+ *
+ *  @param state State to get the opacity for.
+ *
+ *  @return Opacity.
+ */
 - (GLubyte) backgroundOpacityForState:(CCControlState)state;
 
+/**
+ *  Sets the label's color for the specified state.
+ *
+ *  @param color Color applied to the label.
+ *  @param state State to set the color for.
+ */
+- (void) setLabelColor:(ccColor3B) color forState:(CCControlState) state;
+
+/**
+ *  Gets the label's color for the specified state.
+ *
+ *  @param state State to get the color for.
+ *
+ *  @return Label color.
+ */
 - (ccColor3B) labelColorForState:(CCControlState) state;
+
+/**
+ *  Sets the label's opacity for the specified state.
+ *
+ *  @param opacity Opacity applied to the label.
+ *  @param state   State to set the opacity for.
+ */
+- (void) setLabelOpacity:(GLubyte) opacity forState:(CCControlState) state;
+
+/**
+ *  Gets the label's opacity for the specified state.
+ *
+ *  @param state State to get the opacity for.
+ *
+ *  @return Label opacity.
+ */
 - (GLubyte) labelOpacityForState:(CCControlState) state;
 
+/**
+ *  Sets the background's sprite frame for the specified state. The sprite frame will be stretched to the preferred size of the label. If set to `NULL` no background will be drawn.
+ *
+ *  @param spriteFrame Sprite frame to use for drawing the background.
+ *  @param state       State to set the background for.
+ */
 - (void) setBackgroundSpriteFrame:(CCSpriteFrame*)spriteFrame forState:(CCControlState)state;
+
+/**
+ *  Gets the background's sprite frame for the specified state.
+ *
+ *  @param state State to get the sprite frame for.
+ *
+ *  @return Background sprite frame.
+ */
 - (CCSpriteFrame*) backgroundSpriteFrameForState:(CCControlState)state;
 
 @end
