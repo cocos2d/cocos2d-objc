0cbe86f0959c3c3d19359f38f93dd80c4197c27d
Siarhei Fiedartsou <siarhei.fedartsou@gmail.com>
2013-11-19 20:39:39 +0300

    Fix for custom glyph collections

--- cocos2d/CCFontCoreGraphics.m
+++ cocos2d/CCFontCoreGraphics.m
@@ -14,6 +14,7 @@
 
 #import "CCFontDefinition.h"
 #import "CCTextImage.h"
+#import "CCFont_Internal.h"
 
 @implementation CCFontCoreGraphics
 {
@@ -38,6 +39,8 @@
         _isDynamicGlyphCollection = glyphs == CCGlyphCollectionDynamic;
         fontName_ = fontName;
         fontSize_ = fontSize;
+        
+        [self setCurrentGlyphCollection:glyphs customGlyphs:customGlyphs];
     }
     return self;
 }
