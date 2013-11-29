e334321465e2a89a2d3045c2691cb17858baedee
Birkemose <lars.birkemose@gmail.com>
2013-11-29 14:47:12 +0100

    CCActionTween documentation

--- cocos2d/CCActionTween.h
+++ cocos2d/CCActionTween.h
@@ -27,23 +27,17 @@
 #import <Foundation/Foundation.h>
 #import "CCActionInterval.h"
 
-/** CCActionTween
+// -----------------------------------------------------------------
+/** @name CCActionTween */
 
- CCActionTween is an action that lets you update any property of an object.
- For example, if you want to modify the "width" property of a target from 200 to 300 in 2 seconds, then:
-
-	id modifyWidth = [CCActionTween actionWithDuration:2 key:@"width" from:200 to:300];
-	[target runAction:modifyWidth];
-
-
- Another example: CCScaleTo action could be rewriten using CCPropertyAction:
-
-	// scaleA and scaleB are equivalents
-	id scaleA = [CCScaleTo actionWithDuration:2 scale:3];
-	id scaleB = [CCActionTween actionWithDuration:2 key:@"scale" from:1 to:3];
-
-
- @since v0.99.2
+/**
+ *  CCActionTween is an action that lets you update any property of an object.
+ *  Examples: 
+ *  1) Modify the width property of a target from 200 to 300 in 2 seconds:
+ *  - [target runAction:[CCActionTween actionWithDuration:2 key:@"width" from:200 to:300]]
+ *
+ *  2) Modify the scale property from 1 to 3 in 5 seconds
+ *  - [target runAction:[CCActionTween actionWithDuration:5 key:@"scale" from:1 to:3]]
  */
 @interface CCActionTween : CCActionInterval
 {
@@ -53,10 +47,28 @@
 	float			_delta;
 }
 
-/** creates an initializes the action with the property name (key), and the from and to parameters. */
+/**
+ *  Creates an initializes a tween action
+ *
+ *  @param aDuration Action duration
+ *  @param key       Property key to modify
+ *  @param from      Value to tween from
+ *  @param to        Value to tween to
+ *
+ *  @return New tween action
+ */
 + (id)actionWithDuration:(CCTime)aDuration key:(NSString *)key from:(float)from to:(float)to;
 
-/** initializes the action with the property name (key), and the from and to parameters. */
+/**
+ *  Initializes an initializes a tween action
+ *
+ *  @param aDuration Action duration
+ *  @param key       Property key to modify
+ *  @param from      Value to tween from
+ *  @param to        Value to tween to
+ *
+ *  @return New tween action
+ */
 - (id)initWithDuration:(CCTime)aDuration key:(NSString *)key from:(float)from to:(float)to;
 
 @end
