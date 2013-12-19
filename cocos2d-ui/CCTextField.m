190c2cd57aa818716c26df62af230a560af18a8a
slembcke <slembcke@gmail.com>
2013-12-19 14:08:33 -0600

    CCTextField fixes for custom contentScale

--- cocos2d-ui/CCTextField.m
+++ cocos2d-ui/CCTextField.m
@@ -59,6 +59,13 @@
     _textField = [[UITextField alloc] initWithFrame:CGRectZero];
     _textField.delegate = self;
     _textField.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
+		_textField.backgroundColor = [UIColor greenColor];
+		
+		// UIKit might not be running in the same scale as us.
+		_scaleMultiplier = [CCDirector sharedDirector].contentScaleFactor/[UIScreen mainScreen].scale;
+		
+		UIFont *font = _textField.font;
+		_textField.font = [font fontWithSize:font.pointSize*_scaleMultiplier];
     
 #elif defined(__CC_PLATFORM_MAC)
     
@@ -71,6 +78,9 @@
     [_textField setBackgroundColor:[NSColor colorWithCalibratedRed:0 green:0 blue:0 alpha:0]];
     [_textField setWantsLayer:YES];
     
+		// TODO need to handle content sizing on Mac
+		_scaleMultiplier = 1.0;
+		
 #endif
     
     _padding = 4;
@@ -87,6 +97,9 @@
     viewPos.y += _padding;
     
     CGSize size = self.contentSizeInPoints;
+		size.width *= _scaleMultiplier;
+		size.height *= _scaleMultiplier;
+		
     viewPos.y -= size.height;
     size.width -= _padding * 2;
     size.height -= _padding * 2;
