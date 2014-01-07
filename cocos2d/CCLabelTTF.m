06d1bedd0c99545aca7609be205067ecfbb5fa7d
John Twigg <john.twigg@gmail.com>
2014-01-07 15:21:10 -0800

    nodes convertToPoints now uses passed in type->corner. onEnter ttf's now refresh texture.
    Former-commit-id: 06618ca8c7a892cfb10cc8d5bf4df8477ab9fb5a

--- cocos2d/CCLabelTTF.m
+++ cocos2d/CCLabelTTF.m
@@ -309,8 +309,8 @@ static __strong NSMutableDictionary* ccLabelTTF_registeredFonts;
 
 -(void)onEnter
 {
-    if(self.shadowOffsetType.xUnit == CCPositionUnitNormalized || self.shadowOffsetType.yUnit == CCPositionUnitNormalized)
-        [self setTextureDirty];
+    
+    [self setTextureDirty];
     
     [super onEnter];
 }
