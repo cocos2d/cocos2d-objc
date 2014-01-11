0240f7d664e07e5fa243fac5c4193ab64b386dc1
Birkemose <lars.birkemose@gmail.com>
2014-01-11 20:49:34 +0100

    MIT credits + AUTHORS
    Former-commit-id: df3fddc34a1b491cb4b77ebbc6ece922d26e38f6

--- cocos2d/ccGLStateCache.h
+++ cocos2d/ccGLStateCache.h
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2011 Ricardo Quesada
  * Copyright (c) 2011 Zynga Inc.
+ * Copyright (c) 2013-2014 Cocos2D Authors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
