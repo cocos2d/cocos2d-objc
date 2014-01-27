cb351e5fc10e9f1292ce9cc2f06420284a2bc9d0
Birkemose <lars.birkemose@gmail.com>
2014-01-27 13:32:39 +0100

    Fixed some missing extern declarations

--- cocos2d/Platforms/iOS/CCAppDelegate.h
+++ cocos2d/Platforms/iOS/CCAppDelegate.h
@@ -30,27 +30,27 @@
 #import <UIKit/UIKit.h>
 #import "CCDirectorIOS.h"
 
-NSString* const CCSetupPixelFormat;
-NSString* const CCSetupScreenMode;
-NSString* const CCSetupScreenOrientation;
-NSString* const CCSetupAnimationInterval;
-NSString* const CCSetupFixedUpdateInterval;
-NSString* const CCSetupShowDebugStats;
-NSString* const CCSetupTabletScale2X;
+extern NSString* const CCSetupPixelFormat;
+extern NSString* const CCSetupScreenMode;
+extern NSString* const CCSetupScreenOrientation;
+extern NSString* const CCSetupAnimationInterval;
+extern NSString* const CCSetupFixedUpdateInterval;
+extern NSString* const CCSetupShowDebugStats;
+extern NSString* const CCSetupTabletScale2X;
 
 
 // Landscape screen orientation. Used with CCSetupScreenOrientation.
-NSString* const CCScreenOrientationLandscape;
+extern NSString* const CCScreenOrientationLandscape;
 
 // Portrait screen orientation.  Used with CCSetupScreenOrientation.
-NSString* const CCScreenOrientationPortrait;
+extern NSString* const CCScreenOrientationPortrait;
 
 
 // The flexible screen mode is Cocos2d's default. It will give you an area that can vary slightly in size. In landscape mode the height will be 320 points for mobiles and 384 points for tablets. The width of the area can vary from 480 to 568 points.
-NSString* const CCScreenModeFlexible;
+extern NSString* const CCScreenModeFlexible;
 
 // The fixed screen mode will setup the working area to be 568 x 384 points. Depending on the device, the outer edges may be cropped. The safe area, that will be displayed on all sorts of devices, is 480 x 320 points and placed in the center of the working area.
-NSString* const CCScreenModeFixed;
+extern NSString* const CCScreenModeFixed;
 
 
 @class CCAppDelegate;
