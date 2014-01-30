c6cecd93c3916bd6da2b7a0d1735fafd1e6098e9
Birkemose <lars.birkemose@gmail.com>
2014-01-30 12:55:59 +0100

    Added better position handling in template

--- templates/cocos2d iOS.xctemplate/Newton/NewtonScene.m
+++ templates/cocos2d iOS.xctemplate/Newton/NewtonScene.m
@@ -56,6 +56,7 @@
 {
     // Apple recommend assigning self with supers return value, and handling self not created
     self = [super init];
+    self.scale = 0.5;
     if (!self) return(nil);
      
     // Here is where custom code for the newton scene starts
