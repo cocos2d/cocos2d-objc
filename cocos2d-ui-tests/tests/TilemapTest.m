1a8d0e1ec9a7ff243279e421a47136da217937c2
slembcke <slembcke@gmail.com>
2014-04-21 17:02:32 -0500

    Adding more ortho tilemap tests.

--- cocos2d-ui-tests/tests/TilemapTest.m
+++ cocos2d-ui-tests/tests/TilemapTest.m
@@ -10,14 +10,73 @@
 #import "TestBase.h"
 
 @interface TilemapTest : TestBase @end
-
-@implementation TilemapTest
-
--(void) setupTilemapTest
-{
-	CCTiledMap *tmap = [CCTiledMap tiledMapWithFile:@"TileMaps/orthogonal-test1.tmx"];
-	
-	[self.contentNode addChild:tmap];
+@implementation TilemapTest {
+	CCNode *map;
 }
+
+-(void) setupTilemap1Test
+{
+	self.userInteractionEnabled = YES;
+	map = [CCTiledMap tiledMapWithFile:@"TileMaps/orthogonal-test1.tmx"];
+	[self.contentNode addChild:map];
+}
+
+-(void) setupTilemap2Test
+{
+	self.userInteractionEnabled = YES;
+	map = [CCTiledMap tiledMapWithFile:@"TileMaps/orthogonal-desert-test-with-flips.tmx"];
+	[self.contentNode addChild:map];
+}
+
+-(void) setupTilemap3Test
+{
+	self.userInteractionEnabled = YES;
+	map = [CCTiledMap tiledMapWithFile:@"TileMaps/orthogonal-testLarge.tmx"];
+	[self.contentNode addChild:map];
+}
+
+-(void) setupTilemap4Test
+{
+	self.userInteractionEnabled = YES;
+	map = [CCTiledMap tiledMapWithFile:@"TileMaps/orthogonal-desert-obscenely-large.tmx"];
+	[self.contentNode addChild:map];
+}
+
+#if ( TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR )
+
+-(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	[self touchEnded:touch withEvent:event];
+}
+
+- (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	[self touchEnded:touch withEvent:event];
+}
+
+- (void)touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	CGPoint touchLocation = [touch locationInView: [touch view]];
+	CGPoint prevLocation = [touch previousLocationInView: [touch view]];
+  
+	touchLocation = [[CCDirector sharedDirector] convertToGL: touchLocation];
+	prevLocation = [[CCDirector sharedDirector] convertToGL: prevLocation];
+  
+	CGPoint diff = ccpSub(touchLocation,prevLocation);
+	
+
+	CGPoint currentPos = [map position];
+	[map setPosition: ccpAdd(currentPos, diff)];
+}
+
+#else
+
+- (void)mouseDragged:(NSEvent *)theEvent
+{
+	CGPoint currentPos = [parallaxNode position];
+	[parallaxNode setPosition: ccpAdd(currentPos, CGPointMake( theEvent.deltaX, -theEvent.deltaY) )];
+}
+#endif
+
 
 @end
