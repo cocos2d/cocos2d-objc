42b0f0ab33164c343faa6fb324d8e1476e2d1228
slembcke <slembcke@gmail.com>
2014-04-24 18:03:23 -0500

    Merging in new tilemap tests

--- cocos2d-ui-tests/tests/TilemapTest.m
+++ cocos2d-ui-tests/tests/TilemapTest.m
@@ -10,14 +10,69 @@
 #import "TestBase.h"
 
 @interface TilemapTest : TestBase @end
-
 @implementation TilemapTest
 
--(void) setupTilemapTest
+-(void) setupTilemap1Test
 {
-	CCTiledMap *tmap = [CCTiledMap tiledMapWithFile:@"TileMaps/orthogonal-test1.tmx"];
+	self.subTitle = @"TileMaps/orthogonal-test1.tmx";
 	
-	[self.contentNode addChild:tmap];
+	CCNode *map = [CCTiledMap tiledMapWithFile:self.subTitle];
+	CCScrollView *scroll = [[CCScrollView alloc] initWithContentNode:map];
+	scroll.flipYCoordinates = NO;
+	
+	[self.contentNode addChild:scroll];
+}
+
+-(void) setupTilemap2Test
+{
+	self.subTitle = @"TileMaps/orthogonal-desert-test-with-flips.tmx";
+	
+	CCNode *map = [CCTiledMap tiledMapWithFile:self.subTitle];
+	CCScrollView *scroll = [[CCScrollView alloc] initWithContentNode:map];
+	scroll.flipYCoordinates = NO;
+	
+	[self.contentNode addChild:scroll];
+}
+
+-(void) setupTilemap3Test
+{
+	self.subTitle = @"TileMaps/orthogonal-testLarge.tmx";
+	
+	CCNode *map = [CCTiledMap tiledMapWithFile:self.subTitle];
+	CCScrollView *scroll = [[CCScrollView alloc] initWithContentNode:map];
+	scroll.flipYCoordinates = NO;
+	
+	[self.contentNode addChild:scroll];
+}
+
+//-(void) setupTilemap4Test
+//{
+//	self.subTitle = @"TileMaps/orthogonal-desert-obscenely-large.tmx";
+//	
+//	CCNode *map = [CCTiledMap tiledMapWithFile:self.subTitle];
+//	CCScrollView *scroll = [[CCScrollView alloc] initWithContentNode:map];
+//	scroll.flipYCoordinates = NO;
+//	
+//	[self.contentNode addChild:scroll];
+//}
+
+-(void) setupTilemap5Test
+{
+	self.subTitle = @"TileMaps/orthogonal-desert-test-with-flips.tmx";
+	
+	CCNode *map = [CCTiledMap tiledMapWithFile:self.subTitle];
+	map.anchorPoint = ccp(0.5, 0.5);
+	map.position = map.anchorPointInPoints;
+	[map runAction:[CCActionRepeatForever actionWithAction:[CCActionRotateBy actionWithDuration:10.0 angle:90]]];
+	
+	CCNode *content = [CCNode node];
+	content.contentSize = map.contentSize;
+	[content addChild:map];
+	
+	CCScrollView *scroll = [[CCScrollView alloc] initWithContentNode:content];
+	scroll.flipYCoordinates = NO;
+	
+	[self.contentNode addChild:scroll];
 }
 
 @end
