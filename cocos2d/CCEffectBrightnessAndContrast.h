e793c2fe0e863877f2f571e024927270d01641b7
Thayer J Andrews <thayer@apportable.com>
2014-05-01 13:27:14 -0700

    Initial commit of CCEffectBrightnessAndContrast

---
+++ cocos2d/CCEffectBrightnessAndContrast.h
@@ -0,0 +1,13 @@
+//
+//  CCEffectBrightnessAndContrast.h
+//  cocos2d-ios
+//
+//  Created by Thayer J Andrews on 5/1/14.
+//
+//
+
+#import "CCEffect.h"
+
+@interface CCEffectBrightnessAndContrast : CCEffect
+
+@end
---
+++ cocos2d/CCEffectBrightnessAndContrast.m
@@ -0,0 +1,13 @@
+//
+//  CCEffectBrightnessAndContrast.m
+//  cocos2d-ios
+//
+//  Created by Thayer J Andrews on 5/1/14.
+//
+//
+
+#import "CCEffectBrightnessAndContrast.h"
+
+@implementation CCEffectBrightnessAndContrast
+
+@end
