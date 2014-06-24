0917e2d42a805a869e26b288183610085b6e10b3
Thayer J Andrews <thayer@apportable.com>
2014-06-24 13:35:39 -0700

    CCEffectRefraction - Improve the mapping from screen space coordinates to environment coordinates
    The previous code would not work with all transformations that could be applied to the environment sprite
    (changes to the texture rect, rotation, etc). This code constructs the affine transform from screen space
    coordinates to the environment sprite's texture coordinates so it handles all possible transforms of the
    environment.
    
    There is still one issue remaining with my handling of the tangent and binormal vectors. The fix for that
    is coming up.

--- cocos2d/CCEffectRefraction.m
+++ cocos2d/CCEffectRefraction.m
@@ -24,8 +24,7 @@
                           [CCEffectUniform uniform:@"sampler2D" name:@"u_normalMap" value:(NSValue*)[CCTexture none]],
                           [CCEffectUniform uniform:@"vec2" name:@"u_tangent" value:[NSValue valueWithGLKVector2:GLKVector2Make(1.0f, 0.0f)]],
                           [CCEffectUniform uniform:@"vec2" name:@"u_binormal" value:[NSValue valueWithGLKVector2:GLKVector2Make(0.0f, 1.0f)]],
-                          [CCEffectUniform uniform:@"vec2" name:@"u_texCoordOffset" value:[NSValue valueWithGLKVector2:GLKVector2Make(0.0f, 0.0f)]],
-                          [CCEffectUniform uniform:@"vec2" name:@"u_texCoordScale" value:[NSValue valueWithGLKVector2:GLKVector2Make(1.0f, 1.0f)]],
+                          [CCEffectUniform uniform:@"mat4" name:@"u_screenToEnv" value:[NSValue valueWithGLKMatrix4:GLKMatrix4Identity]],
                           ];
     
     if((self = [super initWithFragmentUniforms:uniforms vertextUniforms:nil varying:nil]))
@@ -57,20 +56,20 @@
     CCEffectFunctionInput *input = [[CCEffectFunctionInput alloc] initWithType:@"vec4" name:@"inputValue" snippet:@"texture2D(cc_PreviousPassTexture, cc_FragTexCoord1)"];
     
     NSString* effectBody = CC_GLSL(
-                                   // Compute screen space texture coordinates from the screen space
+                                   // Compute environment space texture coordinates from the screen space
                                    // fragment position.
-                                   vec2 screenTexCoords = (gl_FragCoord.xy + u_texCoordOffset) * u_texCoordScale;
+                                   vec4 envSpaceTexCoords = u_screenToEnv * vec4(gl_FragCoord.xy, 0, 1);
 
                                    // Index the normal map and expand the color value from [0..1] to [-1..1]
                                    vec4 tangentSpaceNormal = texture2D(u_normalMap, cc_FragTexCoord1) * 2.0 - 1.0;
                                    
-                                   // Convert the normal vector from tangent space to screen space
+                                   // Convert the normal vector from tangent space to environment space
                                    vec2 normal = u_tangent * tangentSpaceNormal.x + u_binormal * tangentSpaceNormal.y;
                                    
                                    // Perturb the screen space texture coordinate by the scaled normal
                                    // vector.
-                                   vec2 refractTexCoords = screenTexCoords + normal.xy * u_refraction;
-                                   
+                                   vec2 refractTexCoords = envSpaceTexCoords.xy + normal.xy * u_refraction;
+
                                    return texture2D(u_envMap, refractTexCoords);
                                    );
     
@@ -92,6 +91,7 @@
         pass.shaderUniforms[self.uniformTranslationTable[@"u_envMap"]] = weakSelf.environment.texture;
         pass.shaderUniforms[self.uniformTranslationTable[@"u_normalMap"]] = weakSelf.normalMap;
         
+        // Setup the tangent and binormal vectors for the normal map.
         GLKVector4 tangent = GLKVector4Make(1.0f, 0.0f, 0.0f, 0.0f);
         tangent = GLKMatrix4MultiplyVector4(pass.transform, tangent);
         tangent = GLKVector4Normalize(tangent);
@@ -102,13 +102,21 @@
         pass.shaderUniforms[self.uniformTranslationTable[@"u_tangent"]] = [NSValue valueWithGLKVector2:GLKVector2Make(tangent.x, tangent.y)];
         pass.shaderUniforms[self.uniformTranslationTable[@"u_binormal"]] = [NSValue valueWithGLKVector2:GLKVector2Make(binormal.x, binormal.y)];
         
-        CGAffineTransform envTransform = weakSelf.environment.worldToNodeTransform;
-        CGRect textureRect = weakSelf.environment.textureRect;
+        // Setup the screen space to environment space matrix.
         CGFloat scale = [CCDirector sharedDirector].contentScaleFactor;
-
-        pass.shaderUniforms[self.uniformTranslationTable[@"u_texCoordOffset"]] = [NSValue valueWithGLKVector2:GLKVector2Make(scale * envTransform.tx / envTransform.a, scale * envTransform.ty / envTransform.d)];
-        pass.shaderUniforms[self.uniformTranslationTable[@"u_texCoordScale"]] = [NSValue valueWithGLKVector2:GLKVector2Make(envTransform.a / (scale * textureRect.size.width), envTransform.d / (scale * textureRect.size.height))];
+        CGAffineTransform screenToWorld = CGAffineTransformMake(1.0f / scale, 0.0f, 0.0f, 1.0f / scale, 0.0f, 0.0f);
+        CGAffineTransform envWorldToNode = weakSelf.environment.worldToNodeTransform;
+        CGAffineTransform envNodeToTexture = weakSelf.environment.nodeToTextureTransform;
+        CGAffineTransform screenToEnvNode = CGAffineTransformConcat(screenToWorld, envWorldToNode);
+        CGAffineTransform screenToEnvTexture = CGAffineTransformConcat(screenToEnvNode, envNodeToTexture);
         
+        GLKMatrix4 xformMat = GLKMatrix4Make( screenToEnvTexture.a,  screenToEnvTexture.b, 0.0f, 0.0f,
+                                              screenToEnvTexture.c,  screenToEnvTexture.d, 0.0f, 0.0f,
+                                                              0.0f,                  0.0f, 1.0f, 0.0f,
+                                             screenToEnvTexture.tx, screenToEnvTexture.ty, 0.0f, 1.0f);
+        
+        pass.shaderUniforms[self.uniformTranslationTable[@"u_screenToEnv"]] = [NSValue valueWithGLKMatrix4:xformMat];
+
     } copy]];
     
     self.renderPasses = @[pass0];
