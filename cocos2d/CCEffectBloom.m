e35275a2017c57c241a1eee7cb8571d5324e7501
Thayer J Andrews <thayer@apportable.com>
2014-07-03 13:05:35 -0700

    CCEffectBloom - Don't get the pass's shader uniforms from the effect anymore

--- cocos2d/CCEffectBloom.m
+++ cocos2d/CCEffectBloom.m
@@ -279,7 +279,6 @@
     
     CCEffectRenderPass *pass0 = [[CCEffectRenderPass alloc] init];
     pass0.shader = self.shader;
-    pass0.shaderUniforms = self.shaderUniforms;
     pass0.beginBlocks = @[[^(CCEffectRenderPass *pass, CCTexture *previousPassTexture){
         
         pass.shaderUniforms[CCShaderUniformMainTexture] = previousPassTexture;
@@ -294,7 +293,6 @@
     
     CCEffectRenderPass *pass1 = [[CCEffectRenderPass alloc] init];
     pass1.shader = self.shader;
-    pass1.shaderUniforms = self.shaderUniforms;
     pass1.beginBlocks = @[[^(CCEffectRenderPass *pass, CCTexture *previousPassTexture){
         
         pass.shaderUniforms[CCShaderUniformPreviousPassTexture] = previousPassTexture;
@@ -308,7 +306,6 @@
     
     CCEffectRenderPass *pass2 = [[CCEffectRenderPass alloc] init];
     pass2.shader = self.shader;
-    pass2.shaderUniforms = self.shaderUniforms;
     pass2.beginBlocks = @[[^(CCEffectRenderPass *pass, CCTexture *previousPassTexture){
         pass.shaderUniforms[CCShaderUniformPreviousPassTexture] = previousPassTexture;
         pass.shaderUniforms[self.uniformTranslationTable[@"u_enableGlowMap"]] = [NSNumber numberWithFloat:1.0f];
