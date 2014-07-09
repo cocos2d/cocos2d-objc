6e46dbe849f947c192152157ad478dd5ab71f714
Thayer J Andrews <thayer@apportable.com>
2014-07-09 16:39:13 -0700

    CCEffectHue - Initial commit

---
+++ cocos2d/CCEffectHue.h
@@ -0,0 +1,13 @@
+//
+//  CCEffectHue.h
+//  cocos2d-ios
+//
+//  Created by Thayer J Andrews on 7/9/14.
+//
+//
+
+#import "CCEffect.h"
+
+@interface CCEffectHue : CCEffect
+
+@end
---
+++ cocos2d/CCEffectHue.m
@@ -0,0 +1,13 @@
+//
+//  CCEffectHue.m
+//  cocos2d-ios
+//
+//  Created by Thayer J Andrews on 7/9/14.
+//
+//
+
+#import "CCEffectHue.h"
+
+@implementation CCEffectHue
+
+@end
