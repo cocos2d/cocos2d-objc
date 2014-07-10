7c045d2c42b50bb7c6c6a68aa567be5bc43caaf5
Thayer J Andrews <thayer@apportable.com>
2014-07-10 10:52:31 -0700

    CCEffectHue - Fill out the hue adjustment shader and update the hue test
    - The GLSL implementation comes from GPUImage (the license is added in the header comments).
    - The input hue adjustment follows Photoshop's convention (I think*) of ranging from -180 to 180.
    - The test is updated to test various hue values from -180 to 180.
    
    * I do not have Photoshop so I checked the documentation online.

--- cocos2d/CCEffectHue.m
+++ cocos2d/CCEffectHue.m
@@ -5,6 +5,40 @@
 //  Created by Thayer J Andrews on 7/9/14.
 //
 //
+//  This effect makes use of algorithms and GLSL shaders from GPUImage whose
+//  license is included here.
+//
+//  <Begin GPUImage license>
+//
+//  Copyright (c) 2012, Brad Larson, Ben Cochran, Hugues Lismonde, Keitaroh
+//  Kobayashi, Alaric Cole, Matthew Clark, Jacob Gundersen, Chris Williams.
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//  Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//  Neither the name of the GPUImage framework nor the names of its contributors
+//  may be used to endorse or promote products derived from this software
+//  without specific prior written permission.
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  <End GPUImage license>
+
 
 #import "CCEffectHue.h"
 #import "CCEffect_Private.h"
@@ -53,8 +87,41 @@ static float conditionHue(float hue);
 
     CCEffectFunctionInput *input = [[CCEffectFunctionInput alloc] initWithType:@"vec4" name:@"inputValue" snippet:@"texture2D(cc_PreviousPassTexture, cc_FragTexCoord1)"];
 
+    // Image hue shader based on hue filter in GPUImage - https://github.com/BradLarson/GPUImage
     NSString* effectBody = CC_GLSL(
-                                   return inputValue;
+                                   const highp vec4  kRGBToYPrime = vec4 (0.299, 0.587, 0.114, 0.0);
+                                   const highp vec4  kRGBToI      = vec4 (0.595716, -0.274453, -0.321263, 0.0);
+                                   const highp vec4  kRGBToQ      = vec4 (0.211456, -0.522591, 0.31135, 0.0);
+                                   
+                                   const highp vec4  kYIQToR      = vec4 (1.0, 0.9563, 0.6210, 0.0);
+                                   const highp vec4  kYIQToG      = vec4 (1.0, -0.2721, -0.6474, 0.0);
+                                   const highp vec4  kYIQToB      = vec4 (1.0, -1.1070, 1.7046, 0.0);
+                                   
+                                   // Convert to YIQ
+                                   highp float YPrime = dot (inputValue, kRGBToYPrime);
+                                   highp float I      = dot (inputValue, kRGBToI);
+                                   highp float Q      = dot (inputValue, kRGBToQ);
+                                   
+                                   // Calculate the hue and chroma
+                                   highp float hue    = atan (Q, I);
+                                   highp float chroma = sqrt (I * I + Q * Q);
+                                   
+                                   // Make the user's adjustments.
+                                   hue += -u_hue; // Why is this negative?
+                                   
+                                   // Convert back to YIQ
+                                   Q = chroma * sin (hue);
+                                   I = chroma * cos (hue);
+                                   
+                                   // Convert back to RGB
+                                   vec4 outputColor;
+                                   highp vec4    yIQ   = vec4 (YPrime, I, Q, 0.0);
+                                   outputColor.r = dot (yIQ, kYIQToR);
+                                   outputColor.g = dot (yIQ, kYIQToG);
+                                   outputColor.b = dot (yIQ, kYIQToB);
+                                   outputColor.a = inputValue.a;
+                                   
+                                   return outputColor;
                                    );
     
     CCEffectFunction* fragmentFunction = [[CCEffectFunction alloc] initWithName:@"hueEffect" body:effectBody inputs:@[input] returnType:@"vec4"];
@@ -86,8 +153,8 @@ static float conditionHue(float hue);
 
 float conditionHue(float hue)
 {
-    NSCAssert((hue >= -1.0) && (hue <= 1.0), @"Supplied hue out of range [-1..1].");
-    return clampf(hue, -1.0f, 1.0f);
+    NSCAssert((hue >= -180.0f) && (hue <= 180.0), @"Supplied hue out of range [-180.0..180.0].");
+    return clampf(hue, -180.0f, 180.0f) * M_PI / 180.0f;
 }
 
 #endif
