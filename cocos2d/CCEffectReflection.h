7d5d2b6c45796ee04e3b633aaf2d7b6b91541657
Thayer J Andrews <thayer@apportable.com>
2014-07-14 13:22:12 -0700

    CCEffectReflection - Initial commit

---
+++ cocos2d/CCEffectReflection.h
@@ -0,0 +1,13 @@
+//
+//  CCEffectReflection.h
+//  cocos2d-ios
+//
+//  Created by Thayer J Andrews on 7/14/14.
+//
+//
+
+#import "CCEffect.h"
+
+@interface CCEffectReflection : CCEffect
+
+@end
---
+++ cocos2d/CCEffectReflection.m
@@ -0,0 +1,13 @@
+//
+//  CCEffectReflection.m
+//  cocos2d-ios
+//
+//  Created by Thayer J Andrews on 7/14/14.
+//
+//
+
+#import "CCEffectReflection.h"
+
+@implementation CCEffectReflection
+
+@end
