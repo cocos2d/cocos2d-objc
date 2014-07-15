35f17f83202c2c77e5cfc3cfef8ef4e0fb358f14
Thayer J Andrews <thayer@apportable.com>
2014-07-15 15:24:52 -0700

    CCEffectGlass - Initial commit

---
+++ cocos2d/CCEffectGlass.h
@@ -0,0 +1,13 @@
+//
+//  CCEffectGlass.h
+//  cocos2d-ios
+//
+//  Created by Thayer J Andrews on 7/15/14.
+//
+//
+
+#import "CCEffect.h"
+
+@interface CCEffectGlass : CCEffect
+
+@end
---
+++ cocos2d/CCEffectGlass.m
@@ -0,0 +1,13 @@
+//
+//  CCEffectGlass.m
+//  cocos2d-ios
+//
+//  Created by Thayer J Andrews on 7/15/14.
+//
+//
+
+#import "CCEffectGlass.h"
+
+@implementation CCEffectGlass
+
+@end
