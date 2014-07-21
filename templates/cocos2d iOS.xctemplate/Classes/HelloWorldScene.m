8c6b2d3ee2956c7d1ae1b7b38b7793d0353ca6c2
Philippe Hausler <philippe@apportable.com>
2014-07-21 14:35:25 -0700

    updates for templates for Android abstracted targets

--- templates/cocos2d iOS.xctemplate/Classes/HelloWorldScene.m
+++ templates/cocos2d iOS.xctemplate/Classes/HelloWorldScene.m
@@ -97,7 +97,7 @@
 #pragma mark - Touch Handler
 // -----------------------------------------------------------------------
 
--(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+-(void) touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     CGPoint touchLoc = [touch locationInNode:self];
     
     // Log touch location
