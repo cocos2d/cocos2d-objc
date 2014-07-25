1a8ea1ea4f8cd62aaab5c754e5c31ae4fc04dbde
Thayer J Andrews <thayer@apportable.com>
2014-07-25 14:31:09 -0700

    CCEffect - API documentation

--- cocos2d/CCEffect.h
+++ cocos2d/CCEffect.h
@@ -12,8 +12,21 @@
 #import "ccConfig.h"
 #import "ccTypes.h"
 
+
+/**
+ * CCEffect is the foundation of the Cocos2D effects system. Subclasses of CCEffect can be
+ * used to easil add exciting visual effects such has blur, bloom, reflection, refraction, and
+ * other image processing filters to your applications.
+ *
+ */
+
 @interface CCEffect : NSObject
 
+/// -----------------------------------------------------------------------
+/// @name Accessing Effect Attributes
+/// -----------------------------------------------------------------------
+
+/** An identifier for debugging effects. */
 @property (nonatomic, copy) NSString *debugName;
 
 @end
