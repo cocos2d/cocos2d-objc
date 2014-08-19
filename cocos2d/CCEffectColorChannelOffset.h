75077bd51905e83b7fea5ecca714f3f7faeb4bd4
Thayer J Andrews <thayer@apportable.com>
2014-08-19 10:34:11 -0700

    CCEffectColorChannelOffset - Initial commit

---
+++ cocos2d/CCEffectColorChannelOffset.h
@@ -0,0 +1,13 @@
+//
+//  CCEffectColorChannelOffset.h
+//  cocos2d-ios
+//
+//  Created by Thayer J Andrews on 8/19/14.
+//
+//
+
+#import "CCEffect.h"
+
+@interface CCEffectColorChannelOffset : CCEffect
+
+@end
---
+++ cocos2d/CCEffectColorChannelOffset.m
@@ -0,0 +1,13 @@
+//
+//  CCEffectColorChannelOffset.m
+//  cocos2d-ios
+//
+//  Created by Thayer J Andrews on 8/19/14.
+//
+//
+
+#import "CCEffectColorChannelOffset.h"
+
+@implementation CCEffectColorChannelOffset
+
+@end
