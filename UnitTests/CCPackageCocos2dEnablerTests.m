447ba2d7e63cea3b854ca973ad78729e0592c8fb
Nicky Weber <nicky.vault13@gmail.com>
2014-09-25 15:02:15 +0200

    CCPackageCocos2dEnabler unit tests added.

--- UnitTests/CCPackageCocos2dEnablerTests.m
+++ UnitTests/CCPackageCocos2dEnablerTests.m
@@ -7,28 +7,82 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "CCPackageCocos2dEnabler.h"
+#import "CCPackage.h"
+#import "CCFileUtils.h"
+#import "CCSprite.h"
 
 @interface CCPackageCocos2dEnablerTests : XCTestCase
 
+@property (nonatomic, strong) CCPackage *package;
+@property (nonatomic, copy) NSURL *installURL;
+
 @end
+
 
 @implementation CCPackageCocos2dEnablerTests
 
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    self.package = [[CCPackage alloc] initWithName:@"Foo"
+                                        resolution:@"phonehd"
+                                                os:@"iOS"
+                                         remoteURL:[NSURL URLWithString:@"http://foo.fake/Foo-iOS-phonehd.zip"]];
+
+    NSString *pathToPackage = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Resources-shared/Packages/testpackage-iOS-phonehd_unzipped"];
+    self.installURL = [[NSURL fileURLWithPath:pathToPackage] URLByAppendingPathComponent:@"testpackage-iOS-phonehd"];
+    [_package setValue:_installURL forKey:NSStringFromSelector(@selector(installURL))];
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    CCPackageCocos2dEnabler *packageEnabler = [[CCPackageCocos2dEnabler alloc] init];
+    [packageEnabler disablePackages:@[_package]];
+
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testEnablePackage
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    CCPackageCocos2dEnabler *packageEnabler = [[CCPackageCocos2dEnabler alloc] init];
+    [packageEnabler enablePackages:@[_package]];
+
+    XCTAssertTrue([self isPackageInSearchPath]);
+    XCTAssertEqual(_package.status, CCPackageStatusInstalledEnabled);
+
+    CCSprite *sprite = [CCSprite spriteWithImageNamed:@"boredSmiley.png"];
+    XCTAssertNotNil(sprite);
+}
+
+- (void)testDisablePackage
+{
+    [self testEnablePackage];
+
+    CCPackageCocos2dEnabler *packageEnabler = [[CCPackageCocos2dEnabler alloc] init];
+    [packageEnabler disablePackages:@[_package]];
+
+    XCTAssertEqual(_package.status, CCPackageStatusInstalledDisabled);
+
+    XCTAssertFalse([self isPackageInSearchPath]);
+
+    // Can't use [CCSprite spriteWithImageNamed:@"boredSmiley.png"], assertion exception is screwing up test result
+    CGFloat *scale;
+    NSString *path = [[CCFileUtils sharedFileUtils] fullPathForFilename:@"boredSmiley.png" contentScale:&scale];
+    XCTAssertNil(path);
+}
+
+- (BOOL)isPackageInSearchPath
+{
+    for (NSString *aSearchPath in [CCFileUtils sharedFileUtils].searchPath)
+    {
+        if ([aSearchPath isEqualToString:_package.installURL.path])
+        {
+            return YES;
+        }
+    }
+    return NO;
 }
 
 @end
