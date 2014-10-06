323db86130a11c14830d51e469ba56d18f9a7fa4
Nicky Weber <nicky.vault13@gmail.com>
2014-10-06 14:07:14 +0200

    Fixed CCSpriteFrameCacheTests. Some weirdnesses with the copy bundle phase.

--- UnitTests/CCSpriteFrameCacheTests.m
+++ UnitTests/CCSpriteFrameCacheTests.m
@@ -58,27 +58,35 @@
 - (void)testLoadSpriteFrameLookupsInAllSearchPathsWithName
 {
     NSString *pathToUnzippedPackage = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Resources-shared/Packages/testpackage-iOS-phonehd_unzipped/testpackage-iOS-phonehd"];
-    NSString *pathToUnzippedPackage2 = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Resources-shared/Packages/testpackage-iOS-phonehd_unzipped/testpackage-iOS-phonehd"];
+    NSString *pathToUnzippedPackage2 = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Resources-shared/Packages/anotherpackage-iOS-phonehd_unzipped/anotherpackage-iOS-phonehd"];
 
     NSString *newSearchPathForPackage = [NSTemporaryDirectory() stringByAppendingPathComponent:@"testpackage-iOS-phonehd"];
-    NSString *newSearchPathForPackage2 = [NSTemporaryDirectory() stringByAppendingPathComponent:@"testpackage2-iOS-phonehd"];
+    NSString *newSearchPathForPackage2 = [NSTemporaryDirectory() stringByAppendingPathComponent:@"anotherpackage-iOS-phonehd"];
 
     NSFileManager *fileManager = [NSFileManager defaultManager];
     [fileManager removeItemAtPath:newSearchPathForPackage error:nil];
-    [fileManager copyItemAtPath:pathToUnzippedPackage toPath:newSearchPathForPackage error:nil];
+    NSError *error1;
+    if (![fileManager copyItemAtPath:pathToUnzippedPackage toPath:newSearchPathForPackage error:&error1])
+    {
+        NSLog(@"1: %@", error1);
+    }
 
     [fileManager removeItemAtPath:newSearchPathForPackage2 error:nil];
-    [fileManager copyItemAtPath:pathToUnzippedPackage2 toPath:newSearchPathForPackage2 error:nil];
+    NSError *error2;
+    if (![fileManager copyItemAtPath:pathToUnzippedPackage2 toPath:newSearchPathForPackage2 error:&error2])
+    {
+        NSLog(@"2: %@", error2);
+    }
 
     [CCFileUtils sharedFileUtils].searchPath = @[newSearchPathForPackage,newSearchPathForPackage2];
 
     [[CCSpriteFrameCache sharedSpriteFrameCache] loadSpriteFrameLookupsInAllSearchPathsWithName:@"spriteFrameFileList.plist"];
 
     CCSpriteFrame *frame1 = [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:@"smileys/angrySmiley.png"];
-    CCSpriteFrame *frame2 = [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:@"moresmileys/confusedSmiley.png"];
+    CCSpriteFrame *frame2 = [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:@"moresmileys/knockOutSmiley.png"];
 
     XCTAssertNotNil(frame1, @"Error loading: smileys/angrySmiley.png");
-    XCTAssertNotNil(frame2, @"Error loading: smileys/moresmileys/confusedSmiley.png");
+    XCTAssertNotNil(frame2, @"Error loading: moresmileys/confusedSmiley.png");
 }
 
 @end
