f17915e1d69d7447b832856d2afa6f9e0a969055
slembcke <slembcke@gmail.com>
2014-10-24 21:20:51 -0500

    Making some CGPointExtensions inline.

--- cocos2d/Support/CGPointExtension.m
+++ cocos2d/Support/CGPointExtension.m
@@ -32,41 +32,6 @@
 
 #define kCGPointEpsilon FLT_EPSILON
 
-CGFloat
-ccpLength(const CGPoint v)
-{
-	return sqrtf(ccpLengthSQ(v));
-}
-
-CGFloat
-ccpDistance(const CGPoint v1, const CGPoint v2)
-{
-	return ccpLength(ccpSub(v1, v2));
-}
-
-CGPoint
-ccpNormalize(const CGPoint v)
-{
-	return ccpMult(v, 1.0f/ccpLength(v));
-}
-
-CGPoint
-ccpForAngle(const CGFloat a)
-{
-	return ccp(cosf(a), sinf(a));
-}
-
-CGFloat
-ccpToAngle(const CGPoint v)
-{
-	return atan2f(v.y, v.x);
-}
-
-CGPoint ccpLerp(CGPoint a, CGPoint b, float alpha)
-{
-	return ccpAdd(ccpMult(a, 1.f - alpha), ccpMult(b, alpha));
-}
-
 float clampf(float value, float min_inclusive, float max_inclusive)
 {
 	if (min_inclusive > max_inclusive) {
