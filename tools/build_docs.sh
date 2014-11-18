f04637117dd8c22603cb1714927aa0a39fdb5819
Steffen Itterheim <steffen@learn-cocos2d.com>
2014-11-18 14:24:03 +0100

    build docs: updated ignore list, version

--- tools/build_docs.sh
+++ tools/build_docs.sh
@@ -1,15 +1,24 @@
 #appledoc Xcode script
+
 # Start constants
 company="testpath1";
 companyID="testpath2";
-companyURL="http://www.cocos2d-iphone.org";
+companyURL="http://www.cocos2d-swift.org";
 target="iphoneos";
 outputPath="api-docs";
 # End constants
+
+# Note about ignore list:
+# For some reason you can use "--ignore Path" only to ignore files that *end in* Path
+# but you can't use "--ignore *Package*" (any wildcard combo) to ignore files that begin with or contain Path.
+
+# Adding ignore items:
+# Please try to keep the ignore list sorted alphabetically for easier maintenance.
+
 tools/appledoc \
 --project-name "Cocos2D" \
---project-company "3.2.0" \
---company-id "org.cocos2d" \
+--project-company "3.3.0" \
+--company-id "org.cocos2d-swift" \
 --docset-atom-filename "testpath4.atom" \
 --docset-feed-url "${companyURL}/${company}/%DOCSETATOMFILENAME" \
 --docset-package-url "${companyURL}/${company}/%DOCSETPACKAGEFILENAME" \
@@ -25,16 +34,18 @@ tools/appledoc \
 --no-repeat-first-par \
 --no-warn-invalid-crossref \
 --exit-threshold 2 \
+--verbose 2 \
+--ignore .m \
+--ignore _Private.h \
 --ignore base64.h \
 --ignore CCActionManager.h \
---ignore CCAnimationCache.h \
 --ignore CCAnimation.h \
+--ignore CCAnimationCache.h \
 --ignore CCAtlasNode.h \
 --ignore CCBAnimationManager.h \
 --ignore CCBKeyFrame.h \
 --ignore CCBLocalizationManager.h \
 --ignore CCBReader.h \
---ignore CCBReader_Private.h \
 --ignore CCBSequence.h \
 --ignore CCBSequenceProperty.h \
 --ignore CCBuilderReader.h \
@@ -43,7 +54,6 @@ tools/appledoc \
 --ignore ccDeprecated.h \
 --ignore CCDirectorIOS.h \
 --ignore CCDirectorMac.h \
---ignore CCDirector_Private.h \
 --ignore CCDrawingPrimitives.h \
 --ignore CCES2Renderer.h \
 --ignore CCESRenderer.h \
@@ -54,53 +64,60 @@ tools/appledoc \
 --ignore ccGLStateCache.h \
 --ignore CCGLView.h \
 --ignore CCLabelAtlas.h \
---ignore CCLabelBMFont_Private.h \
 --ignore ccMacros.h \
---ignore CCNode_Private.h \
+--ignore CCMetalView.h \
 --ignore CCNS.h \
+--ignore CCPackage.h \
+--ignore CCPackageCocos2dEnabler.h \
+--ignore CCPackageDownload.h \
+--ignore CCPackageDownloadDelegate.h \
+--ignore CCPackageDownloadManager.h \
+--ignore CCPackageDownloadManagerDelegate.h \
+--ignore CCPackageHelper.h \
+--ignore CCPackageInstaller.h \
+--ignore CCPackageManager.h \
+--ignore CCPackageManagerDelegate.h \
+--ignore CCPackageUnzipper.h \
+--ignore CCPackageUnzipperDelegate.h \
 --ignore CCParticleBatchNode.h \
---ignore CCParticleSystem_Private.h \
---ignore CCParticleSystemQuad_Private.h \
+--ignore CCPhysics+ObjectiveChipmunk.h \
 --ignore CCProfiling.h \
---ignore CCProgressNode_Private.h \
 --ignore CCProtocols.h \
 --ignore CCResponderManager.h \
 --ignore CCScheduler.h \
---ignore CCShaderCache.h \
+--ignore CCShader.h \
+--ignore ccShader_Position_uColor_frag.h \
+--ignore ccShader_Position_uColor_vert.h \
 --ignore ccShader_PositionColor_frag.h \
+--ignore ccShader_PositionColor_vert.h \
 --ignore ccShader_PositionColorLengthTexture_frag.h \
 --ignore ccShader_PositionColorLengthTexture_vert.h \
---ignore ccShader_PositionColor_vert.h \
---ignore ccShader_PositionTextureA8Color_frag.h \
---ignore ccShader_PositionTextureA8Color_vert.h \
---ignore ccShader_PositionTextureColorAlphaTest_frag.h \
---ignore ccShader_PositionTextureColor_frag.h \
---ignore ccShader_PositionTextureColor_vert.h \
 --ignore ccShader_PositionTexture_frag.h \
 --ignore ccShader_PositionTexture_uColor_frag.h \
 --ignore ccShader_PositionTexture_uColor_vert.h \
 --ignore ccShader_PositionTexture_vert.h \
---ignore ccShader_Position_uColor_frag.h \
---ignore ccShader_Position_uColor_vert.h \
+--ignore ccShader_PositionTextureA8Color_frag.h \
+--ignore ccShader_PositionTextureA8Color_vert.h \
+--ignore ccShader_PositionTextureColor_frag.h \
+--ignore ccShader_PositionTextureColor_vert.h \
+--ignore ccShader_PositionTextureColorAlphaTest_frag.h \
+--ignore CCShaderCache.h \
 --ignore ccShaders.h \
---ignore CCSpriteBatchNode_Private.h \
 --ignore CCSpriteFrameCache.h \
---ignore CCSprite_Private.h \
 --ignore CCTextureAtlas.h \
 --ignore CCTextureCache.h \
---ignore CCTexture_Private.h \
 --ignore CCTexturePVR.h \
---ignore CCTiledMapLayer_Private.h \
+--ignore CCTouch.h \
 --ignore ccTypes.h \
 --ignore ccUtils.h \
 --ignore CCVertex.h \
 --ignore CCWindow.h \
 --ignore CGPointExtension.h \
---ignore cocos2d.h \
 --ignore cocos2d-ui.h \
---ignore .m \
+--ignore cocos2d.h \
 --ignore NSAttributedString+CCAdditions.h \
 --ignore NSThread+performBlock.h \
+--ignore NSValue+CCRenderer.h \
 --ignore OpenGL_Internal.h \
 --ignore TGAlib.h \
 --ignore TransformUtils.h \
@@ -108,5 +125,4 @@ tools/appledoc \
 --ignore uthash.h \
 --ignore utlist.h \
 --ignore ZipUtils.h \
---ignore CCPhysics+ObjectiveChipmunk.h \
 cocos2d cocos2d-ui
