9318f9e7db22736b43ac43172c282b6b5a034e25
Steffen Itterheim <steffen@learn-cocos2d.com>
2014-11-20 21:31:57 +0100

    CCPhysics class ref update

--- cocos2d/CCPhysicsJoint.h
+++ cocos2d/CCPhysicsJoint.h
@@ -25,90 +25,116 @@
 #import "CCPhysicsBody.h"
 
 /**
-CCPhysicsJoints hold two CCPhysicsBodies together in some way like a joint between bones or a hinge on a door. Joints work in a fairly automatic fashion.
-They are active from the moment they are created. When you are done with a joint you invalidate it in order to disable it.
-Joints cannot be reactivated once they are invalidated.
+A CCPhysicsJoint connects two CCPhysicsBody objects together, like a joint between bones or a hinge on a door. 
+ 
+ Joints work in a fairly automatic fashion. They are active from the moment they are created. 
+ 
+ When you are done with a joint you invalidate it in order to disable it. This will automatically remove it from the physics simulation.
+
+ @note A joint cannot be reused once it has been invalidated. But you can hold on to it to store its most recent properties which
+ you can then use to create a new joint based on the invalidated joint's properties.
  */
 @interface CCPhysicsJoint : NSObject
 
 
 /// -----------------------------------------------------------------------
-/// @name Creating a CCPhysicsJoint Object
+/// @name Creating Physics Joints
 /// -----------------------------------------------------------------------
 
 /**
 *  Creates and returns a pivot joint object between the two bodies specified. The pivot point is specified in the coordinates of the node that bodyA is attached to.
 *
-*  @param bodyA   Body A.
-*  @param bodyB   Body B.
-*  @param anchorA Anchor point A.
+*  @param bodyA   One of the two bodies to link together.
+*  @param bodyB   One of the two bodies to link together.
+*  @param anchorA Joint anchor point is where the two bodies pivot around (center point of rotation). Anchor is relative to bodyA.
 *
 *  @return The CCPhysicsJoint Object.
+*  @see CCPhysicsBody
 */
-+(CCPhysicsJoint *)connectedPivotJointWithBodyA:(CCPhysicsBody *)bodyA bodyB:(CCPhysicsBody *)bodyB anchorA:(CGPoint)anchorA;
++(CCPhysicsJoint *)connectedPivotJointWithBodyA:(CCPhysicsBody *)bodyA
+                                          bodyB:(CCPhysicsBody *)bodyB
+                                        anchorA:(CGPoint)anchorA;
 
 /**
  *  Creates and returns a pivot joint between the two bodies and keeps the distance of the two anchor points constant.
  *  The anchor points are specified in the coordinates of the node that the bodies are attached to.
  *  The distance between the anchor points will be calculated when the joint first becomes active.
  *
- *  @param bodyA   Body A.
- *  @param bodyB   Body B.
- *  @param anchorA Anchor point A.
- *  @param anchorB Anchor point B.
+ *  @param bodyA   One of the two bodies to link together.
+ *  @param bodyB   One of the two bodies to link together.
+ *  @param anchorA Joint anchor point, relative to bodyA.
+ *  @param anchorB Joint anchor point, relative to bodyB.
  *
  *  @return The CCPhysicsJoint Object.
+ *  @see CCPhysicsBody
  */
-+(CCPhysicsJoint *)connectedDistanceJointWithBodyA:(CCPhysicsBody *)bodyA bodyB:(CCPhysicsBody *)bodyB
-	anchorA:(CGPoint)anchorA anchorB:(CGPoint)anchorB;
++(CCPhysicsJoint *)connectedDistanceJointWithBodyA:(CCPhysicsBody *)bodyA
+                                             bodyB:(CCPhysicsBody *)bodyB
+                                           anchorA:(CGPoint)anchorA
+                                           anchorB:(CGPoint)anchorB;
 
 /**
  *  Creates and returns a pivot joint between the two bodies and keeps the distance of the two anchor points within the range.
  *  The anchor points are specified in the coordinates of the node that the bodies are attached to.
  *
- *  @param bodyA   Body A.
- *  @param bodyB   Body B.
- *  @param anchorA Anchor point A.
- *  @param anchorB Anchor point B.
+ *  @param bodyA   One of the two bodies to link together.
+ *  @param bodyB   One of the two bodies to link together.
+ *  @param anchorA Joint anchor point, relative to bodyA.
+ *  @param anchorB Joint anchor point, relative to bodyB.
  *  @param min     The minimum distance to allow between the anchor points.
  *  @param max     The maximum distance to allow between the anchor points.
  *
  *  @return The CCPhysicsJoint Object.
+ *  @see CCPhysicsBody
  */
-+(CCPhysicsJoint *)connectedDistanceJointWithBodyA:(CCPhysicsBody *)bodyA bodyB:(CCPhysicsBody *)bodyB
-	anchorA:(CGPoint)anchorA anchorB:(CGPoint)anchorB
-	minDistance:(CGFloat)min maxDistance:(CGFloat)max;
++(CCPhysicsJoint *)connectedDistanceJointWithBodyA:(CCPhysicsBody *)bodyA
+                                             bodyB:(CCPhysicsBody *)bodyB
+                                           anchorA:(CGPoint)anchorA
+                                           anchorB:(CGPoint)anchorB
+                                       minDistance:(CGFloat)min
+                                       maxDistance:(CGFloat)max;
 
 /**
- *  Creates and returns a spring joint between the two bodies at the specified anchor points.  The anchor points are specicied in the coordinates of the node that he bodies are attached to.
+ *  Creates and returns a spring joint between the two bodies at the specified anchor points. 
+ *  The anchor points are specicied in the coordinates of the node that he bodies are attached to.
  *
- *  @param bodyA   Body A.
- *  @param bodyB   Body B.
- *  @param anchorA Anchor point A.
- *  @param anchorB Anchor point B.
+ *  @param bodyA   One of the two bodies to link together.
+ *  @param bodyB   One of the two bodies to link together.
+ *  @param anchorA Joint anchor point, relative to bodyA.
+ *  @param anchorB Joint anchor point, relative to bodyB.
  *  @param restLength Rest Length.
  *  @param stiffness  Spring stiffness.
  *  @param damping    Sprin damping.
  *
  *  @return The CCPhysicsJoint Object.
+ *  @see CCPhysicsBody
  */
-+(CCPhysicsJoint *)connectedSpringJointWithBodyA:(CCPhysicsBody *)bodyA bodyB:(CCPhysicsBody *)bodyB
-	anchorA:(CGPoint)anchorA anchorB:(CGPoint)anchorB
-	restLength:(CGFloat)restLength stiffness:(CGFloat)stiffness damping:(CGFloat)damping;
-
++(CCPhysicsJoint *)connectedSpringJointWithBodyA:(CCPhysicsBody *)bodyA
+                                           bodyB:(CCPhysicsBody *)bodyB
+                                         anchorA:(CGPoint)anchorA
+                                         anchorB:(CGPoint)anchorB
+                                      restLength:(CGFloat)restLength
+                                       stiffness:(CGFloat)stiffness
+                                         damping:(CGFloat)damping;
 
 /**
- *  Creates and returns a rotary spring joint between the two bodies. No anchor points are specified as this joint can be used in conjunction with a pivot joint to make a springing pivot joint.
+ *  Creates and returns a rotary spring joint between the two bodies. 
+ *  No anchor points are specified as this joint can be used in conjunction with a pivot joint to make a springing pivot joint.
  *
- *  @param bodyA   Body A.
- *  @param bodyB   Body B.
+ *  @param bodyA   One of the two bodies to link together.
+ *  @param bodyB   One of the two bodies to link together.
  *  @param restAngle Rest angle.
  *  @param stiffness  Spring stiffness.
  *  @param damping    Sprin damping.
  *
  *  @return The CCPhysicsJoint Object.
+ *  @see CCPhysicsBody
  */
-+(CCPhysicsJoint *)connectedRotarySpringJointWithBodyA:(CCPhysicsBody *)bodyA bodyB:(CCPhysicsBody *)bodyB restAngle:(CGFloat)restAngle stiffness:(CGFloat)stiffness damping:(CGFloat)damping;
++(CCPhysicsJoint *)connectedRotarySpringJointWithBodyA:(CCPhysicsBody *)bodyA
+                                                 bodyB:(CCPhysicsBody *)bodyB
+                                             restAngle:(CGFloat)restAngle
+                                             stiffness:(CGFloat)stiffness
+                                               damping:(CGFloat)damping;
 
 
 // This method was misspelled. Please change "stifness" to "stiffness".
@@ -117,77 +143,94 @@ Joints cannot be reactivated once they are invalidated.
 
 
 /**
- *  Creates and returns a Motor joint between the two bodies. No anchor points are specified as this joint can be used in conjunction with a pivot joint to make a motor around a pivot point.
+ *  Creates and returns a Motor joint between the two bodies. 
+ *  No anchor points are specified as this joint can be used in conjunction with a pivot joint to make a motor around a pivot point.
  *
- *  @param bodyA   Body A.
- *  @param bodyB   Body B.
+ *  @param bodyA   One of the two bodies to link together.
+ *  @param bodyB   One of the two bodies to link together.
  *  @param rate    Rate at which the rotate relative to each other. Negative values to reverse direction.
  *
  *  @return The CCPhysicsJoint Object.
+ *  @see CCPhysicsBody
  */
-+(CCPhysicsJoint *)connectedMotorJointWithBodyA:(CCPhysicsBody *)bodyA bodyB:(CCPhysicsBody *)bodyB
++(CCPhysicsJoint *)connectedMotorJointWithBodyA:(CCPhysicsBody *)bodyA
+                                          bodyB:(CCPhysicsBody *)bodyB
                                            rate:(CGFloat)rate;
 
 
 /**
- *  Creates and returns joint whereby the angle of rotation between too bodies is limited. No anchor points are specified as this joint can be used in conjunction with a pivot joint to make the pivots range of motion limited.
+ *  Creates and returns joint whereby the angle of rotation between too bodies is limited. 
+ *  No anchor points are specified as this joint can be used in conjunction with a pivot joint to make the pivots range of motion limited.
  *
- *  @param bodyA   Body A.
- *  @param bodyB   Body B.
+ *  @param bodyA   One of the two bodies to link together.
+ *  @param bodyB   One of the two bodies to link together.
  *  @param min     Minimum angle in radians.
  *  @param max     Maximum angle in radians.
  *
  *  @return The CCPhysicsJoint Object.
+ *  @see CCPhysicsBody
  */
-+(CCPhysicsJoint *)connectedRotaryLimitJointWithBodyA:(CCPhysicsBody *)bodyA bodyB:(CCPhysicsBody *)bodyB
++(CCPhysicsJoint *)connectedRotaryLimitJointWithBodyA:(CCPhysicsBody *)bodyA
+                                                bodyB:(CCPhysicsBody *)bodyB
                                                   min:(CGFloat)min
                                                   max:(CGFloat)max;
 
 
 
 /**
- *  Creates and returns ratchet jointwhereby the angle of rotation between too bodies can go forwards smoothely, but the backwards motion is clipped at 'ratchet' intervals. No anchor points are specified as this joint can be used in conjunction with a pivot joint to ratchet its range of motion.
- *  @param bodyA   Body A.
- *  @param bodyB   Body B.
+ *  Creates and returns [ratchet](http://en.wikipedia.org/wiki/Ratchet_%28device%29) joint whereby the angle of rotation between too bodies can go forward smoothly,
+ *  but the backwards motion is clipped at 'ratchet' intervals (crrnk-crrnk-crrnk). 
+ *  No anchor points are specified as this joint can be used in conjunction with a pivot joint to ratchet its range of motion.
+ *
+ *  @param bodyA   One of the two bodies to link together.
+ *  @param bodyB   One of the two bodies to link together.
  *  @param phase   Phase angle in Radians [0, 2 PI] describing where within the rathet interval the joint is located.
  *  @param ratchet Ratchet interval angle in radians.
  *
  *  @return The CCPhysicsJoint Object.
+ *  @see CCPhysicsBody
  */
-+(CCPhysicsJoint *)connectedRatchetJointWithBodyA:(CCPhysicsBody *)bodyA bodyB:(CCPhysicsBody *)bodyB
++(CCPhysicsJoint *)connectedRatchetJointWithBodyA:(CCPhysicsBody *)bodyA
+                                            bodyB:(CCPhysicsBody *)bodyB
                                             phase:(CGFloat)phase
                                           ratchet:(CGFloat)ratchet;
 
+/// -----------------------------------------------------------------------
+/// @name Removing a Physics Joint
+/// -----------------------------------------------------------------------
+
+/** Disable the joint and remove it from the simulation. */
+-(void)invalidate;
 
 /// -----------------------------------------------------------------------
 /// @name Accessing Physics Joint Attributes
 /// -----------------------------------------------------------------------
 
-/** The first body this joint is attached to. */
+/** The first body this joint is attached to.
+ @see CCPhysicsBody */
 @property(nonatomic, readonly) CCPhysicsBody *bodyA;
 
-/** The second body this joint is attached to. */
+/** The second body this joint is attached to.
+  @see CCPhysicsBody */
 @property(nonatomic, readonly) CCPhysicsBody *bodyB;
 
 /** Maxium foce this joint is allowed to use. Defaults to INFINITY. */
 @property(nonatomic, assign) CGFloat maxForce;
 
-/** Whether or not the connected bodies are allowed to collide with each other. Defaults to YES. */
+/** Whether the connected bodies are allowed to collide with each other. Defaults to YES. */
 @property(nonatomic, assign) BOOL collideBodies;
 
-/** Depending on the joint, either the magnitude of the linear or angular impulse that this joint applied on the previous fixed time step. */
+/** Depending on the joint, either the magnitude of the linear or the angular impulse that this joint applied on the previous fixed time step. */
 @property(nonatomic, readonly) CGFloat impulse;
 
 /**
- *  Maximum force that can be applied before the joint disables itself. Defaults to INFINITY
- *  To avoid problems with solver accuracy, make sure that this value is lower than CCPhysicsJoint.maxForce.
+ *  Maximum force that can be applied before the joint invalidates (removes) itself. Defaults to INFINITY.
+ *
+ *  @note To avoid problems with solver accuracy, make sure that breakingForce is lower than [CCPhysicsJoint maxForce].
  */
 @property(nonatomic, assign) CGFloat breakingForce;
 
 /** Check if the joint is still valid and active. */
 @property(nonatomic, readonly) BOOL valid;
-
-/** Disable the joint and remove it from the simulation. */
--(void)invalidate;
 
 @end
