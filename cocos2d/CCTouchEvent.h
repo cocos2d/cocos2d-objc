a22812d27e4c685dc3ad52932b506ec2671fb6c3
Steffen Itterheim <steffen@learn-cocos2d.com>
2014-11-26 00:15:50 +0100

    touch event and responder doc updates

--- cocos2d/CCTouchEvent.h
+++ cocos2d/CCTouchEvent.h
@@ -8,12 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+/** Thin wrapper around platform-specific touch event objects. */
 @interface CCTouchEvent : NSObject
 
+/** Timestamp for this event. */
 @property(nonatomic) NSTimeInterval timestamp;
+/** A dictionary containing the current touches. The keys are `PlatformTouch` pointers (UITouch on iOS, CCTouchAndroid on Android, NSObject on Mac)
+ and the values are CCTouch objects. 
+ @see CCTouch */
 @property(nonatomic, readonly) NSMutableDictionary* allTouches;
+/** The list of touches for this particular event. Items are CCTouch instances.
+ @see CCTouch */
 @property(nonatomic, readonly) NSMutableSet* currentTouches; // CCTouches
 
+// purposefully undocumented: following methods are for internal use only
 - (id)init;
 
 - (void)updateTouchesBegan:(NSSet*)touches;
