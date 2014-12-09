27287ef92666e2274c34345f881b23e64d66668f
Thayer J Andrews <thayer@apportable.com>
2014-12-09 12:06:46 -0800

    CCBReader - Only handle light property animation if the node is a light

--- cocos2d-ui/CCBReader/CCAnimationManager.m
+++ cocos2d-ui/CCBReader/CCAnimationManager.m
@@ -230,18 +230,20 @@ static NSInteger ccbAnimationManagerID = 0;
         }
     } else if ([name isEqualToString:@"spriteFrame"]) {
         return [CCActionSpriteFrame actionWithSpriteFrame:kf1.value];
-    } else if ([name isEqualToString:@"intensity"] ||
-               [name isEqualToString:@"specularIntensity"] ||
-               [name isEqualToString:@"ambientIntensity"] ||
-               [name isEqualToString:@"cutoffRadius"] ||
-               [name isEqualToString:@"halfRadius"] ||
-               [name isEqualToString:@"depth"])
-    {
-        return [CCActionTween actionWithDuration:duration key:name from:[kf0.value floatValue] to:[kf1.value floatValue]];
-    } else if ([name isEqualToString:@"specularColor"] ||
-               [name isEqualToString:@"ambientColor"])
-    {
-        return [CCBActionTweenColor actionWithDuration:duration key:name from:kf0.value to:kf1.value];
+    } else if ([node isKindOfClass:[CCLightNode class]]) {
+        if ([name isEqualToString:@"intensity"] ||
+            [name isEqualToString:@"specularIntensity"] ||
+            [name isEqualToString:@"ambientIntensity"] ||
+            [name isEqualToString:@"cutoffRadius"] ||
+            [name isEqualToString:@"halfRadius"] ||
+            [name isEqualToString:@"depth"])
+        {
+            return [CCActionTween actionWithDuration:duration key:name from:[kf0.value floatValue] to:[kf1.value floatValue]];
+        } else if ([name isEqualToString:@"specularColor"] ||
+                   [name isEqualToString:@"ambientColor"])
+        {
+            return [CCBActionTweenColor actionWithDuration:duration key:name from:kf0.value to:kf1.value];
+        }
     } else {
         CCLOG(@"CCBReader: Failed to create animation for property: %@", name);
     }
