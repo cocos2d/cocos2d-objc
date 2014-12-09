2674ee4e4b9d60f58d89964d56f5d55c18c21bce
Thayer J Andrews <thayer@apportable.com>
2014-12-09 11:56:57 -0800

    CCBReader - Rename CCActionTweenColor
    ...to CCBActionTweenColor since it's internal to CCBReader for the time being.

--- cocos2d-ui/CCBReader/CCAnimationManager.m
+++ cocos2d-ui/CCBReader/CCAnimationManager.m
@@ -40,7 +40,7 @@
 static NSInteger ccbAnimationManagerID = 0;
 
 
-@interface CCActionTweenColor : CCActionInterval <NSCopying> {
+@interface CCBActionTweenColor : CCActionInterval <NSCopying> {
     NSString *_key;
     CCColor *_to;
     CCColor *_from;
@@ -50,51 +50,6 @@ static NSInteger ccbAnimationManagerID = 0;
 - (id)initWithDuration:(CCTime)duration key:(NSString *)aKey from:(CCColor*)fc to:(CCColor*)tc;
 
 @end
-
-@implementation CCActionTweenColor
-+ (id)actionWithDuration:(CCTime)duration key:(NSString *)key from:(CCColor*)fc to:(CCColor*)tc;
-{
-    return [(CCActionTweenColor*)[ self alloc] initWithDuration:duration key:key from:fc to:tc];
-}
-
-- (id)initWithDuration:(CCTime)duration key:(NSString *)key from:(CCColor*)fc to:(CCColor*)tc;
-{
-    if( (self = [super initWithDuration:duration]) )
-    {
-        _key = [key copy];
-        _from = fc;
-        _to = tc;
-    }
-    return self;
-}
-
--(id) copyWithZone: (NSZone*) zone
-{
-    CCAction *copy = [(CCActionTweenColor*)[[self class] allocWithZone: zone] initWithDuration:[self duration] key:_key from:_from to:_to];
-    return copy;
-}
-
--(void) startWithTarget:(id)aTarget
-{
-    [super startWithTarget:aTarget];
-
-    id value = [_target valueForKey:_key];
-    NSAssert([value isKindOfClass:[CCColor class]], @"CCActionTweenColor is attached to a none-color property.");
-}
-
--(void) update: (CCTime) t
-{
-    CCNode* tn = (CCNode*) _target;
-    
-    ccColor4F fc = _from.ccColor4f;
-    ccColor4F tc = _to.ccColor4f;
-    CCColor *lerped = [CCColor colorWithRed:fc.r + (tc.r - fc.r) * t green:fc.g + (tc.g - fc.g) * t blue:fc.b + (tc.b - fc.b) * t alpha:fc.a + (tc.a - fc.a) * t];
-
-    [_target setValue:lerped forKey:_key];
-}
-@end
-
-
 
 
 @implementation CCAnimationManager
@@ -286,7 +241,7 @@ static NSInteger ccbAnimationManagerID = 0;
     } else if ([name isEqualToString:@"specularColor"] ||
                [name isEqualToString:@"ambientColor"])
     {
-        return [CCActionTweenColor actionWithDuration:duration key:name from:kf0.value to:kf1.value];
+        return [CCBActionTweenColor actionWithDuration:duration key:name from:kf0.value to:kf1.value];
     } else {
         CCLOG(@"CCBReader: Failed to create animation for property: %@", name);
     }
@@ -1054,3 +1009,48 @@ static NSInteger ccbAnimationManagerID = 0;
 }
 
 @end
+
+
+@implementation CCBActionTweenColor
++ (id)actionWithDuration:(CCTime)duration key:(NSString *)key from:(CCColor*)fc to:(CCColor*)tc;
+{
+    return [(CCBActionTweenColor*)[ self alloc] initWithDuration:duration key:key from:fc to:tc];
+}
+
+- (id)initWithDuration:(CCTime)duration key:(NSString *)key from:(CCColor*)fc to:(CCColor*)tc;
+{
+    if( (self = [super initWithDuration:duration]) )
+    {
+        _key = [key copy];
+        _from = fc;
+        _to = tc;
+    }
+    return self;
+}
+
+-(id) copyWithZone: (NSZone*) zone
+{
+    CCAction *copy = [(CCBActionTweenColor*)[[self class] allocWithZone: zone] initWithDuration:[self duration] key:_key from:_from to:_to];
+    return copy;
+}
+
+-(void) startWithTarget:(id)aTarget
+{
+    [super startWithTarget:aTarget];
+    
+    id value = [_target valueForKey:_key];
+    NSAssert([value isKindOfClass:[CCColor class]], @"CCActionTweenColor is attached to a none-color property.");
+}
+
+-(void) update: (CCTime) t
+{
+    CCNode* tn = (CCNode*) _target;
+    
+    ccColor4F fc = _from.ccColor4f;
+    ccColor4F tc = _to.ccColor4f;
+    CCColor *lerped = [CCColor colorWithRed:fc.r + (tc.r - fc.r) * t green:fc.g + (tc.g - fc.g) * t blue:fc.b + (tc.b - fc.b) * t alpha:fc.a + (tc.a - fc.a) * t];
+    
+    [_target setValue:lerped forKey:_key];
+}
+@end
+
