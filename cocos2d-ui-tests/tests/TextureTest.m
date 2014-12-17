e5faa75d4164ed80b490251578e42b51ce17f29a
Andy Korth <andy@howlingmoonsoftware.com>
2014-12-17 16:26:34 -0600

    CCZ files can be loaded. Size hints are used.

--- cocos2d-ui-tests/tests/TextureTest.m
+++ cocos2d-ui-tests/tests/TextureTest.m
@@ -66,16 +66,14 @@
 
 -(void) setupPVRa8LoadingTest
 {
-	// Doesn't seem to work?
-	CCSprite * img = [self loadAndDisplayImageNamed: @"test_image_a8.pvr" withTitle: @"8 bit PVR, alpha only. (With red color overlay)"];
-	[img setColor:[CCColor redColor]];
+	CCSprite * img = [self loadAndDisplayImageNamed: @"test_image_a8.pvr" withTitle: @"8 bit PVR, single channel (greyscale intensity)."];
+    [img setShader:[CCShader positionTextureA8ColorShader]];
 }
 
 -(void) setupPVRa8v3LoadingTest
 {
-	// Doesn't seem to work?
-	CCSprite * img = [self loadAndDisplayImageNamed: @"test_image_a8_v3.pvr" withTitle: @"8 bit PVR v3, alpha only. (With red color overlay)"];
-	[img setColor:[CCColor redColor]];
+	CCSprite * img = [self loadAndDisplayImageNamed: @"test_image_a8_v3.pvr" withTitle: @"8 bit PVR v3, single channel (greyscale intensity)."];
+    [img setShader:[CCShader positionTextureA8ColorShader]];
 }
 
 -(void) setupPVRa88LoadingTest
