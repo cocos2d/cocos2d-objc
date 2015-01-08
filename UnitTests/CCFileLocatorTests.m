07d6500f9b685d60d945df043feaed430ddce1d9
Nicky Weber <nicky.vault13@gmail.com>
2015-01-08 16:47:18 +0100

    Test cases added for no file found and simple recursive search. Simple implementation added as well. Testcase subclass FileSystemTestCase added to easily create file structures.

--- UnitTests/CCFileLocatorTests.m
+++ UnitTests/CCFileLocatorTests.m
@@ -10,8 +10,9 @@
 #import "CCFile.h"
 #import "CCFileLocator.h"
 #import "CCPackageConstants.h"
+#import "FileSystemTestCase.h"
 
-@interface CCFileLocatorTests : XCTestCase
+@interface CCFileLocatorTests : FileSystemTestCase
 
 @property (nonatomic, strong) CCFileLocator *fileLocator;
 
@@ -25,24 +26,77 @@
     [super setUp];
 
     self.fileLocator = [[CCFileLocator alloc] init];
+    _fileLocator.searchPaths = @[[self fullPathForFile:@"Resources"]];
+}
+
+- (void)testFileNamedFileDoesNotExist
+{
+    [self createEmptyFiles:@[
+            @"Resources/images/vehicles/spaceship.png",
+            @"Resources/images/vehicles/car.png",
+            @"Resources/images/vehicles/bike.png",
+            @"Resources/images/vehicles/airplane.png",
+    ]];
+
+    NSError *error;
+    CCFile *file = [_fileLocator fileNamed:@"train.png" options:nil error:&error];
+
+    XCTAssertNotNil(error);
+    XCTAssertEqual(error.code, ERROR_FILELOCATOR_NO_FILE_FOUND);
+    XCTAssertNil(file);
+}
+
+- (void)testFileNamedRecursiveSimple
+{
+    NSString *fileRelPath = @"Resources/images/vehicles/spaceship.png";
+
+    [self createEmptyFiles:@[
+            fileRelPath
+    ]];
+
+    NSError *error;
+    CCFile *file = [_fileLocator fileNamed:@"spaceship.png" options:nil error:&error];
+
+    XCTAssertNotNil(file);
+    XCTAssertNil(error);
+    XCTAssertEqualObjects(file.absoluteFilePath, [self fullPathForFile:fileRelPath]);
 }
 
 - (void)testEmptySearchPaths
 {
+    _fileLocator.searchPaths = @[];
+
     NSError *error;
     CCFile *file = [_fileLocator fileNamed:@"someasset.png" options:nil error:&error];
 
     XCTAssertNotNil(error);
-    XCTAssertEqual(error.code, ERROR_FILELOCATOR_NO_SEARCH_PATHS, @"Error code returned does not match expectation");
-    XCTAssertNil(file, @"file should be nil");
+    XCTAssertEqual(error.code, ERROR_FILELOCATOR_NO_SEARCH_PATHS);
+    XCTAssertNil(file);
 }
 
+/*
 - (void)testPerformanceExample
 {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+    [self createEmptyFiles:@[
+            @"Resources/images/vehicles/spaceship.png",
+            @"Resources/images/vehicles/car.png",
+            @"Resources/images/vehicles/bike.png",
+            @"Resources/images/vehicles/airplane.png",
+            @"Resources/images/housing/castle.png",
+            @"Resources/images/housing/skyscraper.png",
+            @"Resources/images/housing/cottage.png",
+            @"Resources/images/housing/tent.png",
+            @"Resources/sounds/animals/cat.png",
+            @"Resources/sounds/animals/dog.png",
+            @"Resources/sounds/animals/snake.png",
+            @"Resources/sounds/animals/elephant.png",
+    ]];
+
+    [self measureBlock:^
+    {
+        CCFile *file = [_fileLocator fileNamed:@"elephant.png" options:nil error:nil];
     }];
 }
+*/
 
 @end
--- cocos2d/Support/CCFileLocator.m
+++ cocos2d/Support/CCFileLocator.m
@@ -1,6 +1,8 @@
 #import "CCFileLocator.h"
 #import "CCFile.h"
 #import "CCPackageConstants.h"
+#import "ioapi.h"
+#import "CCFile_Private.h"
 
 
 @implementation CCFileLocator
@@ -36,6 +38,59 @@
     {
         [self setErrorPtr:error code:ERROR_FILELOCATOR_NO_SEARCH_PATHS description:@"No search paths set."];
         return nil;
+    }
+
+    for (NSString *searchPath in _searchPaths)
+    {
+        CCFile *aFile = [self findFilename:filename inPath:searchPath];
+        if (aFile)
+        {
+            return aFile;
+        }
+    }
+
+    [self setErrorPtr:error code:ERROR_FILELOCATOR_NO_FILE_FOUND description:@"No search paths set."];
+    return nil;
+}
+
+- (CCFile *)findFilename:(NSString *)filename inPath:(NSString *)path
+{
+    NSFileManager *localFileManager = [[NSFileManager alloc] init];
+
+    NSDirectoryEnumerator *dirEnumerator = [localFileManager enumeratorAtURL:[NSURL fileURLWithPath:path isDirectory:YES]
+                                                  includingPropertiesForKeys:@[NSURLNameKey, NSURLIsDirectoryKey]
+                                                                     options:NSDirectoryEnumerationSkipsHiddenFiles
+                                                                errorHandler:nil];
+
+    for (NSURL *theURL in dirEnumerator)
+    {
+        NSString *currentFilename;
+        [theURL getResourceValue:&currentFilename forKey:NSURLNameKey error:NULL];
+
+        NSNumber *isDirectory;
+        [theURL getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:NULL];
+
+        if ([currentFilename isEqualToString:filename])
+        {
+            return [[CCFile alloc] initWithName:filename url:theURL contentScale:_defaultContentScale];
+        }
+
+        // NSLog(@"-> %@", theURL, fileName);
+/*
+        // Ignore files under the _extras directory
+        if (([fileName caseInsensitiveCompare:@"_extras"] == NSOrderedSame) && [isDirectory boolValue])
+        {
+            [dirEnumerator skipDescendants];
+        }
+        else
+        {
+            // Add full path for non directories
+            if (![isDirectory boolValue])
+            {
+                [theArray addObject:theURL];
+            }
+        }
+*/
     }
 
     return nil;
