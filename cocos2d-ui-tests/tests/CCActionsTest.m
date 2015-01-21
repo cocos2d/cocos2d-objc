668d36a42253a7ad27cf4f6f195f4fcad727b960
Andy Korth <andy@howlingmoonsoftware.com>
2015-01-21 17:24:58 -0600

    Tests for action queueing.

--- cocos2d-ui-tests/tests/CCActionsTest.m
+++ cocos2d-ui-tests/tests/CCActionsTest.m
@@ -8,9 +8,21 @@
 
 - (void) setupBasicLoopTest
 {
-	self.subTitle = @"Bird should running a looping rotation action.";
+	self.subTitle = @"Bird should running a looping rotation action.\nAction added after node.";
     
-    CGSize size = [CCDirector sharedDirector].designSize;
+    CGSize size = [CCDirector currentDirector].designSize;
+    // Tests pausing actions
+    CCSprite *sprite = [CCSprite spriteWithImageNamed:@"Sprites/bird.png"];
+    sprite.position = ccp(size.width * 0.5, size.height * 0.5);
+    [self.contentNode addChild:sprite];
+    [sprite runAction:[CCActionRepeatForever actionWithAction:[CCActionRotateBy actionWithDuration:1 angle:90]]];
+}
+
+- (void) setupBasicLoopActionFirstTest
+{
+    self.subTitle = @"Bird should running a looping rotation action.\nAction added before node.";
+    
+    CGSize size = [CCDirector currentDirector].designSize;
     // Tests pausing actions
     CCSprite *sprite = [CCSprite spriteWithImageNamed:@"Sprites/bird.png"];
     sprite.position = ccp(size.width * 0.5, size.height * 0.5);
@@ -18,21 +30,22 @@
     [self.contentNode addChild:sprite];
 }
 
+
 - (void) setupAlternatingLoopTest
 {
     self.subTitle = @"Bird should run sequence: rotate, change red, repeat forever.";
     
-    CGSize size = [CCDirector sharedDirector].designSize;
+    CGSize size = [CCDirector currentDirector].designSize;
     // Tests pausing actions
     CCSprite *sprite = [CCSprite spriteWithImageNamed:@"Sprites/bird.png"];
     sprite.position = ccp(size.width * 0.5, size.height * 0.5);
+    [self.contentNode addChild:sprite];
     CCActionSequence *seq = [CCActionSequence actions:
                      [CCActionRotateBy actionWithDuration:0.5 angle:90],
                      [CCActionTintTo actionWithDuration:0.25 color:[CCColor redColor]],
                      [CCActionTintTo actionWithDuration:0.25 color:[CCColor whiteColor]],
                      nil];
     [sprite runAction:[CCActionRepeatForever actionWithAction:seq]];
-    [self.contentNode addChild:sprite];
 }
 
 
@@ -40,7 +53,7 @@
 {
     self.subTitle = @"1000 offscreen birds with individual actions.\nRun the profiler here.";
     
-    CGSize size = [CCDirector sharedDirector].designSize;
+    CGSize size = [CCDirector currentDirector].designSize;
     
     for(int i = 0; i < 1000; i++){
         // Tests pausing actions
