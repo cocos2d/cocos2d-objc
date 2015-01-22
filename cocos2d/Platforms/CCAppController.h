8d14c62face2961d248c7acb1dfa5d2b87ac9a8e
Donald Hutchison <git@toastymofo.net>
2015-01-22 13:45:48 +0100

    Documenting app controller.

--- cocos2d/Platforms/CCAppController.h
+++ cocos2d/Platforms/CCAppController.h
@@ -10,16 +10,52 @@
 
 @class NSWindow;
 @class CCGLView;
+
+/**
+ *  
+ CCAppController serves as the setup point for the application and prepares cocos2d to run with various platform specific settings.
+ It exists outside of cocos scene management, and as such can be used to start application specific logic.
+ 
+ An instance of this class (or a subclass) should be invoked from the entry point of execution for each platform. These points are:
+ 
+ * AppDelegate#application:didFinishLaunchingWithOptions: (iOS)
+ * AppDelegate#applicationDidFinishLaunching: (Mac)
+ * CCActivity#run (Android)
+  
+ For SpriteBuilder projects the above files will be generated automatically with the correct invocations. 
+ 
+ Caveats exist for Android, as unlike with other platforms the GL context is not initialized before the setup process begins. Care must 
+ be taken not to perform setup logic that requires access to GL functions before initialization is complete.
+
+ */
+
 @interface CCAppController : NSObject
 
-//for Mac we pass these properties from the AppDelegate to allow configuration
+#if __CC_PLATFORM_MAC
+
+/// -----------------------------------------------------------------------
+/// @name Mac Specific
+/// -----------------------------------------------------------------------
+
+/**
+*  The application window (set from .xib)
+*/
 @property (weak) NSWindow *window;
+
+/**
+ *  The GLView (set from .xib)
+ */
 @property (weak) CCGLView *glView;
 
+#endif
 
-@property(nonatomic, strong) NSDictionary *cocosConfig;
-
+/**
+ *  The name of the first scene to be displayed - by default this is "MainScene.ccb"
+ */
 @property(nonatomic, copy) NSString *firstSceneName;
 
+/**
+ *  Setup and configure cocos for the current platform. This method should be invoked from the entry point of the application.
+ */
 - (void)setupApplication;
 @end
