10a177f8b32721ce6889e6df1d6a90034dbf4793
Nicky Weber <nicky.vault13@gmail.com>
2015-01-23 10:03:24 +0100

    A little helper for the console to dump a node hierarchy. Kudos to Donald: http://donaldhutchison.info/articles/ccnode-recursive-description/ As in UIKit's recursiveDescription and AppKits _subtreeDescription.

---
+++ cocos2d/CCNode+RecursiveDescription.h
@@ -0,0 +1,12 @@
+//
+// Created by Nicky Weber on 23.01.15.
+//
+
+#import <Foundation/Foundation.h>
+#import "CCNode.h"
+
+@interface CCNode (RecursiveDescription)
+
+- (NSString *)recursiveDescription;
+
+@end
