343b51dbb544a58e82314d918624af063d168a1d
Donald Hutchison <git@toastymofo.net>
2015-02-04 12:19:37 +0100

    Optimise imports and don't push director in CCAppDelegate.m.

--- cocos2d/Platforms/iOS/CCAppDelegate.m
+++ cocos2d/Platforms/iOS/CCAppDelegate.m
@@ -27,12 +27,9 @@
 #if __CC_PLATFORM_IOS
 
 #import "ccTypes.h"
-#import "ccUtils.h"
 
 #import "CCAppDelegate.h"
 #import "CCTexture.h"
-#import "CCFileUtils.h"
-#import "OALSimpleAudio.h"
 #import "CCPackageManager.h"
 #import "CCDeviceInfo.h"
 
@@ -105,19 +102,12 @@
 	
 	return GLKMatrix4MakeOrtho(offset.x, sizePoint.width + offset.x, offset.y, sizePoint.height + offset.y, -1024, 1024);
 }
-
-// This is needed for iOS4 and iOS5 in order to ensure
-// that the 1st scene has the correct dimensions
-// This is not needed on iOS6 and could be added to the application:didFinish...
-
-#warning don't believe the lies above! This is the main entry point to your first scene for iOS!
+    
 -(void) directorDidReshapeProjection:(CCDirector*)director
 {
     if(director.runningScene == nil) {
-        [CCDirector pushCurrentDirector:director];
         CCScene * scene = [_appDelegate startScene];
-        [CCDirector popCurrentDirector];
-        
+
         // Add the first scene to the stack. The director will draw it immediately into the framebuffer. (Animation is started automatically when the view is displayed.)
         // and add the scene to the stack. The director will run it when it automatically when the view is displayed.
         [director presentScene: scene];
