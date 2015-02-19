54ad9efe19371fe8de1505b160bb7a1441382884
slembcke <slembcke@gmail.com>
2015-02-19 13:49:49 -0600

    Reimplementing the Mac testbed app delegate.

--- Mac/AppDelegate.m
+++ Mac/AppDelegate.m
@@ -1,28 +1,57 @@
-#import "AppDelegate.h"
 #import "CCPackageManager.h"
 #import "CCDirector_Private.h"
 #import "AppController.h"
+#import "MainMenu.h"
 
-@interface AppDelegate ()
-
-@property (weak) IBOutlet NSWindow *window;
-@property (weak) IBOutlet CC_VIEW<CCView> *glView;
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
 @end
 
 @implementation AppDelegate
+{
+    IBOutlet NSWindow *_window;
+    IBOutlet CCViewMacGL *_view;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    AppController *controller = [AppController sharedController];
-    controller.window = _window;
-    controller.glView = _glView;
-    [controller setupApplication];
-    return;
+    [_window center];
+    [_window makeKeyAndOrderFront:self];
+    
+    CCDirector *director = _view.director;
+    director.contentScaleFactor *= 2;
+    director.UIScaleFactor *= 0.5;
+    
+    CCFileLocator *locator = [CCFileLocator sharedFileLocator];
+    locator.untaggedContentScale = 4;
+    locator.deviceContentScale = director.contentScaleFactor;
+    
+    locator.searchPaths = @[
+        [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Images"],
+        [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Fonts"],
+        [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Resources-shared"],
+        [[NSBundle mainBundle] resourcePath],
+    ];
+
+    // Register spritesheets.
+    [[CCSpriteFrameCache sharedSpriteFrameCache] registerSpriteFramesFile:@"Interface.plist"];
+    [[CCSpriteFrameCache sharedSpriteFrameCache] registerSpriteFramesFile:@"Sprites.plist"];
+    [[CCSpriteFrameCache sharedSpriteFrameCache] registerSpriteFramesFile:@"TilesAtlassed.plist"];
+    
+    [CCDirector pushCurrentDirector:director];
+    [director presentScene:[MainMenu scene]];
+    [CCDirector popCurrentDirector];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification
 {
     [[CCPackageManager sharedManager] savePackages];
+}
+
+//MARK: Window delegate methods:
+
+-(void)windowWillClose:(NSNotification *)notification
+{
+    [[NSApplication sharedApplication] terminate:self];
 }
 
 @end
