b03b9dc03a084a0603d3809fe89c42398582e305
Siarhei Fiedartsou <siarhei.fedartsou@gmail.com>
2015-05-15 21:43:16 +0300

    CCSprite9Slice interface fix

--- cocos2d/CCSprite9Slice.h
+++ cocos2d/CCSprite9Slice.h
@@ -28,6 +28,40 @@
 #import <Foundation/Foundation.h>
 #import "CCSprite.h"
 
+/** Margins for each border of CCSprite9Slice */
+typedef struct _CCSprite9SliceMargins {
+    CGFloat left;
+    CGFloat right;
+    CGFloat top;
+    CGFloat bottom;
+} CCSprite9SliceMargins;
+
+
+/** Makes CCSprite9SliceMargins with appropriate margins */
+static inline CCSprite9SliceMargins CCSprite9SliceMarginsMake(CGFloat left, CGFloat right, CGFloat top, CGFloat bottom)
+{
+    return (CCSprite9SliceMargins)
+    {
+        .left = left,
+        .right = right,
+        .top = top,
+        .bottom = bottom
+    };
+}
+
+/** Makes CCSprite9SliceMargins with the same margin for each border */
+static inline CCSprite9SliceMargins CCSprite9SliceMarginsMakeWithMargin(CGFloat margin)
+{
+    return (CCSprite9SliceMargins)
+    {
+        .left = margin,
+        .right = margin,
+        .top = margin,
+        .bottom = margin
+    };
+}
+
+
 /**
  CCSprite9Slice will render an image in nine quads, keeping the margins fixed and stretching the center quad to fit the content size.
  The effect is that the image's borders will remain unstretched while the center stretches.
@@ -39,13 +73,17 @@
 /// @name Setting the Margin
 /// -----------------------------------------------------------------------
 
+/** Adjusts margins for the left, right, top and bottom borders.
+@note The sum of the this border's margin plus its opposing border's margin must not be equal to or greater than 1.0! */
+@property (nonatomic, assign) CCSprite9SliceMargins margins;
+
 /**
  Sets the margin as a normalized percentage of the total image size.
  If set to 0.25, 25% of the left, right, top and bottom borders of the image will remain unstretched.
  
  @note Margin must be in the range 0.0 to below 0.5.
  */
-@property (nonatomic, assign) float margin;
+@property (nonatomic, assign) float margin  __attribute__((deprecated));
 
 /// -----------------------------------------------------------------------
 /// @name Individual Margins
@@ -53,19 +91,19 @@
 
 /** Adjusts the margin only for this border.
  @note The sum of the this border's margin plus its opposing border's margin must not be equal to or greater than 1.0! */
-@property (nonatomic, assign) float marginLeft;
+@property (nonatomic, assign) float marginLeft  __attribute__((deprecated));
 
 /** Adjusts the margin only for this border.
  @note The sum of the this border's margin plus its opposing border's margin must not be equal to or greater than 1.0! */
-@property (nonatomic, assign) float marginRight;
+@property (nonatomic, assign) float marginRight  __attribute__((deprecated));
 
 /** Adjusts the margin only for this border.
  @note The sum of the this border's margin plus its opposing border's margin must not be equal to or greater than 1.0! */
-@property (nonatomic, assign) float marginTop;
+@property (nonatomic, assign) float marginTop  __attribute__((deprecated));
 
 /** Adjusts the margin only for this border.
  @note The sum of the this border's margin plus its opposing border's margin must not be equal to or greater than 1.0! */
-@property (nonatomic, assign) float marginBottom;
+@property (nonatomic, assign) float marginBottom  __attribute__((deprecated));
 
 @end
 
