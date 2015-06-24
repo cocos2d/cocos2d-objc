ba1d4eb51335c40a892b6f8117bd4b0ecff958a6
Lars Birkemose <lars.birkemose@gmail.com>
2015-06-24 13:23:27 +0200

    Templates added

---
+++ templates/cocos2d iOS.xctemplate/Classes/AppDelegate.m
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.m
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "AppDelegate.h"
+#import "IntroScene.h"
+#import "HelloWorldScene.h"
+
+@implementation AppDelegate
+
+// 
+-(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+	// This is the only app delegate method you need to implement when inheriting from CCAppDelegate.
+	// This method is a good place to add one time setup code that only runs when your app is first launched.
+	
+	// Setup Cocos2D with reasonable defaults for everything.
+	// There are a number of simple options you can change.
+	// If you want more flexibility, you can configure Cocos2D yourself instead of calling setupCocos2dWithOptions:.
+	[self setupCocos2dWithOptions:@{
+		// Show the FPS and draw call label.
+		CCSetupShowDebugStats: @(YES),
+		
+		// More examples of options you might want to fiddle with:
+		// (See CCAppDelegate.h for more information)
+		
+		// Use a 16 bit color buffer: 
+//		CCSetupPixelFormat: kEAGLColorFormatRGB565,
+		// Use a simplified coordinate system that is shared across devices.
+//		CCSetupScreenMode: CCScreenModeFixed,
+		// Run in portrait mode.
+//		CCSetupScreenOrientation: CCScreenOrientationPortrait,
+		// Run at a reduced framerate.
+//		CCSetupAnimationInterval: @(1.0/30.0),
+		// Run the fixed timestep extra fast.
+//		CCSetupFixedUpdateInterval: @(1.0/180.0),
+		// Make iPad's act like they run at a 2x content scale. (iPad retina 4x)
+//		CCSetupTabletScale2X: @(YES),
+	}];
+	
+	return YES;
+}
+
+-(CCScene *)startScene
+{
+	// This method should return the very first scene to be run when your app starts.
+	return [IntroScene scene];
+}
+
+@end
---
+++ templates/cocos2d iOS.xctemplate/Classes/HelloWorldScene.m
@@ -0,0 +1,123 @@
+//
+//  HelloWorldScene.m
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "HelloWorldScene.h"
+#import "IntroScene.h"
+
+// -----------------------------------------------------------------------
+#pragma mark - HelloWorldScene
+// -----------------------------------------------------------------------
+
+@implementation HelloWorldScene
+{
+    CCSprite *_sprite;
+}
+
+// -----------------------------------------------------------------------
+#pragma mark - Create & Destroy
+// -----------------------------------------------------------------------
+
++ (HelloWorldScene *)scene
+{
+    return [[self alloc] init];
+}
+
+// -----------------------------------------------------------------------
+
+- (id)init
+{
+    // Apple recommend assigning self with supers return value
+    self = [super init];
+    if (!self) return(nil);
+    
+    // Enable touch handling on scene node
+    self.userInteractionEnabled = YES;
+    
+    // Create a colored background (Dark Grey)
+    CCNodeColor *background = [CCNodeColor nodeWithColor:[CCColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:1.0f]];
+    [self addChild:background];
+    
+    // Add a sprite
+    _sprite = [CCSprite spriteWithImageNamed:@"Icon-72.png"];
+    _sprite.position  = ccp(self.contentSize.width/2,self.contentSize.height/2);
+    [self addChild:_sprite];
+    
+    // Animate sprite with action
+    CCActionRotateBy* actionSpin = [CCActionRotateBy actionWithDuration:1.5f angle:360];
+    [_sprite runAction:[CCActionRepeatForever actionWithAction:actionSpin]];
+    
+    // Create a back button
+    CCButton *backButton = [CCButton buttonWithTitle:@"[ Menu ]" fontName:@"Verdana-Bold" fontSize:18.0f];
+    backButton.positionType = CCPositionTypeNormalized;
+    backButton.position = ccp(0.85f, 0.95f); // Top Right of screen
+    [backButton setTarget:self selector:@selector(onBackClicked:)];
+    [self addChild:backButton];
+
+    // done
+	return self;
+}
+
+// -----------------------------------------------------------------------
+
+- (void)dealloc
+{
+    // clean up code goes here
+}
+
+// -----------------------------------------------------------------------
+#pragma mark - Enter & Exit
+// -----------------------------------------------------------------------
+
+- (void)onEnter
+{
+    // always call super onEnter first
+    [super onEnter];
+    
+    // In pre-v3, touch enable and scheduleUpdate was called here
+    // In v3, touch is enabled by setting userInteractionEnabled for the individual nodes
+    // Per frame update is automatically enabled, if update is overridden
+    
+}
+
+// -----------------------------------------------------------------------
+
+- (void)onExit
+{
+    // always call super onExit last
+    [super onExit];
+}
+
+// -----------------------------------------------------------------------
+#pragma mark - Touch Handler
+// -----------------------------------------------------------------------
+
+-(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    CGPoint touchLoc = [touch locationInNode:self];
+    
+    // Log touch location
+    CCLOG(@"Move sprite to @ %@",NSStringFromCGPoint(touchLoc));
+    
+    // Move our sprite to touch location
+    CCActionMoveTo *actionMove = [CCActionMoveTo actionWithDuration:1.0f position:touchLoc];
+    [_sprite runAction:actionMove];
+}
+
+// -----------------------------------------------------------------------
+#pragma mark - Button Callbacks
+// -----------------------------------------------------------------------
+
+- (void)onBackClicked:(id)sender
+{
+    // back to intro scene with transition
+    [[CCDirector sharedDirector] replaceScene:[IntroScene scene]
+                               withTransition:[CCTransition transitionPushWithDirection:CCTransitionDirectionRight duration:1.0f]];
+}
+
+// -----------------------------------------------------------------------
+@end
