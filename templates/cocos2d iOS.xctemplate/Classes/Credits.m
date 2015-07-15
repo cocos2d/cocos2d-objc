bfbb2dc8ba84ef002621279a22b7eaea517387fb
Lars Birkemose <lars.birkemose@gmail.com>
2015-07-15 10:55:12 +0200

    Credits working

--- templates/cocos2d iOS.xctemplate/Classes/Credits.m
+++ templates/cocos2d iOS.xctemplate/Classes/Credits.m
@@ -12,6 +12,7 @@
 
 #import "Credits.h"
 #import "CCNode_Private.h"
+#import "cocos2d-ui.h"
 
 // -----------------------------------------------------------------------
 
@@ -20,6 +21,12 @@
     NSMutableArray *_shaderStack;
     NSUInteger _shaderStackPointer;
     __weak CCScene *_scene;
+    CCNode *_scrollNode;
+    float _yPos;
+    CGPoint _lastPosition;
+    BOOL _isScrolling;
+    BOOL _endGame;
+    CCButton *_back;
 }
 
 // -----------------------------------------------------------------------
@@ -40,7 +47,59 @@
     
     _shaderStack = [NSMutableArray array];
     _scene = scene;
-        
+    _yPos = 0.5;
+    _isScrolling = YES;
+    _endGame = NO;
+    
+    // create list of credits
+    NSString *filename = [[NSBundle mainBundle] pathForResource:@"team.plist" ofType:nil];
+    NSDictionary *team = [NSDictionary dictionaryWithContentsOfFile:filename];
+    
+    _scrollNode = [CCNode new];
+    _scrollNode.contentSize = [CCDirector sharedDirector].viewSize;
+    // allow us to fade the scrollnode and all its children
+    _scrollNode.cascadeOpacityEnabled = YES;
+    [self addChild:_scrollNode];
+    
+    CCSprite *sprite = [CCSprite spriteWithImageNamed:@"angry.png"];
+    sprite.positionType = CCPositionTypeNormalized;
+    sprite.position = (CGPoint){0.5, _yPos};
+    _yPos -= 0.25;
+    [_scrollNode addChild:sprite];
+    
+    CCLabelTTF *label;
+    
+    label = [CCLabelTTF labelWithString:@"Cocos2D was made by" fontName:@"ArialMT" fontSize:32];
+    label.positionType = CCPositionTypeNormalized;
+    label.position = (CGPoint){0.5, _yPos};
+    _yPos -= 0.1;
+    [_scrollNode addChild:label];
+    
+    NSString *key = @"New item - ";
+    for (int index = 1; index < 999; index ++)
+    {
+        NSString *entry = [team objectForKey:[NSString stringWithFormat:@"%@%d", key, index]];
+        if (entry != nil)
+        {
+            CCLabelTTF *label = [CCLabelTTF labelWithString:entry fontName:@"ArialMT" fontSize:24];
+            label.positionType = CCPositionTypeNormalized;
+            label.position = (CGPoint){0.5, _yPos};
+            _yPos -= 0.1;
+            [_scrollNode addChild:label];
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    // back button
+    _back = [CCButton buttonWithTitle:@"" spriteFrame:[CCSpriteFrame frameWithImageNamed:@"back.png"]];
+    _back.positionType = CCPositionTypeNormalized;
+    _back.position = (CGPoint){0.92, 0.10};
+    [_back setTarget:self selector:@selector(backPressed:)];
+    [self addChild:_back];
+    
     // traverse all nodes, and swap their shaders
     [self replaceShaderWithGreyScaleShader:_scene];
     
@@ -83,24 +142,100 @@
 
 - (void)dealloc
 {
+    _shaderStackPointer = 0;
+    [self restoreShader:_scene];
+}
+
+// -----------------------------------------------------------------------
+
+- (void)backPressed:(id)sender
+{
+    [_scrollNode runAction:[CCActionSequence actions:
+                     [CCActionFadeOut actionWithDuration:1.0],
+                     [CCActionCallBlock actionWithBlock:^(void)
+                      {
+                          [self removeFromParentAndCleanup:YES];
+                      }],
+                     nil]];
 }
 
 // -----------------------------------------------------------------------
 
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
 {
+    _lastPosition = touch.locationInWorld;
+}
 
+// -----------------------------------------------------------------------
+
+- (void)touchMoved:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    if (!_isScrolling) return;
+    
+    _scrollNode.position = (CGPoint){_scrollNode.position.x, _scrollNode.position.y - (_lastPosition.y - touch.locationInWorld.y)};
+    _lastPosition = touch.locationInWorld;
 }
 
 // -----------------------------------------------------------------------
 
 - (void)touchEnded:(CCTouch *)touch withEvent:(CCTouchEvent *)event
 {
-    [self removeFromParentAndCleanup:YES];
     
-    _shaderStackPointer = 0;
-    [self restoreShader:_scene];
+}
 
+// -----------------------------------------------------------------------
+
+- (void)update:(CCTime)delta
+{
+    if (!_isScrolling) return;
+    
+    float scrollAmountPrSecond = [CCDirector sharedDirector].viewSize.height * 0.125;
+    _scrollNode.position = (CGPoint){_scrollNode.position.x, _scrollNode.position.y + (scrollAmountPrSecond * delta)};
+    // if all text scrolled out, start again
+    
+    if (_endGame) return;
+    
+    // 0.25 is a hardcoded cut'n'try (dont do this at home guys)
+    if (_scrollNode.position.y > ((fabs(_yPos) + 0.25) * [CCDirector sharedDirector].viewSize.height))
+    {
+        // endgame is on
+        _endGame = YES;
+        
+        // hide the back button
+        _back.visible = NO;
+
+        // run endgame sequence
+        [_scrollNode runAction:[CCActionSequence actions:
+                                [CCActionFadeOut actionWithDuration:1.0],
+                                [CCActionCallBlock actionWithBlock:^(void)
+                                 {
+                                     // clear scrollNode (not visible anyways)
+                                     [_scrollNode removeAllChildrenWithCleanup:YES];
+                                     _scrollNode.position = CGPointZero;
+                                     _scrollNode.opacity = 1.0;
+                                     _isScrolling = NO;
+                                     
+                                     // show final text
+                                     CCLabelTTF *label;
+                                     
+                                     label = [CCLabelTTF labelWithString:@"Cocos2D-ObjC" fontName:@"ArialMT" fontSize:24];
+                                     label.positionType = CCPositionTypeNormalized;
+                                     label.position = (CGPoint){0.5, 0.5};
+                                     [_scrollNode addChild:label];
+                                     
+                                     label = [CCLabelTTF labelWithString:@"2015" fontName:@"ArialMT" fontSize:24];
+                                     label.positionType = CCPositionTypeNormalized;
+                                     label.position = (CGPoint){0.5, 0.45};
+                                     [_scrollNode addChild:label];
+                                 }],
+                                [CCActionDelay actionWithDuration:2.0],
+                                [CCActionFadeOut actionWithDuration:1.0],
+                                [CCActionCallBlock actionWithBlock:^(void)
+                                 {
+                                     [self removeFromParentAndCleanup:YES];
+                                 }],
+                                nil]];
+    }
 }
 
 // -----------------------------------------------------------------------
