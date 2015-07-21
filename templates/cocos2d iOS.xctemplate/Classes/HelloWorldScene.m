eae254e00ae3213b5a29be990d1896cf73221411
Lars Birkemose <lars.birkemose@gmail.com>
2015-07-21 11:43:55 +0200

    Template cleanup

---
+++ templates/cocos2d iOS.xctemplate/Classes/HelloWorldScene.m
@@ -0,0 +1,69 @@
+//
+//  ___FILENAME___
+//
+//  Created by : ___FULLUSERNAME___
+//  Project    : ___PROJECTNAME___
+//  Date       : ___DATE___
+//
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import "loadScene.h"
+#import "mainScene.h"
+
+// -----------------------------------------------------------------------
+
+@implementation loadScene
+
+// -----------------------------------------------------------------------
+
+- (id)init
+{
+    // Apple recommend assigning self with supers return value
+    self = [super init];
+    
+    // The thing is, that if this fails, your app will 99.99% crash anyways, so why bother
+    // Just make an assert, so that you can catch it in debug
+    NSAssert(self, @"Whoops");
+    
+    // background
+    CCSprite9Slice *background = [CCSprite9Slice spriteWithImageNamed:@"white_square.png"];
+    background.anchorPoint = CGPointZero;
+    background.contentSize = [CCDirector sharedDirector].viewSize;
+    background.color = [CCColor grayColor];
+    [self addChild:background];
+    
+    
+    // done
+	return self;
+}
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// why not add a few extra lines, so we dont have to sit and edit at the bottom of the screen ...
