25285890bf045fcb8f60afe9f2fc22e6de5b707a
Charlie Williams <c@charliewilliams.org>
2015-09-29 13:37:19 +0100

    Call `fullPathForFilenameIgnoringResolutions` from fullPathForFilename as suggested by the commented out code just below on line 484

--- cocos2d/Support/CCFileUtils.m
+++ cocos2d/Support/CCFileUtils.m
@@ -549,7 +549,7 @@ static CCFileUtils *fileUtils = nil;
 
 -(NSString*) fullPathForFilename:(NSString*)filename
 {
-	return [self fullPathForFilename:filename contentScale:NULL];
+	return [self fullPathForFilenameIgnoringResolutions:filename];
 }
 
 -(NSString*) fullPathForFilename:(NSString*)filename contentScale:(CGFloat *)contentScale
