7cca918f70541732c54fa6384f7ce69610debf25
s1ddok <siddok@gmail.com>
2016-02-06 20:43:02 +0300

    TileMaps now server as an extension

---
+++ cocos2d-ext/TileMaps/CCTileMaps.h
@@ -0,0 +1,12 @@
+//
+//  CCTileMaps.h
+//  cocos2d-tests
+//
+//  Created by Andrey Volodin on 06.02.16.
+//  Copyright © 2016 Cocos2d. All rights reserved.
+//
+
+#import "CCTMXXMLParser.h"
+#import "CCTiledMap.h"
+#import "CCTiledMapLayer.h"
+#import "CCTiledMapObjectGroup.h"
