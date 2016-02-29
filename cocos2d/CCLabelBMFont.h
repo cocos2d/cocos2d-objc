c46b3ca335ab7e0c3ebf0174de2bad40db7e9152
s1ddok <s1ddok@MacBook-Pro-Andrey.local>
2016-02-29 19:34:27 +0300

    Simply move CCLabelBMF private header into the bottom of main one

--- cocos2d/CCLabelBMFont.h
+++ cocos2d/CCLabelBMFont.h
@@ -183,5 +183,131 @@
 
 @end
 
+enum {
+    kCCLabelAutomaticWidth = -1,
+};
 
+// ccBMFontDef BMFont definition.
+typedef struct _BMFontDef {
+    
+    // ID of the character.
+    unichar charID;
+    
+    // Origin and size of the font
+    CGRect rect;
+    
+    // The X amount the image should be offset when drawing the image (in pixels)
+    short xOffset;
+    
+    // The Y amount the image should be offset when drawing the image (in pixels)
+    short yOffset;
+    
+    // The amount to move the current position after drawing the character (in pixels)
+    short xAdvance;
+    
+} ccBMFontDef;
+
+// cBMFontPadding BMFont padding.
+typedef struct _BMFontPadding {
+    
+    // Padding left.
+    int	left;
+    
+    // Padding top.
+    
+    int top;
+    // Padding right.
+    
+    int right;
+    
+    // Padding bottom.
+    int bottom;
+    
+} ccBMFontPadding;
+
+#pragma mark - Hash Element
+// tCCFontDefHashElement.
+typedef struct _FontDefHashElement {
+    
+    // Key. Font Unicode value.
+    NSUInteger		key;
+    
+    // Font definition.
+    ccBMFontDef		fontDef;
+    
+    UT_hash_handle	hh;
+    
+} tCCFontDefHashElement;
+
+// tCCKerningHashElement.
+typedef struct _KerningHashElement {
+    
+    // Key for the hash. 16-bit for 1st element, 16-bit for 2nd element.
+    int				key;
+    
+    // Kerning value.
+    int				amount;
+    
+    // Had Handle.
+    UT_hash_handle	hh;
+    
+} tCCKerningHashElement;
+
+// CCBMFontConfiguration stores the parsed configuration of the specified .fnt file.
+@interface CCBMFontConfiguration : NSObject {
+    
+    // The character set defines the letters that actually exist in the font.
+    NSCharacterSet *_characterSet;
+    
+    // The atlas name.
+    NSString		*_atlasName;
+    
+@public
+    
+    // BMFont definitions.
+    tCCFontDefHashElement	*_fontDefDictionary;
+    
+    // FNTConfig: Common Height.
+    NSInteger		_commonHeight;
+    
+    // Padding.
+    ccBMFontPadding	_padding;
+    
+    // Values for kerning.
+    tCCKerningHashElement	*_kerningDictionary;
+}
+
+/// -----------------------------------------------------------------------
+/// @name Accessing the Configuration Attributes
+/// -----------------------------------------------------------------------
+
+// The character set defines the letters that actually exist in the font.
+@property (nonatomic, strong, readonly) NSCharacterSet *characterSet;
+
+// The atlas name.
+@property (nonatomic, readwrite, strong) NSString *atlasName;
+
+// Creates and returns a CCBMFontConfiguration object from a specified font file value.
++(instancetype) configurationWithFNTFile:(NSString*)FNTfile;
+
+
+/// -----------------------------------------------------------------------
+/// @name Initializing a CCLabelBMFont Object
+/// -----------------------------------------------------------------------
+
+//  Initializes and returns a CCBMFontConfiguration object from a specified font file value.
+-(id) initWithFNTfile:(NSString*)FNTfile;
+
+@end
+
+
+/// -----------------------------------------------------------------------
+/// @name Free Functions
+/// -----------------------------------------------------------------------
+
+// Load/Cache font configuration file and return object.
+CCBMFontConfiguration* FNTConfigLoadFile(NSString *file);
+
+// Clear font configuration cache.
+void FNTConfigRemoveCache(void);
 
