8577abf999dfb15e2bf601b22c7125e753709226
Marco Fabian <mf@frickelstudio.com>
2016-05-15 17:49:10 +0200

    Corrected some typos Converted tabs to spaces Added some comments

--- cocos2d/Support/ZipUtils.h
+++ cocos2d/Support/ZipUtils.h
@@ -10,8 +10,11 @@
  *		http://themanaworld.org/
  *		from the mapreader.cpp file
  *
+ * Some modifications were taken from:
+ *      https://www.codeandweb.com/texturepacker/contentprotection
+ *
  */
-
+ 
 #ifndef __CC_ZIP_UTILS_H
 #define __CC_ZIP_UTILS_H
 
@@ -20,49 +23,49 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    
-    /**
-     * Set the TexturePacker encryption key
-     *
-     * If your key used to encrypt the pvr.ccz file is
-     * aaaaaaaabbbbbbbbccccccccdddddddd 
-     * you have to call this function 4 times:
-     * caw_setkey_part(0, 0xaaaaaaaa);
-     * caw_setkey_part(1, 0xbbbbbbbb);
-     * caw_setkey_part(2, 0xcccccccc);
-     * caw_setkey_part(3, 0xdddddddd);
-     *
-     * Distribute the call accross some files but make sure
-     * to call all of the parts *before* loading the first
-     * spritesheet.
-     *
-     * @param index part of the key [0..3]
-     * @param value value of the key part
-     */
-    void caw_setkey_part(int index, uint32_t value);
 
-	/* XXX: pragma pack ??? */
-	/** @struct CCZHeader
-	 */
-	struct CCZHeader {
-		uint8_t			sig[4];				// signature. Should be 'CCZ!' 4 bytes
-		uint16_t		compression_type;	// should 0
-		uint16_t		version;			// should be 2 (although version type==1 is also supported)
-		uint32_t		reserved;			// Reserverd for users.
-		uint32_t		len;				// size of the uncompressed file
-	};
+/**
+ * Set the TexturePacker encryption key
+ *
+ * If your key used to encrypt the pvr.ccz file is
+ * aaaaaaaabbbbbbbbccccccccdddddddd
+ * you have to call this function 4 times:
+ * caw_setkey_part(0, 0xaaaaaaaa);
+ * caw_setkey_part(1, 0xbbbbbbbb);
+ * caw_setkey_part(2, 0xcccccccc);
+ * caw_setkey_part(3, 0xdddddddd);
+ *
+ * Distribute the call accross some files but make sure
+ * to call all of the parts *before* loading the first
+ * spritesheet.
+ *
+ * @param index part of the key [0..3]
+ * @param value value of the key part
+ */
+void caw_setkey_part(int index, uint32_t value);
 
-	enum {
-		CCZ_COMPRESSION_ZLIB,				// zlib format.
-		CCZ_COMPRESSION_BZIP2,				// bzip2 format (not supported yet)
-		CCZ_COMPRESSION_GZIP,				// gzip format (not supported yet)
-		CCZ_COMPRESSION_NONE,				// plain (not supported yet)
-	};
+/* XXX: pragma pack ??? */
+/** @struct CCZHeader
+ */
+struct CCZHeader {
+    uint8_t sig[4];                         // signature. Should be 'CCZ!' 4 bytes
+    uint16_t compression_type;              // should 0
+    uint16_t version;                       // should be 2 (although version type==1 is also supported)
+    uint32_t reserved;                      // Reserverd for users.
+    uint32_t len;                           // size of the uncompressed file
+};
+
+enum {
+    CCZ_COMPRESSION_ZLIB,                   // zlib format.
+    CCZ_COMPRESSION_BZIP2,                  // bzip2 format (not supported yet)
+    CCZ_COMPRESSION_GZIP,                   // gzip format (not supported yet)
+    CCZ_COMPRESSION_NONE,                   // plain (not supported yet)
+};
 
 /** @file
  * Zip helper functions
  */
-
+ 
 /**
  * Inflates either zlib or gzip deflated memory. The inflated memory is
  * expected to be freed by the caller.
@@ -70,7 +73,7 @@ extern "C" {
  * It will allocate 256k for the destination buffer. If it is not enought it will multiply the previous buffer size per 2, until there is enough memory.
  * @returns the length of the deflated buffer
  *
- @since v0.8.1
+   @since v0.8.1
  */
 int ccInflateMemory(unsigned char *in, unsigned int inLength, unsigned char **out);
 
@@ -78,11 +81,11 @@ int ccInflateMemory(unsigned char *in, unsigned int inLength, unsigned char **ou
  * Inflates either zlib or gzip deflated memory. The inflated memory is
  * expected to be freed by the caller.
  *
- * outLenghtHint is assumed to be the needed room to allocate the inflated buffer.
+ * outLengthHint is assumed to be the needed room to allocate the inflated buffer.
  *
  * @returns the length of the deflated buffer
  *
- @since v1.0.0
+   @since v1.0.0
  */
 int ccInflateMemoryWithHint(unsigned char *in, unsigned int inLength, unsigned char **out, unsigned int outLengthHint );
 
